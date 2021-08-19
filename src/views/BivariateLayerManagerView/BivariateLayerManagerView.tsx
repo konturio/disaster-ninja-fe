e6678542e0023a4bb6e18a1a38d439f5825f9c3d
Alexandr Dubinin <adubinin@kontur.io>
2021-08-19 11:27:18 +0200

    fix: update kit version

--- src/views/BivariateLayerManagerView/BivariateLayerManagerView.tsx
+++ src/views/BivariateLayerManagerView/BivariateLayerManagerView.tsx
@@ -14,7 +14,7 @@ import styles from './BivariateLayerManagerView.module.css';
 
 const BivariateLayerManagerView = () => (
   <>
-    <AppHeader title="Disater Ninja" />
+    <AppHeader title="Disaster Ninja" />
     <div
       style={{
         flex: 1,
