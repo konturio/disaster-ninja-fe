906750130629ca1dd676be79c95979ed3710c919
Alexandr Dubinin <akiyamka@gmail.com>
2021-09-13 17:04:38 +0200

    fix: use es6 format for interlan scripts

---
+++ updatek2.mjs
@@ -0,0 +1,15 @@
+import fs from 'fs';
+
+function updateK2() {
+  const packages = JSON.parse(fs.readFileSync('package.json'));
+  const needUpdate = Object.keys(packages.dependencies).filter(dep => dep.includes('@k2-packages'));
+  const needUpdateDev = Object.keys(packages.devDependencies).filter(dep => dep.includes('@k2-packages'));
+  return [...needUpdate, ...needUpdateDev].join(' ');
+}
+
+/* Python style? :D */
+if (require.main === module) {
+  process.stdout.write(updateK2());
+} else {
+  module.exports = updateK2;
+}
