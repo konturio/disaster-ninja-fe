61026ccd77bde2b38de83a693b3a3963d909ef74
Alexandr Dubinin <akiyamka@gmail.com>
2021-09-14 15:17:49 +0200

    test: fix jest tests

--- updatek2.mjs
+++ updatek2.mjs
@@ -1,15 +1,15 @@
 import fs from 'fs';
+import { fileURLToPath } from 'url';
+import process from 'process';
 
-function updateK2() {
+export function updateK2() {
   const packages = JSON.parse(fs.readFileSync('package.json'));
   const needUpdate = Object.keys(packages.dependencies).filter(dep => dep.includes('@k2-packages'));
   const needUpdateDev = Object.keys(packages.devDependencies).filter(dep => dep.includes('@k2-packages'));
-  return [...needUpdate, ...needUpdateDev].join(' ');
+  return [...needUpdate, ...needUpdateDev].join('\n');
 }
 
-/* Python style? :D */
-if (require.main === module) {
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  // If this script running from cli
   process.stdout.write(updateK2());
-} else {
-  module.exports = updateK2;
 }
