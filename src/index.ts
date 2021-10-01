225645b3c2120c4b0435abcbc5d9fd1fb03f321e
Alexandr Dubinin <akiyamka@gmail.com>
2021-10-01 12:10:12 +0200

    feat: add react bindings for reatom

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-/* Attach styles */
+import '@reatom/react/react-dom-batched-updates';
 import '@k2-packages/default-theme/variables.css';
 import '@k2-packages/default-theme/defaults.css';
 import '@k2-packages/default-theme/typography.css';
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
-import '~core/localization';
+import '~core/localization/i18n';
 import './global.css';
 import './App.tsx';
