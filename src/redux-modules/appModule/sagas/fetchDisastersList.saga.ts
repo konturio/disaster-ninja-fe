68213ea11e318b7147f2b7a77b68c9ad255ad76c
Alexandr Dubinin <akiyamka@gmail.com>
2021-10-01 12:08:33 +0200

    chore: temporary comment client calls - until new client not addded

--- src/redux-modules/appModule/sagas/fetchDisastersList.saga.ts
+++ src/redux-modules/appModule/sagas/fetchDisastersList.saga.ts
@@ -1,18 +1,18 @@
 import { put } from 'redux-saga/effects';
 import { setDisastersList } from '../actions';
-import client from '~services/api/client';
-import { GetDisastersList } from '~services/api/apiTypes';
+// import client from '~core/api_client';
+// import { GetDisastersList } from '~services/api/apiTypes';
 
 export default function* fetchDisastersListSaga() {
   try {
-    const response: GetDisastersList = yield client.getDisastersList();
-    switch (response.kind) {
-      case 'ok':
-        yield put(setDisastersList(response.data));
-        break;
-      default:
-        break;
-    }
+    // const response: GetDisastersList = yield client.getDisastersList();
+    // switch (response.kind) {
+    //   case 'ok':
+    //     yield put(setDisastersList(response.data));
+    //     break;
+    //   default:
+    //     break;
+    // }
   } catch (e) {
     console.error(e);
   }
