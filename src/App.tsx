2c81ad8aac867979773734f029146ed859ee9972
ilyaizr <ilyaizrailyan@gmail.com>
2021-10-22 14:12:03 +0300

    basic routing templete tbc

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import MainView from '~views/Main/Main';
+import CacheRoute, { CacheSwitch } from 'react-router-cache-route';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Reports } from '~views/Reports/Reports';
 
 ReactDOM.render(
   <StrictMode>
-    <MainView />
+    <Router>
+      {/* вум */}
+      <ul>
+        <li>
+          <Link to="/">Map</Link>
+        </li>
+        <li>
+          <Link to="/reports">Reports</Link>
+        </li>
+      </ul>
+
+      <CacheSwitch>
+        <Route exact path="/">
+          <MainView />
+        </Route>
+        <Route path="/reports">
+          <Reports />
+        </Route>
+      </CacheSwitch>
+    </Router>
   </StrictMode>,
   document.getElementById('root'),
 );
