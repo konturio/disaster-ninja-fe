d9c6d0117dafb7368cfb5c83639ee3350e976fbe
Alexandr Dubinin <akiyamka@gmail.com>
2021-10-26 16:09:02 +0000

    Update mapLogicalLayersAtom.ts

--- src/core/shared_state/mapLogicalLayersAtom.ts
+++ src/core/shared_state/mapLogicalLayersAtom.ts
@@ -13,40 +13,53 @@ export interface IMapLogicalLayer {
 export const mapLogicalLayersAtom = createAtom(
   {
     currentMapAtom,
-    mountLayer: (layer: IMapLogicalLayer) => layer,
-    unmountLayer: (layer: IMapLogicalLayer | string) => layer,
+    addLayer: (layer: IMapLogicalLayer) => layer,
+    removeLayer: (layer:  IMapLogicalLayer | string) => typeof layer === 'string' ? layer : layer.id,
+    mountLayer: (layerId: string) => layerId,
+    unmountLayer: (layerId: string) => layerId,
   },
   ({ get, onAction, onChange }, state: IMapLogicalLayer[] = []) => {
     const map = get('currentMapAtom');
 
-    onAction('mountLayer', (layer: IMapLogicalLayer) => {
-      if (!map) return;
+    onAction('addLayer', (layer: IMapLogicalLayer) => {
+      state = [...state, layer];
+    });
 
-      if (state.findIndex((lr) => lr.id === layer.id) !== -1) {
-        throw new Error('Layer with such ID is already added to map');
-        return;
+    onAction('removeLayer', (layerId: string) => {
+      state = state.filter(lr => lr.id !== layerId);
+    });
+
+
+    onAction('mountLayer', (layerId: string) => {
+      if (!map) return;
+      const layer = state.find(layer => layer.id === layerId);
+      if (!layer) {
+        throw new Error(`Layer with '${layerId}' ID is not added to logical layers`);
       }
       layer.mount(map);
     });
 
-    onAction('unmountLayer', (layer: IMapLogicalLayer | string) => {
+    onAction('unmountLayer', (layerId: string) => {
       if (!map) return;
 
-      const layerId = typeof layer !== 'string' ? layer.id : layer;
-      const addedLayer = state.find((lr) => lr.id === layerId);
-      if (!addedLayer) {
-        throw new Error('Layer with such ID is not added to map');
-        return;
+      const layer = state.find(layer => layer.id === layerId);
+      if (!layer) {
+        throw new Error(`Layer with '${layerId}' ID is not added to logical layers`);
       }
-      if (addedLayer.unmount) {
-        addedLayer.unmount(map);
+
+      if (layer.unmount) {
+        layer.unmount(map);
       }
     });
 
-    // reset all map layers when map object has been changed
+
+        // reset all map layers when map object has been changed
     onChange('currentMapAtom', (mapState) => {
       state = [];
     });
+
+
+
 
     return state;
   },
