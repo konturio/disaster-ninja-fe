16a0fcb7175f2f7e36f29ddaadf20d4bdd43d137
Alexandr Dubinin <akiyamka@gmail.com>
2021-10-26 12:40:15 +0000

    Fix cors errors in local development

--- vite.config.ts
+++ vite.config.ts
@@ -8,42 +8,44 @@ import packageJson from './package.json';
 const relative = (folder: string) => path.resolve(__dirname, folder);
 
 // https://vitejs.dev/config/
-export default ({ mode }) => defineConfig({
-  base: mode === 'development' ? '/' : packageJson.homepage,
-  // build: {
-  //   minify: false,
-  //   sourcemap: true,
-  // },
-  plugins: [
-    mode === 'development' && reactRefresh(),
-    mode === 'production' && viteBuildInfoPlugin()
-  ],
-  css: {
-    postcss: postcssConfig,
-  },
-  esbuild: {
-    // Avoid conflicting with "import React"
-    jsxInject: 'import { createElement, Fragment } from "react"',
-    jsxFactory: 'createElement',
-    jsxFragment: 'Fragment',
-  },
-  resolve: {
-    alias: {
-      '~components': relative('./src/components'),
-      '~views': relative('./src/views'),
-      '~config': relative('./src/config'),
-      '~utils': relative('./src/utils'),
-      '~services': relative('./src/services'),
-      '~appModule': relative('./src/redux-modules/appModule'),
-      '~core': relative('./src/core'),
-      '~features': relative('./src/features'),
+export default ({ mode }) =>
+  defineConfig({
+    base: mode === 'development' ? '/' : packageJson.homepage,
+    build: {
+      minify: true,
+      sourcemap: true,
     },
-  },
-  server: {
-    proxy: {
-      '/tiles': 'https://geocint.kontur.io',
-      '/graphql': 'https://test-apps02.konturlabs.com/insights-api',
-      '/boundaries': 'https://test-api02.konturlabs.com'
-    }
-  }
-})
+    plugins: [
+      mode === 'development' && reactRefresh(),
+      mode === 'production' && viteBuildInfoPlugin(),
+    ],
+    css: {
+      postcss: postcssConfig,
+    },
+    esbuild: {
+      // Avoid conflicting with "import React"
+      jsxInject: 'import { createElement, Fragment } from "react"',
+      jsxFactory: 'createElement',
+      jsxFragment: 'Fragment',
+    },
+    resolve: {
+      alias: {
+        '~components': relative('./src/components'),
+        '~views': relative('./src/views'),
+        '~config': relative('./src/config'),
+        '~utils': relative('./src/utils'),
+        '~services': relative('./src/services'),
+        '~appModule': relative('./src/redux-modules/appModule'),
+        '~core': relative('./src/core'),
+        '~features': relative('./src/features'),
+      },
+    },
+    server: {
+      proxy: {
+        [packageJson.homepage + '/api']: {
+          target: 'https://test-apps-ninja02.konturlabs.com',
+          changeOrigin: true,
+        },
+      },
+    },
+  });
