57945ac02dc1b9e38a62a6adc0a3df498d678c2d
ilyaizr <ilyaizrailyan@gmail.com>
2021-11-03 12:30:20 +0300

    toast fixes

--- src/features/toasts/components/Toast/Toast.tsx
+++ src/features/toasts/components/Toast/Toast.tsx
@@ -6,26 +6,33 @@ import closeCross from '../../icons/close_alert.svg';
 import errorAlert from '../../icons/error_alert.svg';
 import warningAlert from '../../icons/warning_alert.svg';
 import infogAlert from '../../icons/info_alert.svg';
+import { useMemo } from 'react';
 
-export function Toast({ notification }: { notification: Notification }) {
+export function Toast({
+  notification,
+  nodeRef,
+}: {
+  notification: Notification;
+  nodeRef: React.MutableRefObject<null>;
+}) {
   function close() {
     notification.onClose();
   }
 
-  function iconType() {
+  const iconType = useMemo(() => {
     if (notification.type === 'warning') return warningAlert;
     if (notification.type === 'info') return infogAlert;
     return errorAlert;
-  }
+  }, [notification]);
 
   return (
-    <div className={clsx(styles.container)}>
+    <div className={clsx(styles.container)} ref={nodeRef}>
       <Card
         inline={true}
         className={clsx(styles.toast, styles['toast-' + notification.type])}
       >
         <div className={clsx(styles.content)}>
-          <img src={iconType()} className={clsx(styles.icon)} />
+          <img src={iconType} className={clsx(styles.icon)} />
           <Text type="short-l">
             <span className={clsx(styles.text)}>
               {notification.message.description || notification.message.title}
--- src/features/toasts/components/ToastGroup/ToastGroup.tsx
+++ src/features/toasts/components/ToastGroup/ToastGroup.tsx
@@ -1,10 +1,12 @@
 import clsx from 'clsx';
+import { useRef } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { Notification } from '~core/shared_state/currentNotifications';
 import { Toast } from '../Toast/Toast';
 import styles from './ToastGroup.module.css';
 
 export function ToastGroup({ toasts }: { toasts: Notification[] }) {
+  const nodeRef = useRef(null);
   return (
     <TransitionGroup className={clsx(styles.toasts)}>
       {toasts.map((t) => (
@@ -16,8 +18,9 @@ export function ToastGroup({ toasts }: { toasts: Notification[] }) {
             enterActive: styles.enterActive,
             exit: styles.toastExit,
           }}
+          nodeRef={nodeRef}
         >
-          <Toast notification={t} />
+          <Toast notification={t} nodeRef={nodeRef} />
         </CSSTransition>
       ))}
     </TransitionGroup>
