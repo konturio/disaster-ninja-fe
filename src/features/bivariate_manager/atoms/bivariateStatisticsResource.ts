32b516ce202294bc5b1bf47a0cfb9588ec97f75a
andrei-kucherov <kucherov.andrei@gmail.com>
2021-11-12 11:11:02 +0000

    Bivariate manager

---
+++ src/features/bivariate_manager/atoms/bivariateStatisticsResource.ts
@@ -0,0 +1,78 @@
+import { createResourceAtom } from '~utils/atoms';
+import { graphQlClient } from '~core/index';
+
+export const bivariateStatisticsResourceAtom = createResourceAtom(
+  null,
+  async () => {
+    const responseData = await graphQlClient.post<any>(`/`, {
+      query: `
+      fragment AxisFields on Axis {
+        label
+        steps {
+          label
+          value
+        }
+        quality
+        quotient
+      }
+
+    query getPolygonStatistics {
+      polygonStatistic(polygonStatisticRequest: {}) {
+        bivariateStatistic {
+          axis {
+            ...AxisFields
+          }
+          meta {
+            max_zoom
+            min_zoom
+          }
+          overlays {
+            name
+            description
+            active
+            colors {
+              id
+              color
+            }
+            x {
+              ...AxisFields
+            }
+            y {
+              ...AxisFields
+            }
+          }
+          indicators {
+            name
+            label
+            copyrights
+            direction
+          }
+          correlationRates {
+            x {
+              ...AxisFields
+            }
+            y {
+              ...AxisFields
+            }
+            rate
+            quality
+            correlation
+          }
+          colors {
+            fallback
+            combinations {
+              color
+              corner
+              color_comment
+            }
+          }
+        }
+      }
+    }`,
+    });
+    if (!responseData || !responseData.data)
+      throw new Error('No data received');
+    return responseData.data;
+  },
+  'bivariateStatisticsResource',
+);
