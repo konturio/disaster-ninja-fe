e816b8e205c3e7d27937fc24542adf4c88360c11
Alexandr Dubinin <akiyamka@gmail.com>
2021-11-16 11:50:52 +0000

    Conditional styling

--- src/utils/atoms/createLogicalLayerAtom/legend.ts
+++ src/utils/atoms/createLogicalLayerAtom/legend.ts
@@ -6,17 +6,24 @@ interface MapCSSProperties {
   // Add bivariate steps
 }
 
+export interface SimpleLegendStep {
+  paramName: string;
+  paramValue: string | number;
+  stepName: string;
+  stepShape: SimpleLegendStepType;
+  sourceLayer?: string; // Required for vector tile source, unnecessary for other
+  style: MapCSSProperties;
+}
+
 export interface SimpleLegend {
   name: string;
   type: 'simple';
-  steps: {
-    paramName: string;
-    paramValue: string | number;
-    stepName: string;
-    stepShape: SimpleLegendStepType;
-    sourceLayer?: string; // Required for vector tile source, unnecessary for other
-    style: MapCSSProperties;
-  };
+  steps: SimpleLegendStep[];
+}
+
+export interface BivariateLegendStep {
+  label: string;
+  color: string;
 }
 
 export interface BivariateLegend {
@@ -27,7 +34,7 @@ export interface BivariateLegend {
     x: Axis;
     y: Axis;
   };
-  steps: { label: string; color: string }[];
+  steps: BivariateLegendStep[];
 }
 
 export type LayerLegend = SimpleLegend | BivariateLegend;
