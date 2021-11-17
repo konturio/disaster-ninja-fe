2dd76f0c38e168b900eac82533e3d69f818ed288
Alexandr Dubinin <akiyamka@gmail.com>
2021-11-17 23:16:17 +0000

    Release 2.0.0 alfa

--- src/features/current_event/index.tsx
+++ src/features/current_event/index.tsx
@@ -1,5 +1,7 @@
 import { currentEventGeometry } from './atoms/currentEventGeometry';
+import { currentEventRefresher } from './atoms/currentEventRefresher';
 
 export function initCurrentEvent() {
   currentEventGeometry.subscribe((val) => null);
+  currentEventRefresher.subscribe(() => null);
 }
