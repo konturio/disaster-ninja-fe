502cdadcf4c8add2dac1ad8d289e4ab93d87bf88
Alexandr Dubinin <akiyamka@gmail.com>
2021-11-19 09:13:11 +0000

    7916 add notification of beta version to dn 2

--- src/views/Reports/Reports.tsx
+++ src/views/Reports/Reports.tsx
@@ -1,13 +1,19 @@
 import { AppHeader } from '@k2-packages/ui-kit';
 import { TranslationService as i18n } from '~core/localization';
 import { ReportsList } from '~features/reports/components/ReportsList/ReportsList';
+import { Row } from '~components/Layout/Layout';
+import { BetaLabel } from '~components/BetaLabel/BetaLabel';
 import s from './Reports.module.css';
 
 export function Reports() {
   return (
     <div>
       <div className={s.headerContainer}>
-        <AppHeader title={`Disaster Ninja ${i18n.t('Reports')}`} />
+        <AppHeader title={`Disaster Ninja ${i18n.t('Reports')}`}>
+          <Row>
+            <BetaLabel />
+          </Row>
+        </AppHeader>
       </div>
       <ReportsList />
     </div>
