c44bb2e9b44aeac9ca2781ae21ada238b23f77fa
Alexandr Dubinin <akiyamka@gmail.com>
2021-11-19 14:07:29 +0000

    Feat  mounted and sorted layers list

--- src/core/shared_state/index.ts
+++ src/core/shared_state/index.ts
@@ -2,7 +2,7 @@ export { currentEpisodeAtom } from './currentEpisode';
 export { currentEventAtom } from './currentEvent';
 export { currentMapPositionAtom } from './currentMapPosition';
 export { currentUserAtom } from './currentUser';
-export { enabledUserLayersAtom } from './enabledUserLayers';
+export { mountedLogicalLayersAtom } from './mountedLogicalLayers';
 export { focusedGeometryAtom } from './focusedGeometry';
 export { mapListenersAtom } from './mapListeners';
 export { currentNotificationAtom } from './currentNotifications';
