f0082f3b91f84125c983f659a64b333b2f4576c0
Alexandr Dubinin <adubinin@kontur.io>
2021-11-19 12:22:59 +0100

    chore: add ability to disable devtools

--- src/core/store/store.ts
+++ src/core/store/store.ts
@@ -3,15 +3,16 @@ import { createDevtoolsLogger } from '~utils/debug/reatom-redux-devtools';
 
 function configureStore() {
   const devtoolsLogger = createDevtoolsLogger();
-  // // Must be cutted out in production by terser
-  // if (import.meta.env.DEV) {
-  //   return createStore({
-  //     // @ts-expect-error - have bad typings since it experimental;
-  //     onError: (error, t) => devtoolsLogger(t, error),
-  //     onPatch: (t) => devtoolsLogger(t),
-  //     now: globalThis.performance?.now.bind(performance) ?? Date.now,
-  //   });
-  // }
+  // Must be cutted out in production by terser
+  if (import.meta.env.VITE_REDUX_DEV_TOOLS) {
+    console.log('devtools enabled');
+    return createStore({
+      // @ts-expect-error - have bad typings since it experimental;
+      onError: (error, t) => devtoolsLogger(t, error),
+      onPatch: (t) => devtoolsLogger(t),
+      now: globalThis.performance?.now.bind(performance) ?? Date.now,
+    });
+  }
   return createStore();
 }
 
