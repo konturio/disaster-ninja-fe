ce4d772e07d82e05d0c14607a25f52492d31083e
ilyaizr <ilyaizrailyan@gmail.com>
2021-11-22 15:45:44 +0300

    WIP styles

--- src/views/Reports/Reports.tsx
+++ src/views/Reports/Reports.tsx
@@ -3,15 +3,33 @@ import { TranslationService as i18n } from '~core/localization';
 import { ReportsList } from '~features/reports/components/ReportsList/ReportsList';
 import { Row } from '~components/Layout/Layout';
 import { BetaLabel } from '~components/BetaLabel/BetaLabel';
+import { Text } from '@k2-packages/ui-kit';
 import s from './Reports.module.css';
+import { useHistory } from 'react-router';
+import config from '~core/app_config';
 
 export function Reports() {
+  const history = useHistory();
+
   return (
     <div>
       <div className={s.headerContainer}>
-        <AppHeader title={`Disaster Ninja ${i18n.t('Reports')}`}>
+        <AppHeader title="" key={history.location.pathname}>
           <Row>
-            <BetaLabel />
+            <Text type="short-l">
+              <div className={s.customAppTitle}>
+                <span
+                  className={s.clickable}
+                  onClick={() => history.push(config.baseUrl)}
+                >
+                  {i18n.t('Disaster Ninja')}
+                </span>{' '}
+                <span>{i18n.t('Reports')}</span>
+              </div>
+            </Text>
+            <div className={s.betaTag}>
+              <BetaLabel />
+            </div>
           </Row>
         </AppHeader>
       </div>
