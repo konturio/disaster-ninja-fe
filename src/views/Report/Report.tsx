b36b76601041f1a33fb013171684aa3abf756cfb
ilyaizr <ilyaizrailyan@gmail.com>
2021-11-22 15:45:44 +0300

    WIP styles

--- src/views/Report/Report.tsx
+++ src/views/Report/Report.tsx
@@ -1,5 +1,6 @@
 import { AppHeader } from '@k2-packages/ui-kit';
 import { TranslationService as i18n } from '~core/localization';
+import { Text } from '@k2-packages/ui-kit';
 import { ReportInfo } from '~features/reports/components/ReportInfo/ReportInfo';
 import s from './Report.module.css';
 
@@ -8,6 +9,7 @@ export function ReportPage() {
     <div>
       <div className={s.headerContainer}>
         <AppHeader title={`Disaster Ninja ${i18n.t('Reports')}`} />
+        <Text type="heading-l">{i18n.t('Disaster Ninja')}</Text>
       </div>
       <ReportInfo />
     </div>
