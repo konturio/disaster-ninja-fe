6e9cea3b10502e2dfd5ff9d820158e25e4a76a4d
ilyaizr <ilyaizrailyan@gmail.com>
2021-11-22 16:26:16 +0300

    link to main app from reports

--- src/views/Report/Report.tsx
+++ src/views/Report/Report.tsx
@@ -2,14 +2,35 @@ import { AppHeader } from '@k2-packages/ui-kit';
 import { TranslationService as i18n } from '~core/localization';
 import { Text } from '@k2-packages/ui-kit';
 import { ReportInfo } from '~features/reports/components/ReportInfo/ReportInfo';
+import { Row } from '~components/Layout/Layout';
+import { BetaLabel } from '~components/BetaLabel/BetaLabel';
 import s from './Report.module.css';
+import { useHistory } from 'react-router';
+import config from '~core/app_config';
 
 export function ReportPage() {
+  const history = useHistory();
   return (
     <div>
       <div className={s.headerContainer}>
-        <AppHeader title={`Disaster Ninja ${i18n.t('Reports')}`} />
-        <Text type="heading-l">{i18n.t('Disaster Ninja')}</Text>
+        <AppHeader title="">
+          <Row>
+            <Text type="short-l">
+              <div className={s.customAppTitle}>
+                <span
+                  className={s.clickable}
+                  onClick={() => history.push(config.baseUrl)}
+                >
+                  {i18n.t('Disaster Ninja')}
+                </span>{' '}
+                <span>{i18n.t('Reports')}</span>
+              </div>
+            </Text>
+            <div className={s.betaTag}>
+              <BetaLabel />
+            </div>
+          </Row>
+        </AppHeader>
       </div>
       <ReportInfo />
     </div>
