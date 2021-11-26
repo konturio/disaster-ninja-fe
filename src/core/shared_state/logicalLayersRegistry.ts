efc53ff932928f91659ebfdc67e7b300171c6b95
Alexandr Dubinin <akiyamka@gmail.com>
2021-11-26 11:13:06 +0000

    mounted-layers refactor

--- src/core/shared_state/logicalLayersRegistry.ts
+++ src/core/shared_state/logicalLayersRegistry.ts
@@ -1,6 +1,9 @@
 import appConfig from '~core/app_config';
 import { createBindAtom } from '~utils/atoms/createBindAtom';
-import { LogicalLayerAtom } from '~utils/atoms/createLogicalLayerAtom';
+import {
+  LogicalLayerAtom,
+  LogicalLayerAtomState,
+} from '~utils/atoms/createLogicalLayerAtom';
 
 export const logicalLayersRegistryAtom = createBindAtom(
   {
@@ -51,4 +54,57 @@ export const logicalLayersRegistryAtom = createBindAtom(
     return state;
   },
   '[Shared state] logicalLayersRegistryAtom',
+);
+
+/**
+ * This atom subscribe to all atoms in registry and save their states.
+ * Read only
+ * Don't use it if yoi need only one layer state
+ **/
+type registryStateAtomContext = {
+  unsubscribes?: Record<string, () => void>;
+};
+
+export const logicalLayersRegistryStateAtom = createBindAtom(
+  {
+    registry: logicalLayersRegistryAtom,
+    _updateState: (stateUpdate: LogicalLayerAtomState) => stateUpdate,
+  },
+  (
+    { onChange, schedule, create, onAction },
+    state: Record<string, LogicalLayerAtomState> = {},
+  ) => {
+    onAction('_updateState', (update) => {
+      state = { ...state, [update.id]: update };
+    });
+
+    onChange('registry', (atomsList) => {
+      const atoms: [LogicalLayerAtom[], LogicalLayerAtom[]] = [[], []];
+
+      const [oldAtoms, newAtoms] = Object.values(atomsList).reduce(
+        (acc, a) => ((state[a.id] ? acc[0] : acc[1]).push(a), acc),
+        atoms,
+      );
+
+      schedule((dispatch, ctx: registryStateAtomContext) => {
+        ctx.unsubscribes = ctx.unsubscribes ?? {};
+        const oldItemsIds = new Set(oldAtoms.map((a) => a.id));
+        Object.keys(state).forEach((atomId) => {
+          if (!oldItemsIds.has(atomId)) {
+            const unsubscribe = ctx.unsubscribes![atomId];
+            if (unsubscribe) unsubscribe();
+          }
+        });
+
+        newAtoms.forEach((atom) => {
+          ctx.unsubscribes![atom.id] = atom.subscribe((s) =>
+            dispatch(create('_updateState', s)),
+          );
+        });
+      });
+    });
+
+    return state;
+  },
+  '[Shared state] logicalLayersRegistryStateAtom',
 );
--- src/utils/atoms/createLogicalLayerAtom/index.ts
+++ src/utils/atoms/createLogicalLayerAtom/index.ts
@@ -1,3 +1,4 @@
 export { createLogicalLayerAtom } from './createLogicalLayerAtom';
 export type { LogicalLayer, LogicalLayerAtom } from './createLogicalLayerAtom';
 export type { LayerLegend } from './legend';
+export type { LogicalLayerAtomState } from './createLogicalLayerAtom';
