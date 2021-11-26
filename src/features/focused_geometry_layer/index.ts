2282f88e4b013d132ebf5cff1cec0f94e7a9d681
Alexandr Dubinin <adubinin@kontur.io>
2021-11-26 19:29:52 +0100

    fix: sometimes focused layers not loaded - in cases when layer want mount before map loaded

--- src/features/focused_geometry_layer/index.ts
+++ src/features/focused_geometry_layer/index.ts
@@ -1,9 +1,14 @@
 import { focusedGeometryLayerAtom } from './atoms/focusedGeometryLayer';
-import { logicalLayersRegistryAtom } from '~core/shared_state/logicalLayersRegistry';
+import { logicalLayersRegistryAtom, currentMapAtom } from '~core/shared_state';
 
 export function initFocusedGeometryLayer() {
   logicalLayersRegistryAtom.registerLayer.dispatch(focusedGeometryLayerAtom);
   focusedGeometryLayerAtom.init.dispatch();
-  focusedGeometryLayerAtom.mount.dispatch();
   focusedGeometryLayerAtom.subscribe(() => null);
+  const unsubscribe = currentMapAtom.subscribe((map) => {
+    if (map) {
+      focusedGeometryLayerAtom.mount.dispatch();
+      unsubscribe();
+    }
+  });
 }
