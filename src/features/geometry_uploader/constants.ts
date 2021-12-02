502212274a807e7bd123b6bd21c26f3313eb6d1b
IlyaIzr <ilyaizrailyan@gmail.com>
2021-12-02 08:32:46 +0000

    8039 measure mode overlaps boundaries selector

---
+++ src/features/geometry_uploader/constants.ts
@@ -0,0 +1,2 @@
+export const GEOMETRY_UPLOADER_CONTROL_ID = 'UploadFile';
+export const GEOMETRY_UPLOADER_CONTROL_NAME = 'Upload File';
--- src/features/reports/index.ts
+++ src/features/reports/index.ts
@@ -2,17 +2,19 @@ import { sideControlsBarAtom } from '~core/shared_state';
 import { REPORTS_CONROL_ID, REPORTS_CONROL_NAME } from './constants';
 import { ReportsIcon } from './components/icon/ReportsIcon';
 import { History } from 'history';
+import { controlVisualGroup } from '~core/shared_state/sideControlsBar';
 
 export function initReportsIcon(history: History) {
   sideControlsBarAtom.addControl.dispatch({
     id: REPORTS_CONROL_ID,
     name: REPORTS_CONROL_NAME,
     active: false,
-    group: 'tools',
+    visualGroup: controlVisualGroup.noAnalitics,
     icon: ReportsIcon(),
     onClick: (becomesActive) => {
-      sideControlsBarAtom.toggleActiveState.dispatch(REPORTS_CONROL_ID);
+      sideControlsBarAtom.enable.dispatch(REPORTS_CONROL_ID);
       history.push('./reports');
+      sideControlsBarAtom.disable.dispatch(REPORTS_CONROL_ID);
     },
   });
 }
