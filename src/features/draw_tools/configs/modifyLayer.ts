bd63826683b4a673b4dda26e7e04e58bce47dfa0
ilyaizr <ilyaizrailyan@gmail.com>
2021-12-03 17:42:20 +0300

    drawn data on modify mode

--- src/features/draw_tools/configs/modifyLayer.ts
+++ src/features/draw_tools/configs/modifyLayer.ts
@@ -1,14 +1,55 @@
-import { MapboxLayerProps } from "@deck.gl/mapbox/mapbox-layer";
 import { EditableGeoJsonLayer } from "@nebula.gl/layers";
-import { AnyLayer } from "maplibre-gl";
 import { drawModes } from "../constants";
 import { ModifyMode } from "../modes/modifyMode";
+
+
+const exampleGeo = {
+  "geometry": {
+    "type": "Polygon",
+    "coordinates": [
+      [
+        [
+          53.24676749999969,
+          73.844393544434
+        ],
+        [
+          82.4967717915341,
+          58.9228002926334
+        ],
+        [
+          25.965515354232487,
+          58.63121797209951
+        ],
+        [
+          25.824892499999685,
+          58.63121797209951
+        ],
+        [
+          53.24676749999969,
+          73.844393544434
+        ]
+      ]
+    ]
+  },
+  "type": "Feature",
+  "properties": {}
+}
+const myFeatureCollection = {
+  type: 'FeatureCollection',
+  features: [
+    exampleGeo
+  ],
+};
 
 
 export const modifyDeckLayerConfig = {
   id: drawModes.ViewMode,
   type: EditableGeoJsonLayer,
   mode: ModifyMode,
+  // be shure to pass array, the modes do not expect non iterable value
+  selectedFeatureIndexes: [], //0 to select firts feature
+  // typescript marks this as error. Yet this collection drawn on map
+  data: myFeatureCollection,
   parameters: {
     depthTest: false, // skip z-buffer check
   },
@@ -22,8 +63,12 @@ export const modifyDeckLayerConfig = {
       //   return 20000 / (zoom * zoom);
       // },
       getFillColor: () => [0xff, 0x66, 0x00, 0xff],
-      getLineWidth: () => 2,
+      getLineWidth: () => 3,
       stroked: false,
+    },
+    geojson: {
+      getFillColor: (feature) => [0xff, 0x66, 0x00, 0xff],
+      getLineColor: (feature) => [0xff, 0x66, 0x00, 0xff],
     },
   },
   modeConfig: {
