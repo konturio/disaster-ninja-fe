988f21358fbcbfed74d200e56e2494372238512a
ilyaizr <ilyaizrailyan@gmail.com>
2021-12-09 12:11:21 +0300

    draw modes toggle on buttons click

--- src/features/draw_tools/atoms/activeDrawMode.ts
+++ src/features/draw_tools/atoms/activeDrawMode.ts
@@ -1,15 +1,23 @@
 import { createBindAtom } from '~utils/atoms/createBindAtom';
-import { DrawModeType } from '../constants';
+import { drawModes, DrawModeType } from '../constants';
 
 export const activeDrawModeAtom = createBindAtom(
   {
     setDrawMode: (mode: DrawModeType | undefined) => mode,
+    toggleDrawMode: (mode: DrawModeType | undefined) => mode
   },
   ({ onAction }, state: DrawModeType | undefined = undefined) => {
     onAction('setDrawMode', (mode) => {
       if (state !== mode) {
         state = mode;
       }
+    });
+    onAction('toggleDrawMode', (mode) => {
+      if (!mode && !state) return;
+      if (mode === drawModes.ModifyMode && state === mode) return; 
+      if (state === mode) {
+        state = drawModes.ModifyMode;
+      } else state = mode
     });
 
     return state;
