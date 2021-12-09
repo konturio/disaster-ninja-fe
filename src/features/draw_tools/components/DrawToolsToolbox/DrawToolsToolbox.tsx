12b99a048075637cfb44868d188e0487220a0bd6
ilyaizr <ilyaizrailyan@gmail.com>
2021-12-09 16:08:15 +0300

    draw mode hint

--- src/features/draw_tools/components/DrawToolsToolbox/DrawToolsToolbox.tsx
+++ src/features/draw_tools/components/DrawToolsToolbox/DrawToolsToolbox.tsx
@@ -1,5 +1,5 @@
-import { Button } from '@k2-packages/ui-kit';
-import { useCallback } from 'react';
+import { Button, Text } from '@k2-packages/ui-kit';
+import { useCallback, useState } from 'react';
 import { TranslationService as i18n } from '~core/localization';
 import s from './DrawToolToolbox.module.css';
 import {
@@ -11,7 +11,7 @@ import {
 import { useAtom } from '@reatom/react';
 import { activeDrawModeAtom } from '~features/draw_tools/atoms/activeDrawMode';
 import clsx from 'clsx';
-import { drawModes, DrawModeType } from '~features/draw_tools/constants';
+import { CLOSE_DRAW_HINT, drawModes, DrawModeType } from '~features/draw_tools/constants';
 import { modeWatcherAtom } from '~features/draw_tools/atoms/drawLayerAtom';
 import { selectedIndexesAtom } from '~features/draw_tools/atoms/selectedIndexesAtom';
 import { drawnGeometryAtom } from '~features/draw_tools/atoms/drawnGeometryAtom';
@@ -21,6 +21,10 @@ export const DrawToolsToolbox = () => {
   const [selected] = useAtom(selectedIndexesAtom)
   const [, { removeByIndexes }] = useAtom(drawnGeometryAtom)
   useAtom(modeWatcherAtom);
+
+  const [hintShown, setHintShown] = useState(
+    window.localStorage.getItem(CLOSE_DRAW_HINT) !== 'true',
+  )
 
   const onClick = useCallback(
     (modeId: string) => {
@@ -36,7 +40,7 @@ export const DrawToolsToolbox = () => {
     },
     [setDrawMode],
   );
-  
+
   const onDelete = useCallback(
     () => {
       if (selected.length) removeByIndexes(selected)
@@ -44,32 +48,49 @@ export const DrawToolsToolbox = () => {
     [selected],
   );
 
+  const closeHint = useCallback(
+    () => {
+      setHintShown(false)
+      window.localStorage.setItem(CLOSE_DRAW_HINT, 'true');
+    },
+    [setHintShown],
+  );
+
+
   return activeDrawMode ? (
-    <div className={s.drawToolsContainer}>
-      <Button id={drawModes.DrawPolygonMode} active={activeDrawMode === drawModes.DrawPolygonMode} onClick={() => onClick(drawModes.DrawPolygonMode)}>
-        <div className={s.btnContainer}>
-          <DrawPolygonIcon /> {i18n.t('Area')}
-        </div>
-      </Button>
-      <Button id={drawModes.DrawLineMode} active={activeDrawMode === drawModes.DrawLineMode} onClick={() => onClick(drawModes.DrawLineMode)}>
-        <div className={s.btnContainer}>
-          <DrawLineIcon /> {i18n.t('Line')}
-        </div>
-      </Button>
-      <Button id={drawModes.DrawPointMode} active={activeDrawMode === drawModes.DrawPointMode} onClick={() => onClick(drawModes.DrawPointMode)}>
-        <div className={s.btnContainer}>
-          <DrawPointIcon /> {i18n.t('Point')}
-        </div>
-      </Button>
-      <Button active={Boolean(selected.length)} onClick={onDelete}>
-        <div className={s.btnContainer}>
-          <TrashBinIcon />
-        </div>
-      </Button>
-      {/* this is temporary  */}
-      <Button className={s.finishBtn} onClick={() => finishDrawing()}>
-        <div className={clsx(s.btnContainer)}>{i18n.t('Finish Drawing')}</div>
-      </Button>
+    <div>
+      {hintShown && <div className={s.drawHint} onClick={closeHint}>
+        <Text type="caption">
+          <span className={s.drawHintText}>{i18n.t('Click on the map to begin drawing')}</span>
+        </Text>
+      </div>}
+
+      <div className={s.drawToolsContainer}>
+        <Button id={drawModes.DrawPolygonMode} active={activeDrawMode === drawModes.DrawPolygonMode} onClick={() => onClick(drawModes.DrawPolygonMode)}>
+          <div className={s.btnContainer}>
+            <DrawPolygonIcon /> {i18n.t('Area')}
+          </div>
+        </Button>
+        <Button id={drawModes.DrawLineMode} active={activeDrawMode === drawModes.DrawLineMode} onClick={() => onClick(drawModes.DrawLineMode)}>
+          <div className={s.btnContainer}>
+            <DrawLineIcon /> {i18n.t('Line')}
+          </div>
+        </Button>
+        <Button id={drawModes.DrawPointMode} active={activeDrawMode === drawModes.DrawPointMode} onClick={() => onClick(drawModes.DrawPointMode)}>
+          <div className={s.btnContainer}>
+            <DrawPointIcon /> {i18n.t('Point')}
+          </div>
+        </Button>
+        <Button active={Boolean(selected.length)} onClick={onDelete}>
+          <div className={s.btnContainer}>
+            <TrashBinIcon />
+          </div>
+        </Button>
+        {/* this is temporary  */}
+        <Button className={s.finishBtn} onClick={() => finishDrawing()}>
+          <div className={clsx(s.btnContainer)}>{i18n.t('Finish Drawing')}</div>
+        </Button>
+      </div>
     </div>
   ) : null;
 };
