6ee8de0830fe0b18b6c394eeade8374a74763b06
ilyaizr <ilyaizrailyan@gmail.com>
2021-12-09 17:32:40 +0300

    geometry uploading interception

--- src/features/draw_tools/atoms/drawnGeometryAtom.ts
+++ src/features/draw_tools/atoms/drawnGeometryAtom.ts
@@ -13,12 +13,13 @@ export const drawnGeometryAtom = createBindAtom(
     sendToFocusedGeometry: () => null,
     updateFeatures: (features: Feature[]) => features,
     removeByIndexes: (indexes: number[]) => indexes,
+    focusedGeometryAtom
   },
-  ({ schedule, onAction, create }, state: FeatureCollection = defaultState) => {
+  ({ schedule, onAction, create, onChange }, state: FeatureCollection = defaultState) => {
 
     onAction('addFeature', (feature) => {
       state = { ...state, features: [...state.features, feature] }
-      
+
       schedule(dispatch => dispatch(create('sendToFocusedGeometry')))
     });
 
@@ -44,9 +45,22 @@ export const drawnGeometryAtom = createBindAtom(
         return !indexesToRemove.includes(featureIndex)
       })
       state = stateCopy
-      
+
       schedule(dispatch => dispatch(create('sendToFocusedGeometry')))
     });
+
+    onChange('focusedGeometryAtom', incoming => {
+      if (incoming?.source.type === 'uploaded') schedule(dispatch => {
+        if (incoming.geometry.type === 'FeatureCollection' && incoming.geometry.features?.length) {
+          const actions: any[] = []
+          incoming.geometry.features.forEach((feature) => actions.push(create('addFeature', feature)));
+          actions.length && dispatch(actions);
+        } else if (incoming.geometry.type === 'Feature') {
+          dispatch(create('addFeature', incoming.geometry))
+        }
+        else console.warn('wrong type of data imported')
+      })
+    })
 
 
     return state;
