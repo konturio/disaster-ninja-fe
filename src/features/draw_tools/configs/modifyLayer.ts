95daf0f78debdaf2917e481032da17a6ee2242aa
ilyaizr <ilyaizrailyan@gmail.com>
2021-12-17 15:18:40 +0300

    icons setting

--- src/features/draw_tools/configs/modifyLayer.ts
+++ src/features/draw_tools/configs/modifyLayer.ts
@@ -21,31 +21,37 @@ export const modifyDeckLayerConfig = {
       getLineColor: [20, 20, 10, 140],
     },
     geojson: {
-      getFillColor: () => [60, 20, 20, 100],
-      getLineColor: [150, 10, 20, 180],
+      getFillColor: (a) => {
+        if (a?.geometry?.type === 'Point') return null        
+        return [60, 20, 20, 100]
+      },
+      getLineColor: (a) => {
+        if (a?.geometry?.type === 'Point') return null        
+        return [150, 10, 20, 180]
+      },
       stroked: true
     },
   },
   // editHandleType: 'icon',  //starts search for icons. However it's unknown where to put these props https://deck.gl/docs/api-reference/layers/icon-layer
 
-//   editHandleType: 'icon',
-//   editHandleIconAtlas: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/icon-atlas.png',
-//   editHandleIconMapping: {
-//     marker: {
-//       x: 0,
-//       y: 0,
-//       width: 128,
-//       height: 128,
-//       anchorY: 128,
-//       mask: true,
-//     }
-//   },
-//   editHandleIconSizeScale: 15,
-//   getEditHandleIcon: d => 'marker',
-//   getEditHandleIconSize: 15,
-//   getEditHandleIconColor: d => [200, 200, 200],
-//   getEditHandlePosition: d => d.coordinates,
-//   pickable: true,
+  //   editHandleType: 'icon',
+  //   editHandleIconAtlas: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/icon-atlas.png',
+  //   editHandleIconMapping: {
+  //     marker: {
+  //       x: 0,
+  //       y: 0,
+  //       width: 128,
+  //       height: 128,
+  //       anchorY: 128,
+  //       mask: true,
+  //     }
+  //   },
+  //   editHandleIconSizeScale: 15,
+  //   getEditHandleIcon: d => 'marker',
+  //   getEditHandleIconSize: 15,
+  //   getEditHandleIconColor: d => [200, 200, 200],
+  //   getEditHandlePosition: d => d.coordinates,
+  //   pickable: true,
 }
 
 
