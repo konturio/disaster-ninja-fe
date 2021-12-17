237d7143935995b3c13661cb1f5bf548fb11e30c
ilyaizr <ilyaizrailyan@gmail.com>
2021-12-17 15:18:40 +0300

    icons setting

--- src/features/draw_tools/configs/showIcons.ts
+++ src/features/draw_tools/configs/showIcons.ts
@@ -11,6 +11,7 @@ export const showIconDeckLayerConfig = {
   *   { coordinates: [-122.466233, 37.684638] },
   *   ...
   * ]
+  * source https://deck.gl/docs/api-reference/layers/icon-layer
   */
 
   // required by class
@@ -25,16 +26,15 @@ export const showIconDeckLayerConfig = {
       // mask: true,
     },
     selectedIcon: {
-      x: 130,
+      x: 128,
       y: 0,
-      width: 135,
+      width: 128,
       height: 165,
       anchorY: 160,
     }
   },
   // required to show data
   getIcon: d => {
-    console.log('%c⧭', 'color: #eeff00', d);
     if (d.isHidden) return null
     if (d.isSelected) return 'selectedIcon'
     return 'defaultIcon'
@@ -42,32 +42,12 @@ export const showIconDeckLayerConfig = {
   getPosition: d => d.coordinates,
 
 
-  sizeScale: 15,
+  sizeScale: 6,
   getSize: d => 10,
-  getColor: d => {
-    return [120, 140, 0]
-  },
-
   onClick: info => console.log(info),
 
 
-  // onIconError: null,
-  // sizeMaxPixels: Number.MAX_SAFE_INTEGER,
-  // sizeMinPixels: 0,
-  // sizeScale: 8,
-  // sizeUnits: 'pixels',
-
-  /* props inherited from Layer class */
-
-  // autoHighlight: false,
-  // coordinateOrigin: [0, 0, 0],
-  // coordinateSystem: COORDINATE_SYSTEM.LNGLAT,
-  // highlightColor: [0, 0, 128, 128],
-  // modelMatrix: null,
-  // opacity: 1,
   pickable: true,
-  // visible: true,
-  // wrapLongitude: false,
   _subLayerProps: {
     guides: {}
   }
