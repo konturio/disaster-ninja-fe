7da0f273e63e1b1b975a87110fb9e22c68314fa1
Andrei Kucherov <kucherov.andrei@gmail.com>
2021-12-23 09:55:43 +0300

    Fixes for bivariate

--- src/features/bivariate_manager/components/BivariateMatrixContainer/BivariateMatrixContainer.tsx
+++ src/features/bivariate_manager/components/BivariateMatrixContainer/BivariateMatrixContainer.tsx
@@ -1,34 +1,28 @@
 import { useCallback, useState } from 'react';
-import { useZoomEvent } from '~utils/events/useZoomEvent';
 import s from './BivariateMatrixContainer.module.css';
 import ConnectedBivariateMatrix
   from '~features/bivariate_manager/components/ConnectedBivariateMatrix/ConnectedBivariateMatrix';
+import clsx from 'clsx';
 
 interface BivariateMatrixContainerProps {
   className?: string;
 }
 
 const BivariateMatrixContainer = ({ className }: BivariateMatrixContainerProps) => {
-  const [reloading, setReloading] = useState<boolean>(false);
   const [dimensions, setDimensions] = useState<{ w: number; h: number } | null>(
     null,
   );
-
-  const [scale, setScale] = useState(1);
 
   const onRefChange = useCallback(
     (ref: HTMLDivElement | null) => {
       if (ref) {
         const recalculateDimensions = () => {
           const dim = ref.getClientRects()[0];
-          const dynamicScale = 1;
-          setScale(dynamicScale);
-          // coeff 0.85 here is because of transform: scale(0.85) applied to matrix
+          // coeff 0.7 here is because of transform: scale(0.7) applied to matrix
           const baseDim =
-            parseFloat(ref.getAttribute('base-dimension') || '0') *
-            dynamicScale;
+            parseFloat(ref.getAttribute('base-dimension') || '0') * .7;
           const newWidth = baseDim + dim.width + 18;
-          const newHeight = dim.height + 2;
+          const newHeight = dim.height + 5;
           if (
             !dimensions ||
             Math.abs(dimensions.w - newWidth) > 3 ||
@@ -38,56 +32,42 @@ const BivariateMatrixContainer = ({ className }: BivariateMatrixContainerProps) 
           }
         };
 
-        const observer = new MutationObserver((mutationsList) => {
-          mutationsList.forEach((mutation) => {
-            if (mutation.type === 'attributes') {
-              recalculateDimensions();
-            }
-          });
-        });
-
-        observer.observe(ref, {
-          attributes: true,
-          childList: false,
-          subtree: false,
-        });
+        // const observer = new MutationObserver((mutationsList) => {
+        //   mutationsList.forEach((mutation) => {
+        //     if (mutation.type === 'attributes') {
+        //       recalculateDimensions();
+        //     }
+        //   });
+        // });
+        //
+        // observer.observe(ref, {
+        //   attributes: true,
+        //   childList: false,
+        //   subtree: false,
+        // });
         recalculateDimensions();
       }
     },
     [dimensions],
   );
 
-  // hack to force refresh bivariate panel on browser zoom out
-  const forceReloading = () => {
-    setReloading(true);
-    setTimeout(() => {
-      setReloading(false);
-    }, 0);
-  };
-
-  useZoomEvent(forceReloading, forceReloading);
-
   return (
     <div
-      className={className}
+      id='bivariate-matrix-container'
+      className={clsx(s.bivariatecContainer, className)}
       style={
         dimensions
           ? { width: dimensions.w, height: dimensions.h }
           : undefined
       }
     >
-      {!reloading && (
-        <div className={s.scrollMatrix}>
-          <div
-            className={s.matrixContainer}
-            style={{ transform: `scale(${scale}) translateX(140px)` }}
-          >
-            <ConnectedBivariateMatrix ref={onRefChange} />
-          </div>
-          <div className={s.topRightCorner} />
-          <div className={s.bottomRightCorner} />
+      <div className={s.scrollMatrix}>
+        <div className={s.matrixContainer}>
+          <ConnectedBivariateMatrix ref={onRefChange} />
         </div>
-      )}
+        <div className={s.topRightCorner} />
+        <div className={s.bottomRightCorner} />
+      </div>
     </div>
   );
 };
