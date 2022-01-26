de2d41702c8744d48730b52b0f9b600c45e70eca
Andrei Kucherov <kucherov.andrei@gmail.com>
2022-01-26 12:38:23 +0300

    feat(app-features): Add login ability as app-feature

--- src/core/auth/models/UserDataModel.ts
+++ src/core/auth/models/UserDataModel.ts
@@ -1,4 +1,4 @@
-type AppFeature = "analytics_panel" | "events_list" | "map_layers_panel" | "side_bar" | "bivariate_manager"
+type AppFeature = "app_login" | "analytics_panel" | "events_list" | "map_layers_panel" | "side_bar" | "bivariate_manager"
   | "current_event" | "focused_geometry_layer" | "layers_in_area" | "map_ruler" | "toasts" | "boundary_selector"
   | "draw_tools" | "geometry_uploader" | "legend_panel" | "reports" | "url_store" | "interactive_map"
   | "current_episode" | "geocoder" | "episode_list" | "communities" | "feature_settings" | "osm_edit_link"
