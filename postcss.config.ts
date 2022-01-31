3c083cc4b8153ae44c4452470e88108affc5f27d
IlyaIzr <ilyaizrailyan@gmail.com>
2022-01-31 08:14:24 +0000

    8493 add tooltips for sidebar and draw tools buttons

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,9 +1,13 @@
 import autoprefixer from 'autoprefixer';
 import postcssNormalize from 'postcss-normalize';
 import postcssNested from 'postcss-nested';
+import postcssCustomMedia from 'postcss-custom-media'
 
 export default {
   plugins: [
+    postcssCustomMedia({
+      importFrom: './node_modules/@k2-packages/default-theme/variables.css'
+    }),
     autoprefixer,
     postcssNormalize,
     postcssNested,
