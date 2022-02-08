e0ee809a5a6cf7113817947d0ad394861832791a
Andrei Kucherov <kucherov.andrei@gmail.com>
2022-02-08 10:36:10 +0300

    fix(user-menu): Add user menu selector

--- src/features/user_profile/components/UserProfileButton/UserProfile.tsx
+++ src/features/user_profile/components/UserProfileButton/UserProfile.tsx
@@ -1,10 +1,47 @@
 import { useAtom } from '@reatom/react';
 import { LoginButton } from '~features/user_profile';
-import s from './UserProfile.module.css';
 import { userStateAtom } from '~core/auth/atoms/userState';
+import { useCallback } from 'react';
+import s from './UserProfile.module.css';
+import { OptionType, Selector } from '@k2-packages/ui-kit/tslib/Selector';
+
+const userMenu: OptionType[] = [
+  {
+    label: 'Foo',
+    hint: 'This is foo',
+    value: 'foo',
+  },
+  {
+    label: 'Bar',
+    hint: 'This is bar',
+    value: 'bar',
+  },
+  {
+    label: 'Baz',
+    hint: 'This is baz',
+    value: 'baz',
+  },
+  {
+    label: 'Disabled',
+    hint: 'This is disabled',
+    value: 'disabled',
+    disabled: true,
+  },
+];
 
 function UserAvatar() {
-  return <img src='assets/default-avatar.png' className={s.avatar} alt='user avatar' />
+  const onImgClick = useCallback(() => {
+
+  }, []);
+
+  return (
+    <>
+      <div className={s.dropdownContainer}>
+        <Selector onChange={console.log} options={userMenu} selected={'foo'} />
+      </div>
+      <img onClick={onImgClick} src='assets/default-avatar.png' className={s.avatar} alt='user avatar' />
+    </>
+  )
 }
 
 export function UserProfile() {
