fe51c90a33bcac7403c5a7005353c263bb169101
IlyaIzr <ilyaizrailyan@gmail.com>
2022-02-11 12:18:17 +0000

    8368-missed-legend-icons-for-active-contributors-layers

--- src/features/current_event/atoms/currentEventResource.ts
+++ src/features/current_event/atoms/currentEventResource.ts
@@ -3,20 +3,27 @@ import { apiClient } from '~core/index';
 import { currentEventAtom, currentEventFeedAtom } from '~core/shared_state';
 import { EventWithGeometry } from '~core/types';
 
-const eventDependencyAtom = createBindAtom({currentEventAtom, currentEventFeedAtom},
-  ({ get },
-  state: { event: { id: string } | null; feed: { id: string } | null } = { event: null, feed: null }) => {
+const eventDependencyAtom = createBindAtom(
+  { currentEventAtom, currentEventFeedAtom },
+  (
+    { get },
+    state: { event: { id: string } | null; feed: { id: string } | null } = {
+      event: null,
+      feed: null,
+    },
+  ) => {
     const event = get('currentEventAtom');
     const feed = get('currentEventFeedAtom');
     return { event, feed };
-});
+  },
+);
 
 export const currentEventResourceAtom = createResourceAtom(
   eventDependencyAtom,
   async (deps) => {
     if (deps && deps.event?.id && deps.feed?.id) {
       const responseData = await apiClient.get<EventWithGeometry>(
-        `/events/${deps.feed.id}/${deps.event.id}`
+        `/events/${deps.feed.id}/${deps.event.id}`,
       );
       if (responseData === undefined) throw 'No data received';
       return responseData;
