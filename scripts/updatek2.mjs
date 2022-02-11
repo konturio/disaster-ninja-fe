82ba280bcae9e5714b07d1c47cbee07ea7c07315
IlyaIzr <ilyaizrailyan@gmail.com>
2022-02-11 12:18:17 +0000

    8368-missed-legend-icons-for-active-contributors-layers

--- scripts/updatek2.mjs
+++ scripts/updatek2.mjs
@@ -5,7 +5,7 @@ import process from 'process';
 export function updateK2() {
   const packages = JSON.parse(fs.readFileSync('package.json'));
   const needUpdate = Object.keys(packages.dependencies).filter(dep => dep.includes('@k2-packages'));
-  const needUpdateDev = Object.keys(packages.devDependencies).filter(dep => dep.includes('@k2-packages'));
+  const needUpdateDev = Object.keys(packages.devDependencies || {}).filter(dep => dep.includes('@k2-packages'));
   return [...needUpdate, ...needUpdateDev].join('\n');
 }
 
