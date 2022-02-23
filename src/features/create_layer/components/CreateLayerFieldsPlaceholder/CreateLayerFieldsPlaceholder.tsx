661cdebd6148a1b39ad2ce5b552dc4a41a2d3b0e
Andrei Kucherov <kucherov.andrei@gmail.com>
2022-02-23 10:05:57 +0300

    feat(create-layer): Update create layer form

--- src/features/create_layer/components/CreateLayerFieldsPlaceholder/CreateLayerFieldsPlaceholder.tsx
+++ src/features/create_layer/components/CreateLayerFieldsPlaceholder/CreateLayerFieldsPlaceholder.tsx
@@ -1,9 +1,33 @@
 import s from './CreateLayerFieldsPlaceholder.module.css';
+import clsx from 'clsx';
+import { translationService as i18n } from '~core/index';
+import { Button } from '@k2-packages/ui-kit';
+import { UploadFileIcon } from '@k2-packages/default-icons';
+import { CreateLayerFieldContainer } from '../CreateLayerFieldContainer/CreateLayerFieldContainer';
+import { LayerFieldAtomType } from '~features/create_layer/atoms/createLayerField';
 
-export function CreateLayerFieldsPlaceholder() {
+interface CreateLayerFieldsPlaceholderProps {
+  fieldModels: LayerFieldAtomType[];
+  onAddField: () => void;
+  onRemoveField: (index: number) => void;
+  onReorderFields: (oldIndex: number, newIndex: number) => void;
+}
+
+export function CreateLayerFieldsPlaceholder( { fieldModels, onAddField, onRemoveField, onReorderFields }: CreateLayerFieldsPlaceholderProps ) {
   return (
-    <>
-
-    </>
+    <div className={s.fieldsContainer}>
+      <div className={clsx(s.fieldsLabel, 'k-font-caption')}>
+        <div className={s.textCaption}>{i18n.t('Fields')}</div>
+      </div>
+      <div className={s.fieldsPlaceholder}>
+        { fieldModels.map((fldModelAtom) => (
+          <CreateLayerFieldContainer key={fldModelAtom.id} data={fldModelAtom} />
+        )) }
+      </div>
+      <Button onClick={onAddField} className={s.addFieldButton} variant='invert-outline'>
+        <UploadFileIcon />
+        {i18n.t('Add field')}
+      </Button>
+    </div>
   );
 }
--- src/features/create_layer/components/CreateLayerForm/CreateLayerForm.tsx
+++ src/features/create_layer/components/CreateLayerForm/CreateLayerForm.tsx
@@ -1,41 +1,56 @@
-import { Atom } from '@reatom/core';
-import { CreateLayerModel } from '~features/create_layer/types';
 import s from './CreateLayerForm.module.css';
 import { useAtom } from '@reatom/react';
 import { LabelWithTooltip } from '~components/LabelWithTooltip/LabelWithTooltip';
 import { translationService as i18n } from '~core/index';
 import { Button, Input } from '@k2-packages/ui-kit';
-import { MarkerIcon, UploadFileIcon } from '@k2-packages/default-icons';
-import clsx from 'clsx';
+import { MarkerIcon } from '@k2-packages/default-icons';
+import { CreateLayerFieldsPlaceholder } from '../CreateLayerFieldsPlaceholder/CreateLayerFieldsPlaceholder';
+import { useCallback } from 'react';
+import { LayerDataAtomType } from '~features/create_layer/atoms/createLayerData';
 
 interface CreateLayerFormProps {
-  data: Atom<CreateLayerModel>;
+  data: LayerDataAtomType;
 }
 
 export function CreateLayerForm({ data }: CreateLayerFormProps) {
-  const [ formState ] = useAtom(data);
+  const [ formState, { addField, removeField, reorderFields } ] = useAtom(data);
+
+  const onAddField = useCallback(() => {
+    addField();
+  }, [addField]);
+
+  const onRemoveField = useCallback((index: number) => {
+
+  }, [removeField]);
+
+  const onReorderFields = useCallback((oldIndex: number, newIndex: number) => {
+
+  }, [reorderFields]);
 
   return (
     <div className={s.formContainer}>
       <div className={s.formParam}>
-        <LabelWithTooltip text={i18n.t('Layer name')} description={i18n.t('Layer name')} />
+        <LabelWithTooltip
+          text={i18n.t('Layer name')}
+          description={i18n.t('Layer name')}
+        />
         <Input className={s.formInput} />
       </div>
       <div className={s.formParam}>
-        <LabelWithTooltip text={i18n.t('Marker icon')} description={i18n.t('Marker icon')} />
+        <LabelWithTooltip
+          text={i18n.t('Marker icon')}
+          description={i18n.t('Marker icon')}
+        />
         <Button>
           <MarkerIcon />
         </Button>
       </div>
-      <div className={s.fieldsContainer}>
-        <div className={clsx(s.fieldsLabel, 'k-font-caption')}>
-          <div className={s.textCaption}>{i18n.t('Fields')}</div>
-        </div>
-        <Button className={s.addFieldButton} variant='invert-outline'>
-          <UploadFileIcon />
-          {i18n.t('Add field')}
-        </Button>
-      </div>
+      <CreateLayerFieldsPlaceholder
+        fieldModels={formState.fields}
+        onAddField={onAddField}
+        onRemoveField={onRemoveField}
+        onReorderFields={onReorderFields}
+      />
       <div className={s.buttonsContainer}>
         <Button className={s.saveBtn}>
           {i18n.t('Save')}
