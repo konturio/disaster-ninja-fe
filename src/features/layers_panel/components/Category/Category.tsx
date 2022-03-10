34411a120b67366efb29cc228235691628ea7b71
Alexandr Dubinin <akiyamka@gmail.com>
2022-03-10 16:38:37 +0000

    8997-allow-to-disable-radio-button-by-click-on-enabled-layer

--- src/features/layers_panel/components/Category/Category.tsx
+++ src/features/layers_panel/components/Category/Category.tsx
@@ -4,7 +4,10 @@ import { FoldingWrap } from '~components/FoldingWrap/FoldingWrap';
 import { mountedLayersByCategoryAtom } from '~features/layers_panel/atoms/mountedLayersByCategory';
 import { Group } from '../Group/Group';
 import { CategoryWithSettings } from '~core/types/layers';
+import { TranslationService as i18n } from '~core/localization';
+import { useAction } from '@reatom/react';
 import s from './Category.module.css';
+import { categoryDeselection } from '../../atoms/categoryDeselection';
 
 function CategoryMountedLayersCounter({ categoryId }: { categoryId: string }) {
   const [counters] = useAtom(mountedLayersByCategoryAtom);
@@ -13,18 +16,28 @@ function CategoryMountedLayersCounter({ categoryId }: { categoryId: string }) {
 
 export function Category({ category }: { category: CategoryWithSettings }) {
   const [isOpen, setOpenState] = useState(category.openByDefault ?? false);
-
+  const onGroupDeselect = useAction(
+    () => categoryDeselection.deselect(category.id),
+    [category.id],
+  );
   return (
     <div className={s.category}>
       <FoldingWrap
         open={isOpen}
-        label={
+        title={
           <div className={s.categoryTitle}>
             <span>{category.name}</span>
             {!category.mutuallyExclusive ? (
               <CategoryMountedLayersCounter categoryId={category.id} />
             ) : null}
           </div>
+        }
+        controls={
+          category.mutuallyExclusive && (
+            <button onClick={onGroupDeselect} className={s.categoryControl}>
+              {i18n.t('Deselect')}
+            </button>
+          )
         }
         onStateChange={(newState) => setOpenState(!newState)}
       >
