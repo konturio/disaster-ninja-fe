fba8a733b7e0234639021a97b35ebd69b6c0588e
adubinin <adubinin@gmail.com>
2022-03-15 12:43:37 +0100

    feat: perfomance improvement - do not load font awesome before bivariate manager was activeated

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import '@reatom/react/react-dom-batched-updates';
 import '@k2-packages/default-theme/variables.css';
 import '@k2-packages/default-theme/defaults.css';
 import '@k2-packages/default-theme/typography.css';
-import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@fortawesome/fontawesome-free/js/solid';
 import '~utils/atoms/disableDefaultStore';
 import './core';
 import './global.css';
