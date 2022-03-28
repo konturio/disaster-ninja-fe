d1fad522b6564c022a6c99b93c101c6ff4fab869
Andrei Kucherov <kucherov.andrei@gmail.com>
2022-03-28 16:40:28 +0300

    fix(login): Reload layers on login/logout

--- src/core/shared_state/currentEvent.ts
+++ src/core/shared_state/currentEvent.ts
@@ -1,5 +1,6 @@
 import { createAtom } from '~utils/atoms';
 import { focusedGeometryAtom } from './focusedGeometry';
+import { currentEventFeedAtom } from '~core/shared_state/currentEventFeed';
 
 export type CurrentEventAtomState = {
   id: string;
@@ -10,13 +11,20 @@ export const currentEventAtom = createAtom(
     setCurrentEventId: (eventId: string) => eventId,
     resetCurrentEvent: () => null,
     focusedGeometryAtom,
+    currentEventFeedAtom,
   },
-  ({ onAction, onChange }, state: CurrentEventAtomState = null) => {
+  ({ onAction, onChange, getUnlistedState }, state: CurrentEventAtomState = null) => {
     onAction('setCurrentEventId', (eventId) => (state = { id: eventId }));
     onAction('resetCurrentEvent', () => (state = null));
     onChange('focusedGeometryAtom', (focusedGeometry) => {
       const currentGeometrySource = focusedGeometry?.source;
       if (currentGeometrySource && currentGeometrySource.type !== 'event') {
+        state = null;
+      }
+    });
+    onChange('currentEventFeedAtom', () => {
+      const focusedGeometry = getUnlistedState(focusedGeometryAtom);
+      if (!focusedGeometry || focusedGeometry.source?.type === 'event') {
         state = null;
       }
     });
