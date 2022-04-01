38aed98e1092bc4de384a4474bdbb32f53f7abbc
ilyaizr <ilyaizrailyan@gmail.com>
2022-04-01 15:30:40 +0300

    tooltip implementation

--- src/core/logical_layers/types/legends.ts
+++ src/core/logical_layers/types/legends.ts
@@ -20,6 +20,10 @@ export interface SimpleLegend {
   linkProperty?: string;
   type: 'simple';
   steps: SimpleLegendStep[];
+  tooltip?: {
+    type: 'markdown';
+    propName: string;
+  };
 }
 
 export interface BivariateLegendStep {
@@ -53,6 +57,4 @@ export type LayerLegend =
   | BivariateLegend
   | BivariateLegendBackend;
 
-export type LayerDetailsLegend =
-  | SimpleLegend
-  | BivariateLegendBackend;
+export type LayerDetailsLegend = SimpleLegend | BivariateLegendBackend;
