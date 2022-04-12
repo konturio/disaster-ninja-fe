57b17d823d828382a2503884b8e30a1de3535337
adubinin <adubinin@kontur.io>
2022-04-12 21:34:17 +0200

    fix: - currentEditedLayerFeatures and drawnGeometryAtom desync

--- src/features/create_layer/atoms/currentEditedLayerFeatures.ts
+++ src/features/create_layer/atoms/currentEditedLayerFeatures.ts
@@ -5,7 +5,6 @@ import { drawnGeometryAtom } from '~core/draw_tools/atoms/drawnGeometryAtom';
 import { apiClient, notificationService } from '~core/index';
 import { deepCopy } from '~core/logical_layers/utils/deepCopy';
 import { editableLayersListResource } from './editableLayersListResource';
-import { selectedIndexesAtom } from '~core/draw_tools/atoms/selectedIndexesAtom';
 
 export const currentEditedLayerFeatures = createAtom(
   {
@@ -13,6 +12,7 @@ export const currentEditedLayerFeatures = createAtom(
     setFeatures: drawnGeometryAtom.setFeatures,
     addFeature: drawnGeometryAtom.addFeature,
     removeFeature: drawnGeometryAtom.removeByIndexes,
+    drawnGeometryAtom,
     reset: () => null,
     save: () => null,
     setFeatureProperty: (
@@ -21,61 +21,72 @@ export const currentEditedLayerFeatures = createAtom(
     ) => ({ featureIdx, properties }),
   },
   (
-    { onAction, getUnlistedState, schedule },
+    { onAction, getUnlistedState, schedule, get },
     state: null | GeoJSON.Feature[] = null,
   ): typeof state => {
+    state = get('drawnGeometryAtom').features;
+
     onAction('reset', () => {
       state = null;
+      schedule((dispatch, ctx = {}) => {
+        dispatch(drawnGeometryAtom.setFeatures([]));
+      });
     });
 
-    /* I read features only once because layer course can be updated in the meddle of my editing */
+    /* I read features only once because layer course can be updated in the middle of my editing */
     onAction('readFeaturesFromLayer', (layerId) => {
+      /* Read layer source */
       const layersSources = getUnlistedState(layersSourcesAtom);
       const layerSource = layersSources.get(layerId);
       const sourceData = layerSource?.data?.source;
+      let features: GeoJSON.Feature[] = [];
       if (sourceData?.type === 'geojson') {
         const sourceDataCopy = deepCopy(sourceData);
         if (sourceDataCopy.data.type === 'Feature') {
-          state = [sourceDataCopy.data];
+          features = [sourceDataCopy.data];
         }
         if (sourceDataCopy.data.type === 'FeatureCollection') {
-          state = sourceDataCopy.data.features;
+          features = sourceDataCopy.data.features;
         }
       }
+
+      /**
+       * Note that i'am not set state here.
+       * This is because it will be done later in onAction('setFeatures', ...)
+       * where features will be with id's
+       */
       schedule((dispatch, ctx = {}) => {
         ctx.layerId = layerId;
-        if (state) dispatch(drawnGeometryAtom.setFeatures(state));
+        if (state) dispatch(drawnGeometryAtom.setFeatures(features));
       });
     });
 
     onAction('addFeature', (feature) => {
       if (state === null) return;
-      const lastIndex = state.length - 1;
-      schedule((dispatch) => {
-        dispatch(selectedIndexesAtom.setIndexes([lastIndex]));
-      });
-    });
 
-    onAction('removeFeature', (indexes) => {
-      if (state === null) return;
-      const newState = Array.from(state);
-      indexes.forEach((i) => {
-        newState.splice(i, 1);
-      });
-      state = newState;
+      /**
+       * This not working because selectedIndexesAtom !== selectedFeature
+       * In fact after feature selected selectedIndexesAtom reset
+       */
+      // const lastIndex = state.length - 1;
+      // schedule((dispatch) => {
+      //   dispatch(selectedIndexesAtom.setIndexes([lastIndex]));
+      // });
     });
 
     onAction('setFeatureProperty', ({ featureIdx, properties }) => {
-      if (state === null) return;
-      const newState = Array.from(state);
-      newState[featureIdx] = {
-        ...newState[featureIdx],
-        properties: {
-          ...newState[featureIdx].properties,
-          ...properties,
-        },
-      };
-      state = newState;
+      schedule((dispatch, ctx = {}) => {
+        if (state === null) return;
+        const newState = Array.from(state);
+        newState[featureIdx] = {
+          ...newState[featureIdx],
+          properties: {
+            ...newState[featureIdx].properties,
+            ...properties,
+          },
+        };
+        if (state) dispatch(drawnGeometryAtom.setFeatures(newState));
+      });
     });
 
     onAction('save', () => {
@@ -96,41 +107,6 @@ export const currentEditedLayerFeatures = createAtom(
           }
         }
       });
-    });
-
-    // drawnGeometryAtom use 'setFeatures' for update selected point position after drag
-    onAction('setFeatures', (features) => {
-      if (state === null) return;
-      // This features not contain new properties, so I update ONLY positions here
-      const featureIdToGeometryMap = new Map<
-        string | number,
-        GeoJSON.Geometry
-      >();
-      let featuresWithoutId = 0;
-      features.forEach(({ geometry, id }) => {
-        if (id === undefined) {
-          featuresWithoutId++;
-        } else {
-          featureIdToGeometryMap.set(id, geometry);
-        }
-      });
-
-      const newState = Array.from(state);
-
-      state = newState.map((f) => {
-        if (f.id === undefined) {
-          featuresWithoutId++;
-        } else {
-          const newGeometry = featureIdToGeometryMap.get(f.id);
-          if (newGeometry) {
-            return { ...f, geometry: newGeometry };
-          }
-        }
-        return f;
-      });
-
-      if (featuresWithoutId)
-        console.error('Cannot update position for feature without id');
     });
 
     return state;
