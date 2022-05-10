01b4854c3682344970378ac92c50b4146e155bfb
IlyaIzr <ilyaizrailyan@gmail.com>
2022-05-10 13:24:28 +0000

    10160 adjust dn 2 0 to mobile devices

--- src/features/legend_panel/index.tsx
+++ src/features/legend_panel/index.tsx
@@ -3,11 +3,15 @@ import { LegendPanel } from '~features/legend_panel/components/LegendPanel/Legen
 import { mountedLayersAtom } from '~core/logical_layers/atoms/mountedLayers';
 import { useMemo } from 'react';
 
-export function Legend({ iconsContainerId }: { iconsContainerId: string }) {
+export function Legend({
+  iconsContainerRef,
+}: {
+  iconsContainerRef: React.MutableRefObject<HTMLDivElement | null>;
+}) {
   const [layers] = useAtom(mountedLayersAtom);
   const layersAtoms = useMemo(() => Array.from(layers.values()), [layers]);
 
   return (
-    <LegendPanel iconsContainerId={iconsContainerId} layers={layersAtoms} />
+    <LegendPanel iconsContainerRef={iconsContainerRef} layers={layersAtoms} />
   );
 }
