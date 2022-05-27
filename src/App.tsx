b13e2f7aa224e090d1254292e5eeca4176a48823
IlyaIzr <ilyaizrailyan@gmail.com>
2022-05-27 09:32:46 +0000

    Release 2.0.23

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import ReactDOM from 'react-dom';
 import { RoutedApp } from './Routes';
 import { reatomContext } from '@reatom/react';
 import { store } from '~core/store/store';
-import { BetaSplashScreen } from '~components/BetaSplashScreen/BetaSplashScreen';
 import { AuthWrapper } from '~core/auth';
 
 ReactDOM.render(
   <reatomContext.Provider value={store}>
-    <BetaSplashScreen />
     <AuthWrapper>
       <RoutedApp />
     </AuthWrapper>
