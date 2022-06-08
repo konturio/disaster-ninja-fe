57abdd509a063b611b2d5ae1cd1f54cbee0dd8bb
andrei-kucherov <kucherov.andrei@gmail.com>
2022-06-08 11:41:36 +0000

    fix(bivariate-legend): change tile schema to be able to use relative tiles

--- public/config/appconfig.js
+++ public/config/appconfig.js
@@ -5,6 +5,8 @@ window.konturAppConfig = {
   BOUNDARIES_API: 'https://test-api02.konturlabs.com',
   REPORTS_API: 'https://test-apps-ninja02.konturlabs.com/active/reports',
   TILES_API: 'https://zigzag.kontur.io/tiles/stats/',
+  // use param BIVARIATE_TILES_API if you need to setup external server for bivariate tiles
+  //BIVARIATE_TILES_API: 'http://localhost:3000/',
   REFRESH_INTERVAL_SEC: 300,
   MAP_ACCESS_TOKEN: '',
   MAP_BASE_STYLE: 'https://zigzag.kontur.io/tiles/basemap/style_ninja.json',
