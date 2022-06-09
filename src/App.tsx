dee6a7438c139f41bca47934abd6bc11ba82f519
Andrei Kucherov <kucherov.andrei@gmail.com>
2022-06-09 12:01:56 +0300

    chore: update packages

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { reatomContext } from '@reatom/react';
 import { store } from '~core/store/store';
 import { AuthWrapper } from '~core/auth';
 
+console.log('test');
+console.log('test2');
+
 ReactDOM.render(
   <reatomContext.Provider value={store}>
     <AuthWrapper>
