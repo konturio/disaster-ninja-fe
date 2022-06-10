54d085af4c008eb10fffae33deec9469a181ffa2
andrei-kucherov <kucherov.andrei@gmail.com>
2022-06-10 12:03:52 +0000

    10487 color interpolation scheme

--- src/components/ConnectedMap/map-libre-adapter/useArrayDiff.test.ts
+++ src/components/ConnectedMap/map-libre-adapter/useArrayDiff.test.ts
@@ -1,4 +1,4 @@
-import { setupTestContext } from '../../../utils/test-utils/setupTest';
+import { setupTestContext } from '../../../utils/test_utils/setupTest';
 
 const test = setupTestContext(() => ({}));
 test('useArrayDiff tests', (t) => {
