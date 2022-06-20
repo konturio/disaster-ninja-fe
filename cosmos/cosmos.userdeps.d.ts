8bd5d195c911545647b3a531a646399f9f0e2f2d
Pavel Ropakov <propakov@kontur.io>
2022-06-20 13:29:20 +0300

    cosmos run + export

---
+++ cosmos/cosmos.userdeps.d.ts
@@ -0,0 +1,9 @@
+import type {
+  ReactDecoratorsByPath,
+  ReactFixtureExportsByPath,
+} from 'react-cosmos-shared2/react';
+import type { DomRendererConfig } from 'react-cosmos/dist/shared/rendererConfig';
+
+export const rendererConfig: DomRendererConfig;
+export const fixtures: ReactFixtureExportsByPath;
+export const decorators: ReactDecoratorsByPath;
