760aa1ea3b46ae030de7a325ad5c9e8fab3524c2
Alexander CherryTea <adubinin@kontur.io>
2022-07-07 18:34:13 +0200

    fix bug: event id is removed from request for app with limited ammount of features (#48)
    * fix bug when event id from url was removed
    in case when event list disabled as feature
    
    * change logick of auto select
    We have more cases when auto select unnesasary.
    Because of this I decide run autoselect only if explicitly required
    
    this commit broke auto focus to event
    
    * fix autofocus
    I use the very same logick as for autoselect
    
    * fix: app not loaded without pararmters in url

--- src/features/current_event/index.tsx
+++ src/features/current_event/index.tsx
@@ -1,9 +1,12 @@
+import { forceRun } from '~utils/atoms/forceRun';
 import { currentEventGeometryAtom } from './atoms/currentEventGeometry';
 import { currentEventRefresherAtom } from './atoms/currentEventRefresher';
 import { currentEventAutoFocusAtom } from './atoms/currentEventAutoFocus';
 
 export function initCurrentEvent() {
-  currentEventGeometryAtom.subscribe((val) => null);
-  currentEventRefresherAtom.subscribe(() => null);
-  currentEventAutoFocusAtom.subscribe(() => null);
+  forceRun([
+    currentEventGeometryAtom,
+    currentEventRefresherAtom,
+    currentEventAutoFocusAtom,
+  ]);
 }
