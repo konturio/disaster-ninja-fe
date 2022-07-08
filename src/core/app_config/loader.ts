f883b2fc77599e11cd2cf623b200747dad83b940
ilyaizr <ilyaizrailyan@gmail.com>
2022-07-08 13:34:58 +0400

    config fix for report table page

--- src/core/app_config/loader.ts
+++ src/core/app_config/loader.ts
@@ -1,5 +1,9 @@
 export async function loadConfig() {
-  const response = await fetch('config/appconfig.json');
+  const pathname = window.location.pathname;
+  const url = pathname.includes('/active/')
+    ? '/active/config/appconfig.json'
+    : '/config/appconfig.json';
+  const response = await fetch(url);
   // Here we can add runtime config check.
   // Example in scripts/build-config-scheme.mjs
   const config = await response.json();
