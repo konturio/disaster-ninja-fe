eee8605e391c88caa3937318d9f29c793cf2bbc3
ilyaizr <ilyaizrailyan@gmail.com>
2022-07-11 14:59:20 +0400

    review fix #1

--- src/core/app_config/loader.ts
+++ src/core/app_config/loader.ts
@@ -1,9 +1,7 @@
 export async function loadConfig() {
-  const pathname = window.location.pathname;
-  const url = pathname.includes('/active/')
-    ? '/active/config/appconfig.json'
-    : '/config/appconfig.json';
-  const response = await fetch(url);
+  const response = await fetch(
+    `${import.meta.env?.BASE_URL}config/appconfig.json`,
+  );
   // Here we can add runtime config check.
   // Example in scripts/build-config-scheme.mjs
   const config = await response.json();
