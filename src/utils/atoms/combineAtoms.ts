4ebdedd6baf731cff689d080caffd8eef05bb0f2
Alexandr Dubinin <akiyamka@gmail.com>
2022-07-18 10:49:42 +0200

    fix: lint error

--- src/utils/atoms/combineAtoms.ts
+++ src/utils/atoms/combineAtoms.ts
@@ -1,16 +1,16 @@
-import { Atom, createAtom } from '@reatom/core';
+import { createAtom } from '@reatom/core';
+import type { Atom } from '@reatom/core';
 
-type AtomsMap = { [key: string]: Atom<any> }
+type AtomsMap = { [key: string]: Atom<any> };
 
-export function combineAtoms<T extends AtomsMap >(
+export function combineAtoms<T extends AtomsMap>(
   shape: T,
-): Atom<{ [key in keyof T]: T[key] extends Atom<infer S> ? S : never }>
-{
+): Atom<{ [key in keyof T]: T[key] extends Atom<infer S> ? S : never }> {
   return createAtom(shape, ({ get }, state = {}) => {
     return Object.keys(shape).reduce((acc, key) => {
       // @ts-ignore
       acc[key] = get(key);
       return acc;
     }, {} as { [key in keyof T]: T[key] extends Atom<infer S> ? S : never });
-  })
+  });
 }
