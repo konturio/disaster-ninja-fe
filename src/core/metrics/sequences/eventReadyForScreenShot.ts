6a8e02d3141e6470884be2e267f34c47efaa7960
Alexandr Dubinin <akiyamka@gmail.com>
2022-07-20 18:11:43 +0200

    fix: event fireid to early We should wait event details request

--- src/core/metrics/sequences/eventReadyForScreenShot.ts
+++ src/core/metrics/sequences/eventReadyForScreenShot.ts
@@ -1,23 +1,27 @@
 import type { AppMetrics } from '../app-metrics';
 
-export function eventReadyForScreenShot(appMetrics: AppMetrics) {
-  appMetrics
+export function eventReadyForScreenShot(mtr: AppMetrics) {
+  mtr
     .addSequence('eventReadyForScreenShot')
-    .on(appMetrics.loaded('appConfig'))
-    .on(appMetrics.loaded('userResourceAtom'))
-    .on(appMetrics.loaded('feature.map'), (ctx, map: maplibregl.Map) => {
+    .on(mtr.loaded('appConfig'))
+    .on(mtr.loaded('userResourceAtom'))
+    .on(mtr.loaded('feature.map'), (ctx, map: maplibregl.Map) => {
       ctx.map = map;
     })
-    .on(appMetrics.loaded('areaLayersDetailsResourceAtom'), (ctx) => {
+    .on(mtr.loading('areaLayersDetailsResourceAtom'), (ctx, payload) => {
+      if (payload?.eventId === undefined) {
+        return { continueSq: false };
+      }
+    })
+    .on(mtr.loaded('areaLayersDetailsResourceAtom'), (ctx) => {
       ctx.map.loaded()
-        ? appMetrics.mark('mapIdle')
-        : ctx.map.once('idle', () => appMetrics.mark('mapIdle'));
+        ? setTimeout(() => mtr.mark('mapIdle')) // Here setTimeout because we want exit from this handler before process next handler
+        : ctx.map.once('idle', () => mtr.mark('mapIdle'));
     })
     .on('mapIdle', () => {
       setTimeout(() => {
         const eventReadyEvent = new Event('event_ready_for_screenshot');
         window.dispatchEvent(eventReadyEvent);
       }, 1000); // Mapbox have ~1000ms fadeIn animation after it report first 'idle'
-      return true;
     });
 }
