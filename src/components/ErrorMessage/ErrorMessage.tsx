931dac17a2f703a4092b954f74463196c41dec73
Alexandr Dubinin <akiyamka@gmail.com>
2022-07-21 13:29:10 +0200

    fix: not show backend errors on ui

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -6,9 +6,7 @@ export function ErrorMessage({ message }) {
   return (
     <div className={s.spinner}>
       <Text type="short-l">
-        {i18n.t(
-          message || 'Sorry, we are having issues, which will be fixed soon',
-        )}
+        {i18n.t('Sorry, we are having issues, which will be fixed soon')}
       </Text>
       <img src="assets/error-state-img.png" alt="" className={s.icon} />
     </div>
