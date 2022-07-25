9014d0213996b5a6b5ff5ea825e803d5d1366bd5
Alexandr Dubinin <akiyamka@gmail.com>
2022-07-25 18:58:03 +0200

    fix: proxy

--- src/utils/axios/replaceUrlWithProxy.ts
+++ src/utils/axios/replaceUrlWithProxy.ts
@@ -1,22 +1,20 @@
-const host = 'http://localhost:3000';
+const proxyHost = 'localhost:3000';
 
 /* Replace api url that require CORS to proxy */
 export function replaceUrlWithProxy(originalUrl: string): string {
   const viteProxyConfig = window['viteProxyConfig'];
   if (!viteProxyConfig) return originalUrl;
+
+  const associatedProxyTarget = Object.keys(viteProxyConfig).find((link) =>
+    originalUrl.includes(link),
+  );
+  if (!associatedProxyTarget) return originalUrl;
+
+  // Replace host in link with same link with proxy host
+  // Was: Client -> Server with cors
+  // Will: Client -> Dev server (-> Server with cors)
   try {
-    return Object.entries(viteProxyConfig).reduce(
-      (url, rule: [string, any]) => {
-        const proxyTarget =
-          typeof rule[1] === 'string' ? rule[1] : rule[1].target;
-        if (proxyTarget) {
-          return url.replace(String(proxyTarget), host);
-        } else {
-          return url;
-        }
-      },
-      originalUrl,
-    );
+    return originalUrl.replace(new URL(originalUrl).host, proxyHost);
   } catch (e: any) {
     return originalUrl;
   }
