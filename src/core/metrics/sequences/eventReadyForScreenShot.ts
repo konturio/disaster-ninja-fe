04c3fbf282876510ee396ec97865f4c312be3f99
Alexandr Dubinin <akiyamka@gmail.com>
2022-07-25 19:24:43 +0200

    debug: remove alert

--- src/core/metrics/sequences/eventReadyForScreenShot.ts
+++ src/core/metrics/sequences/eventReadyForScreenShot.ts
@@ -22,7 +22,6 @@ export function eventReadyForScreenShot(mtr: AppMetrics) {
     .on('mapIdle', () => {
       setTimeout(() => {
         const eventReadyEvent = new Event('event_ready_for_screenshot');
-        alert('ready');
         window.dispatchEvent(eventReadyEvent);
       }, 1000); // Mapbox have ~1000ms fadeIn animation after it report first 'idle'
     });
