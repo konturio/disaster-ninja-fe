d5bdfb5ff545516d81f8853ce5b377e428f70bbc
ilyaizr <ilyaizrailyan@gmail.com>
2022-08-01 20:14:07 +0400

    run google tag manager via tag module later in the app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom';
 import { reatomContext } from '@reatom/react';
 import { store } from '~core/store/store';
 import { AuthWrapper } from '~core/auth';
+import { runGoogleTagManager } from '~utils/metrics/tagManager';
 import { RoutedApp } from './Routes';
 
 ReactDOM.render(
@@ -12,3 +13,15 @@ ReactDOM.render(
   </reatomContext.Provider>,
   document.getElementById('root'),
 );
+
+// delayed run of statistics
+// 4 seconds is the time we'd expect app to be ready
+// TODO put this to when app is loaded when task #11042 is done
+if (import.meta.env?.MODE !== 'development')
+  try {
+    setTimeout(() => {
+      runGoogleTagManager();
+    }, 4000);
+  } catch (error) {
+    console.warn('error when loading statistics', error);
+  }
---
+++ src/utils/metrics/tagManager.ts
@@ -0,0 +1,5 @@
+import TagManager from 'react-gtm-module';
+
+export function runGoogleTagManager() {
+  TagManager.initialize({ gtmId: 'GTM-5LD2Z3D' });
+}
