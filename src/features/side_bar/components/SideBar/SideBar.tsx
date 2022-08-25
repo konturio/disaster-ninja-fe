22a049b17669a45e199e94ea65d8ef9868381f4e
ilyaizr <ilyaizrailyan@gmail.com>
2022-08-25 18:09:17 +0400

    cleanup commit # 3

--- src/features/side_bar/components/SideBar/SideBar.tsx
+++ src/features/side_bar/components/SideBar/SideBar.tsx
@@ -5,8 +5,6 @@ import { Link } from 'react-router-dom';
 import { modesControlsAtom } from '~core/modes/modesControls';
 import { APP_ROUTES } from '~core/app_config/appRoutes';
 import { MODES_LABELS } from '~core/modes/constants';
-import { controlsOrder } from '../../constants';
-import { sortByPredefinedOrder } from './sortByPredefinedOrder';
 import s from './SideBar.module.css';
 
 export function SideBar() {
@@ -14,28 +12,26 @@ export function SideBar() {
 
   return (
     <ActionsBar>
-      {sortByPredefinedOrder(Object.values(controls), controlsOrder).map(
-        (control) => {
-          return (
-            <Link
-              key={nanoid(4)}
-              className={s.sideBarContainer}
-              to={APP_ROUTES[control.id]}
-            >
-              <div className={s.buttonWrap} onClick={() => control.onClick()}>
-                <ActionsBarBTN
-                  active={control.active}
-                  iconBefore={control.icon}
-                  value={control.id}
-                  className={s.controlButton}
-                >
-                  <span className={s.modeName}>{MODES_LABELS[control.id]}</span>
-                </ActionsBarBTN>
-              </div>
-            </Link>
-          );
-        },
-      )}
+      {Object.values(controls).map((control) => {
+        return (
+          <Link
+            key={nanoid(4)}
+            className={s.sideBarContainer}
+            to={APP_ROUTES[control.id]}
+          >
+            <div className={s.buttonWrap} onClick={() => control.onClick()}>
+              <ActionsBarBTN
+                active={control.active}
+                iconBefore={control.icon}
+                value={control.id}
+                className={s.controlButton}
+              >
+                <span className={s.modeName}>{MODES_LABELS[control.id]}</span>
+              </ActionsBarBTN>
+            </div>
+          </Link>
+        );
+      })}
     </ActionsBar>
   );
 }
