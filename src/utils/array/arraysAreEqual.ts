30311143e27222b2c693c7138cd0d8627f16d887
Andrei Kucherov <kucherov.andrei@gmail.com>
2022-08-30 18:07:23 +0300

    chore: add more comments

--- src/utils/array/arraysAreEqual.ts
+++ src/utils/array/arraysAreEqual.ts
@@ -10,7 +10,7 @@ export function arraysAreEqualWithStrictOrder<T>(
 
 export function arraysAreEqual<T>(array1: T[], array2: T[]): boolean {
   return (
-    array1.length === array2.length &&
+    array1.length === array2.length && // length fastcheck to skip expensive shallow copying and sorting
     arraysAreEqualWithStrictOrder(array1.slice().sort(), array2.slice().sort())
   );
 }
