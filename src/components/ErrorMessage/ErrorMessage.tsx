c83cd196c17ec45add24e527da9489a79fd4bf75
Alexandr Dubinin <akiyamka@gmail.com>
2022-09-12 18:48:35 +0200

    configurable margin for placeholder components

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -3,13 +3,17 @@ import { i18n } from '~core/localization';
 import errorImage from './error-state-img.png';
 import s from './ErrorMessage.module.css';
 
-export function ErrorMessage({ message }) {
+export function ErrorMessage({
+  message,
+  marginTop = '30%',
+}: {
+  message?: string;
+  marginTop?: string;
+}) {
   return (
-    <div className={s.spinner}>
+    <div className={s.spinner} style={{ marginTop }}>
       <Text type="short-l">
-        {i18n.t(
-          message ?? 'Sorry, we are having issues, which will be fixed soon',
-        )}
+        {i18n.t(message ?? 'Sorry, we are having issues, which will be fixed soon')}
       </Text>
       <img src={errorImage} alt="" className={s.icon} />
     </div>
--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -5,11 +5,13 @@ import s from './LoadingSpinner.module.css';
 
 export function LoadingSpinner({
   message = i18n.t('spinner_text'),
+  marginTop = '30%',
 }: {
   message?: string;
+  marginTop?: string;
 }) {
   return (
-    <div className={s.spinner}>
+    <div className={s.spinner} style={{ marginTop: marginTop ?? 'inherit' }}>
       <Text type="short-l">{message}</Text>
       <div className={s.animationContainer}>
         <KonturSpinner />
