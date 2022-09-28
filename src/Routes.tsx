9228ff357502f39f3a91712ba5efc230cf605b9d
ilyaizr <ilyaizrailyan@gmail.com>
2022-09-28 16:44:33 +0400

    auth functions

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -68,7 +68,6 @@ export function RoutedApp() {
           )}
         </CommonRoutesFeatures>
       </Router>
-      <LoginForm />
     </StrictMode>
   );
 }
--- src/views/Profile/Profile.tsx
+++ src/views/Profile/Profile.tsx
@@ -1,12 +1,35 @@
+import { Button } from '@konturio/ui-kit';
+import { useAtom } from '@reatom/react';
+import { userStateAtom } from '~core/auth';
 import { LoginForm } from '~features/user_profile';
+import { authClientInstance } from '~core/authClientInstance';
+import s from './Profile.module.css';
 
 export function ProfileMode() {
   // login if no auth
   // Profile and Settings othervise
+  const [userState] = useAtom(userStateAtom);
+  function logout() {
+    authClientInstance.logout();
+  }
+
+  if (userState === 'authorized')
+    return (
+      <div className={s.modeWrap}>
+        <div className={s.profileWrap}>
+          <Button title="Logout" onClick={logout}>
+            Logout
+          </Button>
+        </div>
+        <div className={s.settingsWrap}></div>
+      </div>
+    );
+
   return (
-    <div>
-      Login h
-      <LoginForm />
+    <div className={s.modeWrap}>
+      <div className={s.loginWrap}>
+        <LoginForm />
+      </div>
     </div>
   );
 }
