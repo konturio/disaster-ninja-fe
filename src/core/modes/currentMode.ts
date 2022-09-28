80b92f3cdf5885c3ac8ba2f28471ca3f5b00eafd
VK <vkozel@kontur.io>
2022-09-28 20:37:57 +0300

    fix type

--- src/core/modes/currentMode.ts
+++ src/core/modes/currentMode.ts
@@ -1,12 +1,6 @@
 import { createAtom } from '~utils/atoms';
 
-export type ApplicationMode =
-  | 'map'
-  | 'reports'
-  | 'event'
-  | 'bivariateManager'
-  | 'about'
-  | 'profile';
+export type ApplicationMode = 'map' | 'about' | 'reports' | 'event' | 'bivariateManager';
 
 export const currentModeAtom = createAtom(
   {
