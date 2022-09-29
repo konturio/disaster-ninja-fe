9b89339036b2d715f83dba4424fc384c2ba0fada
Pavel Ropakov <propakov@kontur.io>
2022-09-29 11:04:27 +0300

    feat(bivariate-color-manager): 12095 - bivariate color manager moved to modes in sidebar

--- src/core/modes/constants.ts
+++ src/core/modes/constants.ts
@@ -5,5 +5,5 @@ export const MODES_LABELS: Record<ApplicationMode, string> = {
   map: i18n.t('modes.map'),
   event: i18n.t('modes.event'),
   reports: i18n.t('modes.reports'),
-  bivariateManager: i18n.t('bivariate.color_manager.title'),
+  bivariateManager: i18n.t('sidebar.biv_color_manager'),
 };
--- src/core/modes/initializeModes.ts
+++ src/core/modes/initializeModes.ts
@@ -1,8 +1,14 @@
+import { AppFeature } from '~core/auth/types';
+import { registerBivariateColorManagerMode } from './BivariateColorManagerMode';
 import { registerMapMode } from './MapMode';
 import { modesControlsAtom } from './modesControls';
 import { registerReportsMode } from './ReportsMode';
+import type { UserDataModel } from '~core/auth';
 
-export function initModes() {
+export function initModes(userModel?: UserDataModel | null) {
   registerMapMode(modesControlsAtom);
   registerReportsMode(modesControlsAtom);
+  if (userModel?.hasFeature(AppFeature.BIVARIATE_COLOR_MANAGER)) {
+    registerBivariateColorManagerMode(modesControlsAtom);
+  }
 }
