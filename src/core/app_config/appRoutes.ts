b8732ed18ba1a4ce7231950d4c93dcdcd400b558
VK <112831093+vkozio@users.noreply.github.com>
2022-10-04 14:25:44 +0300

    Update src/core/app_config/appRoutes.ts
    Co-authored-by: Pavel Ropakov <propakov@kontur.io>

--- src/core/app_config/appRoutes.ts
+++ src/core/app_config/appRoutes.ts
@@ -11,10 +11,9 @@ export const APP_ROUTES = {
 };
 
 export function findCurrentMode(path: string): ApplicationMode {
-  let mode: ApplicationMode = 'map';
-  if (path.indexOf('event') > -1) mode = 'event';
-  if (path.indexOf('reports') > -1) mode = 'reports';
-  if (path.indexOf('bivariate-manager') > -1) mode = 'bivariateManager';
-  if (path.indexOf('about') > -1) mode = 'about';
-  return mode;
+  if (path.includes('event')) return 'event';
+  if (path.includes('reports')) return 'reports';
+  if (path.includes('bivariate-manager')) return 'bivariateManager';
+  if (path.includes('about')) return 'about';
+  return 'map';
 }
