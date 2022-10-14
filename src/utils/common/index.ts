a903bf68dcbc1707839dac4c0cff0e93b367a318
ilyaizr <ilyaizrailyan@gmail.com>
2022-10-14 14:00:15 +0400

    add layouts, translations and loading logic

--- src/utils/common/index.ts
+++ src/utils/common/index.ts
@@ -22,3 +22,23 @@ export const isErrorWithMessage = (
 } => {
   return e !== null && typeof e === 'object' && 'message' in e;
 };
+
+type simpleObject = Record<string, string | boolean | number | undefined | null>;
+
+export function simpleObjectsAreEqual(
+  objectA: simpleObject | null,
+  objectB: simpleObject | null,
+) {
+  if (objectA === null || objectB === null) return false;
+  const aKeys = Object.keys(objectA);
+  const bKeys = Object.keys(objectB);
+  if (!aKeys.length && !bKeys.length) return true;
+  if (aKeys.length !== bKeys.length) return false;
+
+  for (let i = 0; i < aKeys.length; i++) {
+    const keyToCompare = aKeys[i];
+    if (!(keyToCompare in objectB)) return false;
+    if (objectA[keyToCompare] !== objectB[keyToCompare]) return false;
+  }
+  return true;
+}
--- src/views/Profile/atoms/userProfile.ts
+++ src/views/Profile/atoms/userProfile.ts
@@ -1,7 +1,10 @@
 import { apiClient } from '~core/apiClientInstance';
 import { i18n } from '~core/localization';
 import { currentUserAtom } from '~core/shared_state';
-import { createAtom, createResourceAtom } from '~utils/atoms';
+import { createAtom } from '~utils/atoms';
+import { createAsyncAtom } from '~utils/atoms/createAsyncAtom';
+import { createStringAtom } from '~utils/atoms/createPrimitives';
+import type { Action } from '@reatom/core';
 
 // Atoms we might need
 // userStateAtom
@@ -10,6 +13,29 @@ import { createAtom, createResourceAtom } from '~utils/atoms';
 // authClientInstance
 type currentProfile = {
   token?: string;
+  email?: string;
+  fullName?: string;
+  bio?: string;
+};
+
+const dummyUser: currentProfile = {
+  token: 'dumdudmdudn',
+  email: 'disasterNinja@kontur.io',
+  fullName: 'Daemon Ninjaki',
+  bio: 'I am the disaster',
+};
+
+const dummyResponse: profileGetResponse = {
+  email: 'disasterNinja@kontur.io',
+  fullName: 'Daemon Ninjaki',
+  bio: 'Disaster creator',
+  defaultFeed: 'kontur-public',
+  language: 'en',
+  osmEditor: 'josm',
+  subscribedToKonturUpdates: true,
+  theme: 'kontur',
+  useMetricUnits: true,
+  username: 'dummyUser',
 };
 
 type profileGetResponse = {
@@ -25,34 +51,81 @@ type profileGetResponse = {
   theme: string;
 };
 
+export type UserProfileState = {
+  username?: string;
+  email?: string;
+  fullName?: string;
+  language?: string;
+  useMetricUnits?: true;
+  subscribedToKonturUpdates?: true;
+  bio?: string;
+  osmEditor?: string;
+  defaultFeed?: string;
+  theme?: string;
+  loading?: boolean;
+};
+
+export const pageStatusAtom = createStringAtom<'init' | 'changed' | 'loading'>('init');
+
 export const currentProfileAtom = createAtom(
   {
     currentUserAtom,
+    setUser: (user: UserProfileState) => user,
+    getUserProfile: () => {
+      // noop
+    },
+    updateUserProfile: (user: UserProfileState) => user,
   },
-  ({ get }, state: currentProfile = {}) => {
-    const newState: currentProfile = {};
-    const currentUser = get('currentUserAtom');
+  ({ onChange, onAction, schedule, create }, state: UserProfileState | null = null) => {
+    onAction('getUserProfile', () => {
+      schedule(async (dispatch) => {
+        dispatch(pageStatusAtom.set('loading'));
+        // let responseData = await apiClient.get<profileGetResponse>(
+        //   '/users/current_user',
+        //   {},
+        //   true
+        // );
+        // temp dummy data
+        await new Promise((res) => {
+          setTimeout(() => {
+            res('');
+          }, 2000);
+        });
+        const responseData = dummyResponse;
+        // if (!responseData)
+        // throw new Error(i18n.t('no_data_received'));
+        // console.log('%c⧭', 'color: #f27999', responseData);
+        dispatch(create('setUser', responseData));
+      });
+    });
 
-    newState.token = currentUser.token;
-    state = newState;
-    // console.log('%c⧭ state'  , 'color: #00736b', state);
+    onAction('updateUserProfile', (user) => {
+      schedule(async (dispatch) => {
+        dispatch(pageStatusAtom.set('loading'));
+        // temp dummy data
+        await new Promise((res) => {
+          setTimeout(() => {
+            res('');
+          }, 2000);
+        });
+        dispatch(create('setUser', user));
+      });
+    });
+
+    onAction('setUser', (user) => {
+      pageStatusAtom.set('init');
+      state = user;
+    });
+
+    onChange('currentUserAtom', async (newUser, prevUser) => {
+      const actions: Action[] = [];
+      if (newUser.id === 'public') return actions.push(create('setUser', {}));
+
+      actions.push(create('getUserProfile'));
+      schedule((dispatch) => dispatch(actions));
+    });
+
+    // console.log('%c⧭ state', 'color: #00736b', state);
     return state;
   },
-);
-
-export const profileResourceAtom = createResourceAtom(
-  async (profile) => {
-    // TODO - change to createAsyncAtom
-    if (!profile) return null;
-    const responseData = await apiClient.get<profileGetResponse>(
-      '/users/current_user',
-      undefined,
-      true,
-    );
-    // console.log('%c⧭ responseData', 'color: #99614d', responseData);
-    if (responseData === undefined) throw new Error(i18n.t('no_data_received'));
-    return responseData;
-  },
-  'profileResourceAtom',
-  currentProfileAtom,
 );
