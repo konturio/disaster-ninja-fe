24f9d120a884c69a75167af5371f490c312eef64
Alexandr Dubinin <akiyamka@gmail.com>
2022-10-17 17:19:22 +0200

    fix path in windows

--- scripts/auto-link/config.mjs
+++ scripts/auto-link/config.mjs
@@ -10,7 +10,7 @@ export async function geConfig(pathToConfig = './scripts/auto-link/.path') {
     return { pathToUIKitRepo };
   } catch(e) {
     log.debug(e)
-    const pathToUIKitRepo = await prompts.askPathToFile('Path to folder with @kontur/ui repo');
+    const pathToUIKitRepo = await prompts.askPathToFile('Path to folder with @kontur/ui repo.\n Note that on windows system you should use double back slashes "\\" ');
     writeFileSync(pathToConfig, pathToUIKitRepo)
     return { pathToUIKitRepo };
   }
