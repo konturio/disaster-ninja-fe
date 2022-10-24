b20cd733e3c6b9506031e2b90a41c1aee3875a54
VK <vkozel@kontur.io>
2022-10-25 02:55:56 +0300

    modes: use title from registration in sidebar

--- src/core/modes/registrations/profileMode.tsx
+++ src/core/modes/registrations/profileMode.tsx
@@ -1,12 +1,14 @@
 import { User24 } from '@konturio/default-icons';
+import { useAtom } from '@reatom/react';
 import { i18n } from '~core/localization';
+import { userStateAtom } from '~core/auth/atoms/userState';
 import { currentModeAtom } from '../currentMode';
 import type { ModesControlsAtom } from '../modesControls';
 
 export function registerProfileMode(modesControlAtom: ModesControlsAtom) {
   modesControlAtom.addControl.dispatch({
     id: 'profile',
-    title: i18n.t('modes.profile'),
+    title: (<ATitle />) as unknown as string,
     active: false,
     icon: <User24 />,
     onClick() {
@@ -17,4 +19,11 @@ export function registerProfileMode(modesControlAtom: ModesControlsAtom) {
     },
     order: 20,
   });
+}
+
+function ATitle() {
+  const [userState] = useAtom(userStateAtom);
+  return userState === 'authorized'
+    ? i18n.t('modes.profile')
+    : i18n.t('login.login_button');
 }
--- src/features/side_bar/components/SideBar/SideBar.tsx
+++ src/features/side_bar/components/SideBar/SideBar.tsx
@@ -7,7 +7,6 @@ import { useCallback, useEffect, useState } from 'react';
 import { DoubleChevronLeft24, DoubleChevronRight24 } from '@konturio/default-icons';
 import { modesControlsAtom } from '~core/modes/modesControls';
 import { APP_ROUTES } from '~core/app_config/appRoutes';
-import { MODES_LABELS } from '~core/modes/constants';
 import { IS_MOBILE_QUERY, useMediaQuery } from '~utils/hooks/useMediaQuery';
 import { i18n } from '~core/localization';
 import { currentTooltipAtom } from '~core/shared_state/currentTooltip';
@@ -86,9 +85,7 @@ export function SideBar() {
                   value={control.id}
                   className={s.controlButton}
                 >
-                  {isOpen ? (
-                    <span className={s.modeName}>{MODES_LABELS[control.id]}</span>
-                  ) : null}
+                  {isOpen ? <span className={s.modeName}>{control.title}</span> : null}
                 </ActionsBarBTN>
               </div>
             </Link>
