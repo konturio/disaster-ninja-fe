994d8964d3a71f89b6816f0b91e096ab727273cb
VK <vkozel@kontur.io>
2022-10-27 14:07:23 +0300

    add debug flags

--- src/core/store/store.ts
+++ src/core/store/store.ts
@@ -2,6 +2,11 @@ import { createStore } from '@reatom/core';
 import { createDevtoolsLogger } from '~utils/debug/reatom-redux-devtools';
 import { appMetrics } from '~core/metrics';
 
+// enable with localStorage.setItem('KONTUR_DEBUG', 'true')
+const KONTUR_DEBUG = !!localStorage.getItem('KONTUR_DEBUG');
+// enable with localStorage.setItem('KONTUR_TRACE_ERROR', 'true')
+const KONTUR_TRACE_ERROR = !!localStorage.getItem('KONTUR_TRACE_ERROR');
+
 function configureStore() {
   const devtoolsLogger = createDevtoolsLogger();
   // Must be cutted out in production by terser
@@ -18,6 +23,14 @@ function configureStore() {
         for (const action of t.actions) {
           if (!action.type.includes('invalidate')) {
             appMetrics.processEvent(action.type, action.payload);
+            if (KONTUR_TRACE_ERROR) {
+              if (action.type.includes('_error')) {
+                console.trace('ERROR IN:', action.type, t);
+              }
+            }
+            if (KONTUR_DEBUG) {
+              console.debug(action.type, action.payload);
+            }
           }
         }
       }
--- src/features/user_profile/atoms/userProfile.ts
+++ src/features/user_profile/atoms/userProfile.ts
@@ -111,4 +111,5 @@ export const currentProfileAtom = createAtom(
 
     return state;
   },
+  'currentProfileAtom',
 );
