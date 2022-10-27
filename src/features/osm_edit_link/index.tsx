b3e69eb69881f42457060db84ce467981ad55562
IlyaIzr <50058726+IlyaIzr@users.noreply.github.com>
2022-10-27 12:42:06 +0300

    fix(osm_edit_link) - 12302: implement osm editor preference

--- src/features/osm_edit_link/index.tsx
+++ src/features/osm_edit_link/index.tsx
@@ -1,7 +1,12 @@
 import { EditOsm24 } from '@konturio/default-icons';
-import { currentMapPositionAtom, toolbarControlsAtom } from '~core/shared_state';
+import {
+  currentMapPositionAtom,
+  currentUserAtom,
+  toolbarControlsAtom,
+} from '~core/shared_state';
 import { controlGroup, controlVisualGroup } from '~core/shared_state/toolbarControls';
 import { i18n } from '~core/localization';
+import app_config from '~core/app_config';
 import { EDIT_IN_OSM_CONTROL_ID, EDIT_IN_OSM_CONTROL_NAME } from './constants';
 
 export function initOsmEditLink() {
@@ -21,7 +26,13 @@ export function initOsmEditLink() {
       const position = currentMapPositionAtom.getState();
       if (!position) return;
       const { lat, lng, zoom } = position;
-      const url = `https://www.openstreetmap.org/edit?#map=${zoom}/${lat}/${lng}`;
+      const { osmEditor } = currentUserAtom.getState();
+      if (!osmEditor) return;
+      const baseLink =
+        app_config.osmEditors.find((editor) => editor.id === osmEditor)?.url ||
+        'https://www.openstreetmap.org/edit?#map=';
+
+      const url = `${baseLink}${zoom}/${lat}/${lng}`;
       window.open(url)?.focus();
     },
   });
