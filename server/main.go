2d5ca314ef1d880ccd5b9a9c5f57252b1a798edc
Alexander Zapasnik <sanych27@gmail.com>
2022-11-03 10:02:27 +0100

    feat: 12133 Set up redirect from DN1 to DN2

--- server/main.go
+++ server/main.go
@@ -34,6 +34,15 @@ func renderTemplate(response http.ResponseWriter, request *http.Request) {
 	}
 }
 
+func redirectWithDn1Params(response http.ResponseWriter) {
+	parsedTemplate, _ := template.ParseFiles("./live/index.html")
+	err := parsedTemplate.Execute(response, nil)
+	if err != nil {
+		log.Println("Error executing live/index.html template:", err)
+		return
+	}
+}
+
 func handleStaticFiles(fs http.Handler) http.HandlerFunc {
 	return func(response http.ResponseWriter, request *http.Request) {
 		url := request.URL.Path
@@ -54,6 +63,8 @@ func redirect(response http.ResponseWriter, request *http.Request) {
 
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/live", http.StripPrefix("/live", http.HandlerFunc(redirectWithDn1Params)))
+	http.Handle("/live/", http.StripPrefix("/live/", http.HandlerFunc(redirectWithDn1Params)))
 	http.Handle("/active", http.StripPrefix("/active", http.HandlerFunc(redirect)))
 	http.Handle("/active/", http.StripPrefix("/active/", http.HandlerFunc(renderTemplate)))
 	http.Handle("/active/static/", http.StripPrefix("/active/static", http.HandlerFunc(handleStaticFiles(fs))))
