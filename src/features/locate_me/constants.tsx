38a431396d3826df104902a98dbce41f4854fdc1
ilyaizr <ilyaizrailyan@gmail.com>
2022-11-08 14:45:23 +0400

    implementation of feature

---
+++ src/features/locate_me/constants.tsx
@@ -0,0 +1,2 @@
+export const LOCATE_ME_CONTROL_ID = 'LocateMe';
+export const LOCATE_ME_CONTROL_NAME = 'Locate me feature';
---
+++ src/features/locate_me/index.tsx
@@ -0,0 +1,50 @@
+import { Locate24 } from '@konturio/default-icons';
+import {
+  currentMapPositionAtom,
+  currentNotificationAtom,
+  toolbarControlsAtom,
+} from '~core/shared_state';
+import { controlGroup, controlVisualGroup } from '~core/shared_state/toolbarControls';
+import { i18n } from '~core/localization';
+import { LOCATE_ME_CONTROL_ID, LOCATE_ME_CONTROL_NAME } from './constants';
+
+function successCb(location: GeolocationPosition) {
+  const { coords } = location;
+  const { latitude: lat, longitude: lng } = coords;
+
+  currentMapPositionAtom.setCurrentMapPosition.dispatch({ lat, lng, zoom: 14 });
+  disableControl();
+}
+
+function errorCb(error: GeolocationPositionError) {
+  currentNotificationAtom.showNotification.dispatch(
+    'warning',
+    { title: i18n.t('locate_me.get_location_error' + ': ' + error.message) },
+    3,
+  );
+  disableControl();
+}
+
+export function initLocateMe() {
+  toolbarControlsAtom.addControl.dispatch({
+    id: LOCATE_ME_CONTROL_ID,
+    name: LOCATE_ME_CONTROL_NAME,
+    title: i18n.t('locate_me.feature_title'),
+    active: false,
+    visualGroup: controlVisualGroup.noAnalytics,
+    exclusiveGroup: controlGroup.mapTools,
+    icon: <Locate24 />,
+    onClick: () => {
+      toolbarControlsAtom.enable.dispatch(LOCATE_ME_CONTROL_ID);
+      const geolocation = navigator.geolocation;
+      // Location dialogue should appear for the user
+      geolocation.getCurrentPosition(successCb, errorCb);
+    },
+  });
+}
+
+function disableControl() {
+  setTimeout(() => {
+    toolbarControlsAtom.disable.dispatch(LOCATE_ME_CONTROL_ID);
+  }, 3000);
+}
