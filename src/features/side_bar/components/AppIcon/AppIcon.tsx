5c469408f84defd40ede0b02bc11097060572f07
ilyaizr <ilyaizrailyan@gmail.com>
2022-11-15 14:37:42 +0400

    remove fallback icon and add alt for icon <img/>

--- src/features/side_bar/components/AppIcon/AppIcon.tsx
+++ src/features/side_bar/components/AppIcon/AppIcon.tsx
@@ -1,10 +1,18 @@
-import { Ninja24 } from '@konturio/default-icons';
 import { useAtom } from '@reatom/react';
+import { i18n } from '~core/localization';
 import { currentAppPropertiesResourceAtom } from '~core/shared_state/currentApplication';
 
 export function SidebarAppIcon() {
   const [{ data: appParams }] = useAtom(currentAppPropertiesResourceAtom);
-  const appIcon = <img src={appParams?.sidebarIconUrl} width={24} height={24} />;
-  const asyncIcon = appParams?.sidebarIconUrl ? appIcon : <Ninja24 />;
+
+  const appIcon = (
+    <img
+      src={appParams?.sidebarIconUrl}
+      width={24}
+      height={24}
+      alt={i18n.t('sidebar.icon_alt')}
+    />
+  );
+  const asyncIcon = appParams?.sidebarIconUrl ? appIcon : null;
   return asyncIcon;
 }
