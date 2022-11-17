bdce7553cec6d7ebf7cff6ad29fd1c4b7acd4c0f
Alexandr Dubinin <akiyamka@gmail.com>
2022-11-17 19:02:26 +0100

    remove language cycled deps

--- src/core/localization/index.ts
+++ src/core/localization/index.ts
@@ -1,5 +1,2 @@
 export { TranslationService as i18n } from './TranslationService';
 export { LocaleNumber } from './Locale';
-import { forceRun } from '~utils/atoms/forceRun';
-import { languageWatcherAtom } from './atoms/languageWatcher';
-forceRun(languageWatcherAtom);
