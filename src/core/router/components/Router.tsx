52b7888ec7830e932501b62786a62ce6dd8aed2d
Alexandr Dubinin <akiyamka@gmail.com>
2022-11-17 12:27:50 +0100

    resolve circular deps with router

--- src/core/router/components/Router.tsx
+++ src/core/router/components/Router.tsx
@@ -1,17 +1,14 @@
 import { CacheSwitch } from 'react-router-cache-route';
-import { Router as ReactRouter, Route, Redirect } from 'react-router-dom';
+import { Router as ReactRouter } from 'react-router-dom';
 import history from '~core/history';
-import { CommonRoutesFeatures } from '~views/Common';
 import { Routes } from './Routes';
 
 export function Router() {
   return (
     <ReactRouter history={history}>
-      <CommonRoutesFeatures>
-        <CacheSwitch>
-          <Routes />
-        </CacheSwitch>
-      </CommonRoutesFeatures>
+      <CacheSwitch>
+        <Routes />
+      </CacheSwitch>
     </ReactRouter>
   );
 }
