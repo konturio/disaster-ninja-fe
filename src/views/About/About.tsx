bd9189f1baf888a75a527acf643e2ed593c097b1
Pavel Ropakov <propakov@kontur.io>
2022-12-06 17:07:23 +0300

    fix(i18n) - fix spacebar in about section

--- src/views/About/About.tsx
+++ src/views/About/About.tsx
@@ -88,7 +88,7 @@ export function AboutPage({ toHomePage }: { toHomePage: () => void }) {
           <Trans i18nKey="about.p6">
             We hope you find this tool valuable. Use the chatbox on Disaster Ninja for any
             questions about the functionality, and we will be happy to guide you. You can
-            also contact us by email at
+            also contact us by email at{' '}
             <a href="mailto:hello@kontur.io">hello@kontur.io</a> if you have feedback or
             suggestions on improving the tool.
             <br />
