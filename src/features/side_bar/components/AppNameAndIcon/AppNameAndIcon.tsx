9b716212dd482d49d479cc53c39cfd13e2a1dc95
IlyaIzr <50058726+IlyaIzr@users.noreply.github.com>
2022-12-14 12:30:44 +0300

    fix(sidebar): 13451 - also define app name by app id and change icon in tab (#285)

---
+++ src/features/side_bar/components/AppNameAndIcon/AppNameAndIcon.tsx
@@ -0,0 +1,32 @@
+import { ActionsBarBTN } from '@konturio/ui-kit';
+import { useAtom } from '@reatom/react';
+import app_config from '~core/app_config';
+import { i18n } from '~core/localization';
+import { currentAppPropertiesResourceAtom } from '~core/shared_state/currentApplication';
+import { transformIconLink } from '~utils/common';
+
+type AppNameAndIconProps = {
+  wrapClassName?: string;
+  appNameClassName?: string;
+  isOpen: boolean;
+};
+
+export function AppNameAndIcon({
+  isOpen,
+  appNameClassName,
+  wrapClassName,
+}: AppNameAndIconProps) {
+  const [{ data: appParams }] = useAtom(currentAppPropertiesResourceAtom);
+  const iconPath =
+    appParams?.sidebarIconUrl && transformIconLink(appParams?.sidebarIconUrl);
+
+  const appIcon = iconPath ? (
+    <img src={iconPath} width={24} height={24} alt={i18n.t('sidebar.icon_alt')} />
+  ) : null;
+
+  return (
+    <ActionsBarBTN active={false} iconBefore={appIcon} className={wrapClassName}>
+      {isOpen ? <span className={appNameClassName}>{appParams?.name}</span> : null}
+    </ActionsBarBTN>
+  );
+}
