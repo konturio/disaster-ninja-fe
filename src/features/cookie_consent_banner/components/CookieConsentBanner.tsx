2c5d15df400e9f01f9fbc1cf68db25aa2698ed5c
Alexandr Dubinin <akiyamka@gmail.com>
2022-12-15 17:36:50 +0100

    feature flag check added

--- src/features/cookie_consent_banner/components/CookieConsentBanner.tsx
+++ src/features/cookie_consent_banner/components/CookieConsentBanner.tsx
@@ -1,13 +1,18 @@
 import { Button, Card, Heading, Animation } from '@konturio/ui-kit';
 import { useAtom } from '@reatom/react';
 import ReactMarkdown from 'react-markdown';
+import { userResourceAtom } from '~core/auth';
+import { AppFeature } from '~core/auth/types';
 import { i18n } from '~core/localization';
 import { cookieSettingsAtom } from '../atoms/cookieSettingsAtom';
 import s from './CookieConsentBanner.module.css';
 
 export function CookieConsentBanner() {
   const [havePrompts, { acceptAll, rejectAll }] = useAtom(cookieSettingsAtom);
-
+  const [{ data, loading }] = useAtom(userResourceAtom);
+  const userModel = data && !loading ? data : null;
+  // FIXME: Since this banner also blocks intercom in should check something more common
+  if (!userModel?.hasFeature(AppFeature.USE_3RDPARTY_ANALYTICS)) return null;
   return havePrompts ? (
     <Animation variant="fade-in">
       <Card className={s.cookieBanner}>
