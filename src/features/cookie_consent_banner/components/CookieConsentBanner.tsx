51f29dd37a23ed959e2ea3f9d0a91e6a300bf418
Alexandr Dubinin <akiyamka@gmail.com>
2022-12-21 12:39:23 +0100

    fix(cookie_consent_banner):  privacy policy link in banner

--- src/features/cookie_consent_banner/components/CookieConsentBanner.tsx
+++ src/features/cookie_consent_banner/components/CookieConsentBanner.tsx
@@ -1,9 +1,30 @@
-import { Button, Card, Heading, Animation } from '@konturio/ui-kit';
-import { useAtom } from '@reatom/react';
+import { memo } from 'react';
 import ReactMarkdown from 'react-markdown';
+import { useAtom } from '@reatom/react';
+import { Link } from 'react-router-dom';
+import { Button, Card, Heading, Animation } from '@konturio/ui-kit';
+import { getAbsoluteRoute } from '~core/router';
 import { i18n } from '~core/localization';
 import { cookieSettingsAtom } from '../atoms/cookieSettingsAtom';
 import s from './CookieConsentBanner.module.css';
+
+type ElementWrapProps = React.DetailedHTMLProps<
+  React.AnchorHTMLAttributes<HTMLAnchorElement>,
+  HTMLAnchorElement
+>;
+
+const InternalLinkAdapter = (props: ElementWrapProps) => {
+  if (props.href === undefined) return <a {...props} />;
+
+  const isExternalLink = props.href?.startsWith('http') || props.href?.startsWith('www');
+  return isExternalLink ? (
+    <a href={props.href} target="_blank" rel="noreferrer">
+      {props.children}
+    </a>
+  ) : (
+    <Link to={getAbsoluteRoute(props.href)}> {props.children}</Link>
+  );
+};
 
 export function CookieConsentBanner() {
   const [havePrompts, { acceptAll, rejectAll }] = useAtom(cookieSettingsAtom);
@@ -12,7 +33,9 @@ export function CookieConsentBanner() {
       <Card className={s.cookieBanner}>
         <Heading type="heading-04">{i18n.t('cookie_banner.header')}</Heading>
         <div className={s.body}>
-          <ReactMarkdown>{i18n.t('cookie_banner.body')}</ReactMarkdown>
+          <ReactMarkdown components={{ a: memo(InternalLinkAdapter) }}>
+            {i18n.t('cookie_banner.body')}
+          </ReactMarkdown>
         </div>
         <div className={s.buttonsRow}>
           <Button variant="invert-outline" onClick={rejectAll}>
