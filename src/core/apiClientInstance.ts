fdc67234445113f86614f1b33118ad9a227c26b3
ilyaizr <ilyaizrailyan@gmail.com>
2022-12-23 15:45:40 +0400

    logout to unauthorizedCallback

--- src/core/apiClientInstance.ts
+++ src/core/apiClientInstance.ts
@@ -1,8 +1,10 @@
 import { ApiClient } from './api_client';
+import app_config from './app_config';
 import config from './app_config';
-import { authClientInstance } from './authClientInstance';
+import { userStateAtom } from './auth';
 import { i18n } from './localization';
 import { notificationServiceInstance } from './notificationServiceInstance';
+import { currentUserAtom } from './shared_state';
 
 // initialize main api client
 ApiClient.init({
@@ -12,8 +14,19 @@ ApiClient.init({
   refreshTokenApiPath: `${config.keycloakUrl}/auth/realms/${config.keycloakRealm}/protocol/openid-connect/token`,
   translationService: i18n,
   unauthorizedCallback(apiClient) {
+    // TODO refactor: same pice of logic exists in AuthClient.logout()
     apiClient.logout();
-    authClientInstance.logout();
+    currentUserAtom.setUser.dispatch();
+    userStateAtom.logout.dispatch();
+
+    if (window['Intercom']) {
+      app_config.intercom.name = window.konturAppConfig.INTERCOM_DEFAULT_NAME;
+      app_config.intercom['email'] = null;
+      window['Intercom']('update', {
+        name: app_config.intercom.name,
+        email: app_config.intercom['email'],
+      });
+    }
   },
 });
 const apiClientInstance = ApiClient.getInstance();
