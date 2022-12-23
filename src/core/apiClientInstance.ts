62f6ca5c8952fc43337cdd4a0548e889ddbd1cc4
IlyaIzr <50058726+IlyaIzr@users.noreply.github.com>
2022-12-23 12:16:14 +0300

    fix(core): 13914 - reset user token after failed checkAuth() method in AuthClient (#326)
    * reset user token after failed checkAuth() method in AuthClient
    
    * logout when 401 or 403 instead
    
    * restore pre-push
    
    * restore auth client changes
    
    * restore auth client
    
    * revert pre-push

--- src/core/apiClientInstance.ts
+++ src/core/apiClientInstance.ts
@@ -10,6 +10,9 @@ ApiClient.init({
   loginApiPath: `${config.keycloakUrl}/auth/realms/${config.keycloakRealm}/protocol/openid-connect/token`,
   refreshTokenApiPath: `${config.keycloakUrl}/auth/realms/${config.keycloakRealm}/protocol/openid-connect/token`,
   translationService: i18n,
+  unauthorizedCallback(apiClient) {
+    apiClient.logout();
+  },
 });
 const apiClientInstance = ApiClient.getInstance();
 
