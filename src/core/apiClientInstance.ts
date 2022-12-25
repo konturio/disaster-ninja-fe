f3eef8ba92e7600f7929e58ee82aea232d57d1f2
ilyaizr <ilyaizrailyan@gmail.com>
2022-12-25 16:45:42 +0400

    simplify ApiClient initialization to avoid circular deps

--- src/core/apiClientInstance.ts
+++ src/core/apiClientInstance.ts
@@ -1,10 +1,7 @@
 import { ApiClient } from './api_client';
-import app_config from './app_config';
 import config from './app_config';
-import { userStateAtom } from './auth';
 import { i18n } from './localization';
 import { notificationServiceInstance } from './notificationServiceInstance';
-import { currentUserAtom } from './shared_state';
 
 // initialize main api client
 ApiClient.init({
@@ -14,21 +11,11 @@ ApiClient.init({
   refreshTokenApiPath: `${config.keycloakUrl}/auth/realms/${config.keycloakRealm}/protocol/openid-connect/token`,
   translationService: i18n,
   unauthorizedCallback(apiClient) {
-    // TODO refactor: same pice of logic exists in AuthClient.logout()
     apiClient.logout();
-    currentUserAtom.setUser.dispatch();
-    userStateAtom.logout.dispatch();
-
-    if (window['Intercom']) {
-      app_config.intercom.name = window.konturAppConfig.INTERCOM_DEFAULT_NAME;
-      app_config.intercom['email'] = null;
-      window['Intercom']('update', {
-        name: app_config.intercom.name,
-        email: app_config.intercom['email'],
-      });
-    }
+    apiClient.expiredTokenCallback?.();
   },
 });
+
 const apiClientInstance = ApiClient.getInstance();
 
 export const apiClient = apiClientInstance;
