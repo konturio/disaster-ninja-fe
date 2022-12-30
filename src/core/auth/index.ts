29b5fc412b6cf5592829452e879ee868c4930345
VK <vkozel@kontur.io>
2022-12-30 08:52:32 +0300

    remove AuthWrapper

--- src/core/auth/index.ts
+++ src/core/auth/index.ts
@@ -1,5 +1,4 @@
 export { AuthClient } from './client/AuthClient';
 export { userStateAtom } from './atoms/userState';
-export { AuthWrapper } from './components/AuthWrapper/AuthWrapper';
 export { UserStateToComponents } from './components/UserStateToComponents/UserStateToComponents';
 export { landUser } from './atoms/userWasLanded';
