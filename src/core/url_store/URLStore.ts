6338bf14f1d3632af340b4a29e20352675dcd1f1
VK <112831093+vkozio@users.noreply.github.com>
2023-01-18 12:57:09 +0300

    fix(core): 13914 Reset login state if getting 401 from the server (#388)
    * auth flow and api client fixes
    
    * review fix

--- src/core/url_store/URLStore.ts
+++ src/core/url_store/URLStore.ts
@@ -1,5 +1,4 @@
 import { apiClient } from '~core/apiClientInstance';
-import { appConfig } from '~core/app_config';
 import type { ApiClient } from '~core/api_client';
 import type { UrlData } from './types';
 
@@ -15,59 +14,9 @@ export class URLStore {
   _encoder: UrlEncoder<UrlData>;
   _client: ApiClient;
 
-  getDefaults = {
-    app: async () => {
-      try {
-        return await this._client.get<string>('/apps/default_id');
-      } catch (e) {
-        console.error('[URLStore]: Failed to get default app id');
-        console.debug(e);
-        // if auth error(or bad token) was the reason to not to get appId - make it without it anyway
-        return await this._client.get<string>('/apps/default_id', undefined, false);
-      }
-    },
-    layers: async (appId: string) => {
-      // ! TEMPORARY MOCK
-      // Backend right not can't send as layers that not in layers API,
-      // So we should wait until all layers migrated to Layers API
-      // Until that moment we take static list of default layers
-      // * That similar for all applications *
-      // Ask Alexander Zapasnik before remove this
-      if (appConfig.layersByDefault) {
-        return new Promise<string[]>((res) => {
-          res(appConfig.layersByDefault);
-        });
-      } else {
-        const layers = await this._client.get<{ id: string }[] | null>(
-          `/apps/${appId}/layers/`,
-          undefined,
-          false,
-        );
-        return layers?.map((l) => l.id) ?? null;
-      }
-    },
-  };
   constructor(encoder: UrlEncoder<UrlData>) {
     this._encoder = encoder;
     this._client = apiClient;
-  }
-
-  async getInitialState() {
-    const initialUrl = this.readCurrentState();
-
-    if (initialUrl.app === undefined) {
-      const appId = await this.getDefaults.app();
-      if (appId) initialUrl.app = appId;
-      // TODO if (!appId) the app won't go past global loading state
-    }
-
-    if (initialUrl.layers === undefined && initialUrl.app) {
-      const layers = await this.getDefaults.layers(initialUrl.app);
-      if (layers) initialUrl.layers = layers;
-    }
-
-    this.updateUrl(initialUrl);
-    return initialUrl;
   }
 
   readCurrentState() {
