617536f0b2ecd60a27abd393fd7c647913197f9e
Pavel Ropakov <propakov@kontur.io>
2023-02-03 17:09:03 +0300

    code review fix: cache structure change: hash: boolean -> hash: Map

--- src/features/layers_in_area/atoms/areaLayersDetailsResource/areaLayersDetailsResourceAtomCache.ts
+++ src/features/layers_in_area/atoms/areaLayersDetailsResource/areaLayersDetailsResourceAtomCache.ts
@@ -8,7 +8,7 @@ type Hash = string;
 type LayerId = string;
 type AreaLayersDetailsResourceAtomCache = Map<
   EventId | Hash,
-  Map<LayerId, LayerInAreaDetails> | boolean
+  Map<LayerId, LayerInAreaDetails>
 >;
 
 const createDefaultCacheState = (state?) => {
@@ -20,7 +20,7 @@ const createDefaultCacheState = (state?) => {
 Store all prev request
 Cache structure: 
   Map 
-  - key: focusedGeometry hash, value: true | For layer with boundaryRequiredForRetrieval=true flag
+  - key: focusedGeometry hash, value: Map | For layer with boundaryRequiredForRetrieval=true flag
   - key: eventId, value: Map | For layer with eventIdRequiredForRetrieval=true flag
   - key: null, value: Map | For layer with eventIdRequiredForRetrieval=false flag
 */
@@ -45,28 +45,39 @@ export const areaLayersDetailsResourceAtomCache = createAtom(
         request.layersToRetrieveWithGeometryFilter,
       );
       response.forEach((layer) => {
-        if (layersToRetrieveWithGeometryFilter.has(layer.id)) {
-          const geometryHash = request?.geoJSON?.hash;
-          if (geometryHash && !state.get(geometryHash)) {
-            state.set(geometryHash, true);
-          }
-          return state;
-        }
+        const cacheKey: string | null = getLayersDetailsCacheKey({
+          boundaryRequiredForRetrieval: layersToRetrieveWithGeometryFilter.has(layer.id),
+          eventIdRequiredForRetrieval: layersToRetrieveWithEventId.has(layer.id),
+          eventId: request.eventId,
+          hash: request.geoJSON?.hash,
+        });
 
-        const eventId = layersToRetrieveWithEventId.has(layer.id)
-          ? request.eventId ?? null
-          : null;
-        if (!state.get(eventId)) {
-          state.set(eventId, new Map());
+        if (!state.get(cacheKey)) {
+          state.set(cacheKey, new Map());
         }
-
-        const cacheValue = state.get(eventId);
-        if (cacheValue instanceof Map) {
-          cacheValue.set(layer.id, layer);
-        }
+        state.get(cacheKey)!.set(layer.id, layer);
       });
     });
     return state;
   },
   'areaLayersDetailsResourceAtomCache',
 );
+
+export const getLayersDetailsCacheKey = ({
+  boundaryRequiredForRetrieval,
+  eventIdRequiredForRetrieval,
+  eventId,
+  hash,
+}: {
+  boundaryRequiredForRetrieval?: boolean;
+  eventIdRequiredForRetrieval?: boolean;
+  eventId?: string | null;
+  hash?: string;
+}): string | null => {
+  if (boundaryRequiredForRetrieval && hash) {
+    return hash;
+  } else if (eventIdRequiredForRetrieval && eventId) {
+    return eventId;
+  }
+  return null;
+};
