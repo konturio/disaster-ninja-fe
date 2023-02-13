2d77a818de23adc6ba6b23e30c897fe1a4c5d3cf
VK <vkozel@kontur.io>
2023-02-13 16:25:49 +0300

    cleanup

--- src/core/logical_layers/renderers/BivariateRenderer.tsx
+++ src/core/logical_layers/renderers/BivariateRenderer.tsx
@@ -1,5 +1,4 @@
 import throttle from 'lodash-es/throttle';
-import isNil from 'lodash-es/isNil';
 import { h3ToGeoBoundary, geoToH3 } from 'h3-js';
 import { Popup as MapPopup } from 'maplibre-gl';
 import { createRoot } from 'react-dom/client';
