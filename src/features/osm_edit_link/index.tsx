e23f4498569b834d1df63b9f82681dd74cfe2cf6
IlyaIzr <50058726+IlyaIzr@users.noreply.github.com>
2023-02-22 21:43:40 +0300

    doc(osm_edit_link): 13742 - create doc for osm_edit_link (#470)

--- src/features/osm_edit_link/index.tsx
+++ src/features/osm_edit_link/index.tsx
@@ -8,7 +8,11 @@ import { controlGroup, controlVisualGroup } from '~core/shared_state';
 import { i18n } from '~core/localization';
 import { appConfig } from '~core/app_config';
 import { URL_ZOOM_OFFSET } from '~core/constants';
-import { EDIT_IN_OSM_CONTROL_ID, EDIT_IN_OSM_CONTROL_NAME } from './constants';
+import {
+  DISABLE_CONTROL_TIMEOUT,
+  EDIT_IN_OSM_CONTROL_ID,
+  EDIT_IN_OSM_CONTROL_NAME,
+} from './constants';
 
 export function initOsmEditLink() {
   toolbarControlsAtom.addControl.dispatch({
@@ -23,12 +27,15 @@ export function initOsmEditLink() {
       toolbarControlsAtom.enable.dispatch(EDIT_IN_OSM_CONTROL_ID);
       setTimeout(() => {
         toolbarControlsAtom.disable.dispatch(EDIT_IN_OSM_CONTROL_ID);
-      }, 3000);
+      }, DISABLE_CONTROL_TIMEOUT);
+
       const position = currentMapPositionAtom.getState();
       if (!position) return;
+
       const { lat, lng, zoom } = position;
       const { osmEditor } = currentUserAtom.getState();
       if (!osmEditor) return;
+
       const baseLink =
         appConfig.osmEditors.find((editor) => editor.id === osmEditor)?.url ||
         'https://www.openstreetmap.org/edit?#map=';
