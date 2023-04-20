adb8a77d6dd0b210e9eef1cbb5931f018a559b43
Alexandr Dubinin <akiyamka@gmail.com>
2023-04-20 16:01:45 +0200

    fix types errors

--- src/features/live_sensor/AppSensorsController.ts
+++ src/features/live_sensor/AppSensorsController.ts
@@ -4,18 +4,19 @@ import type { Constructor } from './types';
 export class AppSensorsController<S extends Array<Constructor<AppSensor>>> {
   mainSensor: AppSensor;
   sensors: Array<AppSensor>;
-  settings: { mainSensorRefreshRate: number };
+  settings?: { mainSensorRefreshRate: number };
   /**
    * Main sensor - first sensors in sensors array
    * @param sensors - sensors
    **/
-  constructor(sensors: S, settings: { mainSensorRefreshRate: number }) {
+  constructor(sensors: S, settings?: { mainSensorRefreshRate: number }) {
     this.settings = settings;
-    this.sensors = sensors.map((Sensor, i) =>
-      i === 0
-        ? new Sensor({ timeout: this.settings.mainSensorRefreshRate })
-        : new Sensor(),
-    );
+    this.sensors = sensors.map((Sensor, i) => {
+      if (this.settings?.mainSensorRefreshRate && i === 0) {
+        return new Sensor({ timeout: this.settings.mainSensorRefreshRate });
+      }
+      return new Sensor();
+    });
     const mainSensor = this.sensors.at(0);
     if (!mainSensor) {
       throw Error('At least one sensor needed');
