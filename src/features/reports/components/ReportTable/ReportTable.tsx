7a6cd200a0ab06b1db9e929a5b4ff1242e33ed93
Alexandr Dubinin <akiyamka@gmail.com>
2023-06-06 14:16:54 +0200

    stream render for tables

--- src/features/reports/components/ReportTable/ReportTable.tsx
+++ src/features/reports/components/ReportTable/ReportTable.tsx
@@ -1,7 +1,6 @@
 import { useAtom } from '@reatom/react';
 import { Heading } from '@konturio/ui-kit';
-import { useVirtual } from 'react-virtual';
-import { useRef } from 'react';
+import { memo, useEffect, useRef, useState, useTransition } from 'react';
 import { i18n } from '~core/localization';
 import { currentReportAtom } from '../../atoms/reportResource';
 import { tableAtom } from '../../atoms/tableAtom';
@@ -10,19 +9,69 @@ import styles from './ReportTable.module.css';
 import { TableCell } from './TableCell';
 import { PREFIX } from './constants';
 
+const Rows = memo(function Rows({ rows }: { rows: Array<any> }) {
+  return (
+    <>
+      {rows.map((row, rowIndex) => {
+        return (
+          <tr
+            key={row[0] + 'row' + rowIndex}
+            className={row[0].startsWith(PREFIX.subRow) ? styles.subRow : ''}
+          >
+            {row.map((cell, cellIndex) => (
+              <TableCell
+                key={row[0] + 'cell' + cellIndex}
+                cell={
+                  cell.startsWith(PREFIX.subRow)
+                    ? cell.substring(PREFIX.subRow.length)
+                    : cell
+                }
+              />
+            ))}
+          </tr>
+        );
+      })}
+    </>
+  );
+});
+
 export function ReportTable() {
   const [{ data, thead, ascending, sortIndex, isSorting }, { sortBy, setState }] =
     useAtom(tableAtom);
   const [meta] = useAtom(currentReportAtom);
 
-  const tableContainerRef = useRef<HTMLDivElement>(null);
-  const { virtualItems: virtualRows, totalSize } = useVirtual({
-    parentRef: tableContainerRef,
-    size: data?.length ?? 0,
-    overscan: 10,
-  });
+  const [isPending, startTransition] = useTransition();
+  const [dataStream, setDataStream] = useState<string[][]>([]);
+  const dataRef = useRef(data);
+  const pointerRef = useRef(0);
+  useEffect(() => {
+    if (!data || data.length === 0) {
+      dataRef.current = data;
+      pointerRef.current = 0;
+      startTransition(() => {
+        setDataStream([]);
+      });
+      return;
+    }
+    if (data !== dataRef.current) {
+      // Data changed
+      dataRef.current = data;
+      pointerRef.current = 0;
+    }
 
-  if (data === null) {
+    if (!isPending && pointerRef.current < data.length) {
+      const sliceSize = 10;
+      const slice = data.slice(0, pointerRef.current + sliceSize);
+      pointerRef.current += sliceSize;
+      startTransition(() => {
+        setDataStream(slice);
+      });
+    }
+  }, [data, isPending, dataStream]);
+
+  const tableContainerRef = useRef<HTMLDivElement>(null);
+
+  if (data === null || data === undefined) {
     return <Heading type="heading-01">{i18n.t('reports.no_data')}</Heading>;
   }
 
@@ -32,6 +81,9 @@ export function ReportTable() {
 
   return (
     <div ref={tableContainerRef}>
+      <div style={{ opacity: dataStream.length !== data.length ? 1 : 0 }}>
+        {`${dataStream.length} / ${data.length} rows loaded`}
+      </div>
       <table>
         <thead>
           <tr>
@@ -59,27 +111,7 @@ export function ReportTable() {
         </thead>
 
         <tbody>
-          {virtualRows.map((virtualRow) => {
-            const rowIndex = virtualRow.index;
-            const row = data![rowIndex];
-            return (
-              <tr
-                key={row[0] + 'row' + rowIndex}
-                className={row[0].startsWith(PREFIX.subRow) ? styles.subRow : ''}
-              >
-                {row.map((cell, cellIndex) => (
-                  <TableCell
-                    key={row[0] + 'cell' + cellIndex}
-                    cell={
-                      cell.startsWith(PREFIX.subRow)
-                        ? cell.substring(PREFIX.subRow.length)
-                        : cell
-                    }
-                  />
-                ))}
-              </tr>
-            );
-          })}
+          <Rows rows={dataStream ?? []} />
         </tbody>
       </table>
     </div>
