14c8510ed5e94eb6330c04238ac9267ea24c4df9
Alexander CherryTea <adubinin@kontur.io>
2023-06-08 11:34:07 +0200

    feat(reports): 15980 fe improve reports table (#543)
    * add support for multiple links
    + add virtualization for fast fix performance
    + fix proxy config for reports
    
    * parse csv in worker thread
    
    * fix: react-virtual work perfect with 18 react, but limited with 17 version
    
    * fix - remove _tab flag after check
    
    * stream render for tables
    
    * fix regexp
    
    * fix links

--- configs/proxy-config.default.js
+++ configs/proxy-config.default.js
@@ -1,12 +1,21 @@
 export default {
   /* Bivariate layers */
+  ['/active/reports']: {
+    target: 'https://disaster.ninja',
+    changeOrigin: true,
+  },
+  ['/active/api']: {
+    target: 'https://disaster.ninja',
+    changeOrigin: true,
+  },
+  /* Bivariate layers */
   ['/api/tiles/bivariate']: {
     target: 'https://disaster.ninja/active',
     changeOrigin: true,
   },
   /* For bivariate manager */
   '/tiles/stats': 'https://disaster.ninja',
-  /* For Active contributors  */
+  /* For Active contributors */
   '/tiles/users': 'https://disaster.ninja',
   /* Hot activations */
   '/tiles/public.hot_projects': 'https://disaster.ninja',
---
+++ src/features/reports/components/ReportTable/constants.ts
@@ -0,0 +1,6 @@
+export const PREFIX = {
+  subRow: 'subrow_',
+  href: 'href_',
+  hrefIcon: 'hrefIcon_',
+  tab: 'tab_',
+} as const;
