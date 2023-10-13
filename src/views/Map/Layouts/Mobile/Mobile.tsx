c457a6283db2ec114a271c9dc4ab5e9f51c6f583
Alexander CherryTea <adubinin@kontur.io>
2023-10-13 14:00:48 +0200

    fix(layout): 16393-center-disaster-geometry-by-map-without-panels-on-mobile-devices-2

--- src/views/Map/Layouts/Mobile/Mobile.tsx
+++ src/views/Map/Layouts/Mobile/Mobile.tsx
@@ -16,11 +16,8 @@ export function MobileLayout({
     <div className={s.contentWrap}>
       <div className={s.toolbox}>{drawToolbox}</div>
       <div className={s.panelsColumn}>{firstColumn}</div>
-      <div className={s.mapWrap}>
-        <div className={s.mapSpaceRight} id={appConfig.mapBlankSpaceId}>
-          {mapColumn}
-        </div>
-      </div>
+      <div className={s.mapWrap} id={appConfig.mapBlankSpaceId}></div>
+      <div className={s.rightPanel}>{mapColumn}</div>
       <div className={s.footerWrap}>{footer}</div>
       <div className={s.bottomBorder}></div>
     </div>
