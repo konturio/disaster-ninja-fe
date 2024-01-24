639a40487c499adf17cb37435c5d55fee46628b9
Alexandr Dubinin <akiyamka@gmail.com>
2024-01-24 13:11:46 +0100

    core: add modal service

---
+++ src/core/modal/index.tsx
@@ -0,0 +1,66 @@
+import { useCallback } from 'react';
+import { Modal } from '@konturio/ui-kit';
+import { action, atom } from '@reatom/core';
+import { useAction, useAtom } from '@reatom/npm-react';
+import { store } from '~core/store/store';
+
+interface RequiredModalBodyProps<V> {
+  onConfirm: (value: V | null) => void;
+}
+
+interface ModalState<Value = any> {
+  component: React.FunctionComponent<any>;
+  onConfirm: (value: Value) => void;
+  extraProps?: Record<string, unknown>;
+}
+
+const modalAtom = atom<ModalState | null>(null, 'modalAtom');
+const closeModalAction = action((ctx) => modalAtom(ctx, null));
+
+export type ModalResultType<Component> = Parameters<
+  // @ts-expect-error - idk how to type it
+  React.ComponentProps<Component>['onConfirm']
+>[0];
+
+export function showModal<
+  Value,
+  Component extends React.FunctionComponent<Props & Extra>,
+  Extra extends Record<string, unknown>,
+  Props extends RequiredModalBodyProps<Value>,
+>(component: Component, extraProps?: Extra): Promise<ModalResultType<Component>> {
+  return new Promise((res, rej) => {
+    try {
+      modalAtom(store.v3ctx, {
+        component,
+        onConfirm: res,
+        extraProps,
+      });
+    } catch (e) {
+      rej(e);
+    }
+  });
+}
+
+export function GlobalModal() {
+  const [state] = useAtom(modalAtom);
+  const closeModal = useAction(closeModalAction);
+
+  const onCancel = useCallback(() => {
+    state?.onConfirm(null);
+    closeModal();
+  }, [closeModal, state]);
+
+  const onConfirm = useCallback(
+    (result) => {
+      state?.onConfirm(result);
+      closeModal();
+    },
+    [closeModal, state],
+  );
+
+  return state ? (
+    <Modal onCancel={onCancel}>
+      <state.component onConfirm={onConfirm} {...state.extraProps} />
+    </Modal>
+  ) : null;
+}
