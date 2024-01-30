e7305cece607fabf1f15ef4010f2870448a527f5
Alexandr Dubinin <akiyamka@gmail.com>
2024-01-30 19:01:43 +0100

    prevent bugs with default store usage

--- src/utils/atoms/disableDefaultStore.ts
+++ src/utils/atoms/disableDefaultStore.ts
@@ -1,7 +1,17 @@
 import { defaultStore } from '@reatom/core-v2';
-defaultStore.dispatch = () => {
-  throw new Error(
-    `This store instance is restricted in our app.
-Use createAtom from ~utils/atoms/createPrimitives`,
-  );
+
+const preventReading = (obj, message) => {
+  for (const [key, value] of Object.entries(obj)) {
+    Object.defineProperty(obj, key, {
+      get() {
+        throw new Error(message ?? 'This object reading os forbidden');
+      },
+    });
+  }
 };
+
+preventReading(
+  defaultStore,
+  `This store instance is restricted in our app.
+Use createAtom from ~utils/atoms/createPrimitives`,
+);
