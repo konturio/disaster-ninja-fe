6f8bf35e07189181dd93776c5d8d22d1190c3aa1
VK <vkozel@kontur.io>
2024-02-09 05:08:13 +0300

    logging redesign

--- src/core/store/store.ts
+++ src/core/store/store.ts
@@ -1,4 +1,5 @@
 import { createStore } from '@reatom/core-v2';
+import { connectLogger, createLogBatched } from '@reatom/logger';
 import { dispatchMetricsEvent } from '~core/metrics/dispatch';
 import {
   KONTUR_DEBUG,
@@ -7,6 +8,7 @@ import {
   KONTUR_WARN,
   patchTracer,
 } from '~utils/debug';
+import type { Logs } from '@reatom/core';
 
 function configureStore() {
   return createStore({});
@@ -15,31 +17,62 @@ function configureStore() {
 export const store = configureStore();
 
 if (import.meta.env.MODE !== 'test') {
-  store.v3ctx.subscribe((logs) => {
-    for (const patch of logs) {
-      if (!patch.proto.isAction) continue;
+  store.v3ctx.subscribe(logger);
+}
 
-      const name = patch.proto.name!;
+function logger(logs: Logs) {
+  let err: Error;
+  let group = '';
 
-      if (name.includes('invalidate')) continue;
-
-      patch.state.forEach((s) => {
-        const { payload } = patch.state.at(-1)!;
-
-        dispatchMetricsEvent(name, payload);
-
-        if (KONTUR_TRACE_TYPE) {
-          if (name.includes(KONTUR_TRACE_TYPE)) {
-            console.trace('TRACE:', name, logs);
-          }
-        }
-        if (KONTUR_DEBUG) {
-          console.debug(name, payload);
-        }
-        if (KONTUR_WARN) {
-          console.warn(name, payload);
-        }
-      });
+  if (KONTUR_WARN) {
+    try {
+      throw new Error();
+    } catch (e) {
+      err = e as Error;
     }
-  });
+    const stack = err?.stack
+      ?.split('\n')
+      .slice(4)
+      .filter((a) => a.includes('/src/'))
+      .map((a) =>
+        String(a)
+          .trim()
+          .replace('https://localhost:3000/src', '~')
+          .replace('at ', '')
+          .replace(/\?t=\d+/, ''),
+      );
+    if (0 < (stack?.length ?? 0)) {
+      group = `\x1b[94m${Math.trunc(performance.now())}\x1b[m [${logs.filter((d) => d.proto.isAction).length}]: ${stack?.at(0)}`;
+      console.groupCollapsed(group);
+      console.trace();
+    }
+  }
+
+  for (const patch of logs) {
+    if (!patch.proto.isAction) continue;
+
+    const name = patch.proto.name!;
+
+    if (name.includes('invalidate')) continue;
+
+    patch.state.forEach((s) => {
+      const { payload } = patch.state.at(-1)!;
+
+      dispatchMetricsEvent(name, payload);
+
+      if (KONTUR_TRACE_PATCH) {
+        connectLogger(store.v3ctx, { showCause: true, devtools: true });
+      }
+      if (KONTUR_TRACE_TYPE) {
+        if (name.includes(KONTUR_TRACE_TYPE)) {
+          console.trace('TRACE:', name, logs);
+        }
+      }
+      if (KONTUR_DEBUG) {
+        console.debug(name, payload);
+      }
+      KONTUR_WARN && console.info(name, payload);
+    });
+  }
+  group && console.groupEnd();
 }
