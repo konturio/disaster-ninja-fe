24b0d429d5133a48086cd66b188b7b7c3bb0ed7d
Aliaksandr Baranau <abaranau.dev@gmail.com>
2024-03-20 11:33:52 +0100

    Fix login in test version

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,30 @@ import { store } from '~core/store/store';
 import { postAppInit } from '~core/app/postAppInit';
 import type { Config } from '~core/config/types';
 const { Router } = lazily(() => import('~core/router'));
+import * as Sentry from '@sentry/react';
+
+function initSentry(config: Config) {
+  Sentry.init({
+    dsn: config.sentryDsn,
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration({
+        maskAllText: false,
+        blockAllMedia: false,
+      }),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: [
+      'localhost',
+      /^https:\/\/dev-disaster-ninja\.k8s-01\.konturlabs\.com/,
+    ],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+}
 
 const root = createRoot(document.getElementById('root')!);
 
@@ -33,5 +57,6 @@ function renderApp() {
 
 export async function startApp(config: Config) {
   await postAppInit(config);
+  initSentry(config);
   renderApp();
 }
