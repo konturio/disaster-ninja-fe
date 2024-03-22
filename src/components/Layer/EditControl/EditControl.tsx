4dd0bae7a1b15e59eed29355cea0107b3e3a4e9c
Aliaksandr Baranau <abaranau.dev@gmail.com>
2024-03-22 16:21:00 +0100

    fix(18085): Import features/mcda/mcdaConfig in dynamically to avoid 403 errors for unauthenticated users

--- src/components/Layer/EditControl/EditControl.tsx
+++ src/components/Layer/EditControl/EditControl.tsx
@@ -1,9 +1,10 @@
 import { Edit16 } from '@konturio/default-icons';
+import { useAtom } from '@reatom/react-v2';
 import { store } from '~core/store/store';
 import { mcdaLayerAtom } from '~features/mcda/atoms/mcdaLayer';
-import { editMCDAConfig } from '~features/mcda/mcdaConfig';
 import { enabledLayersAtom } from '~core/logical_layers/atoms/enabledLayers';
 import { getMutualExcludedActions } from '~core/logical_layers/utils/getMutualExcludedActions';
+import { FeatureFlag, featureFlagsAtom } from '~core/shared_state';
 import style from './EditControl.module.css';
 import type {
   LogicalLayerActions,
@@ -17,17 +18,22 @@ export function EditControl({
   layerState: LogicalLayerState;
   layerActions: LogicalLayerActions;
 }) {
+  const [featureFlags] = useAtom(featureFlagsAtom);
   async function editLayer() {
-    if (layerState.style?.type === 'mcda') {
-      const config = await editMCDAConfig(layerState);
-      if (config?.id) {
-        layerActions.destroy();
-        store.dispatch([
-          mcdaLayerAtom.createMCDALayer({ ...config, id: config.id }),
-          enabledLayersAtom.set(config.id),
-          ...getMutualExcludedActions(layerState),
-        ]);
-      }
+    if (layerState.style?.type === 'mcda' && featureFlags[FeatureFlag.MCDA]) {
+      import('~features/mcda/mcdaConfig').then(async ({ editMCDAConfig }) => {
+        {
+          const config = await editMCDAConfig(layerState);
+          if (config?.id) {
+            layerActions.destroy();
+            store.dispatch([
+              mcdaLayerAtom.createMCDALayer({ ...config, id: config.id }),
+              enabledLayersAtom.set(config.id),
+              ...getMutualExcludedActions(layerState),
+            ]);
+          }
+        }
+      });
     }
   }
   return (
