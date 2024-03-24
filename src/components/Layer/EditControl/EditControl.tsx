50f8e3e31ef1b43bff67c931ff7a9b6da729bee2
Aliaksandr Baranau <abaranau.dev@gmail.com>
2024-03-24 12:41:02 +0100

    chore(18085): separate editMCDA function in features/mcda/index

--- src/components/Layer/EditControl/EditControl.tsx
+++ src/components/Layer/EditControl/EditControl.tsx
@@ -1,9 +1,5 @@
 import { Edit16 } from '@konturio/default-icons';
 import { useAtom } from '@reatom/react-v2';
-import { store } from '~core/store/store';
-import { mcdaLayerAtom } from '~features/mcda/atoms/mcdaLayer';
-import { enabledLayersAtom } from '~core/logical_layers/atoms/enabledLayers';
-import { getMutualExcludedActions } from '~core/logical_layers/utils/getMutualExcludedActions';
 import { FeatureFlag, featureFlagsAtom } from '~core/shared_state';
 import style from './EditControl.module.css';
 import type {
@@ -21,18 +17,8 @@ export function EditControl({
   const [featureFlags] = useAtom(featureFlagsAtom);
   async function editLayer() {
     if (layerState.style?.type === 'mcda' && featureFlags[FeatureFlag.MCDA]) {
-      import('~features/mcda/mcdaConfig').then(async ({ editMCDAConfig }) => {
-        {
-          const config = await editMCDAConfig(layerState);
-          if (config?.id) {
-            layerActions.destroy();
-            store.dispatch([
-              mcdaLayerAtom.createMCDALayer({ ...config, id: config.id }),
-              enabledLayersAtom.set(config.id),
-              ...getMutualExcludedActions(layerState),
-            ]);
-          }
-        }
+      import('~features/mcda').then(async ({ editMCDA }) => {
+        editMCDA(layerState, layerActions);
       });
     }
   }
--- src/features/mcda/index.ts
+++ src/features/mcda/index.ts
@@ -1,10 +1,16 @@
 import { i18n } from '~core/localization';
 import { toolbar } from '~core/toolbar';
 import { store } from '~core/store/store';
+import { enabledLayersAtom } from '~core/logical_layers/atoms/enabledLayers';
+import { getMutualExcludedActions } from '~core/logical_layers/utils/getMutualExcludedActions';
 import { mcdaLayerAtom } from './atoms/mcdaLayer';
-import { createMCDAConfig } from './mcdaConfig';
+import { createMCDAConfig, editMCDAConfig } from './mcdaConfig';
 import { MCDA_CONTROL_ID, UPLOAD_MCDA_CONTROL_ID } from './constants';
 import { askMcdaJSONFile } from './openMcdaFile';
+import type {
+  LogicalLayerActions,
+  LogicalLayerState,
+} from '~core/logical_layers/types/logicalLayer';
 
 export const mcdaControl = toolbar.setupControl({
   id: MCDA_CONTROL_ID,
@@ -67,6 +73,22 @@ uploadMcdaControl.onStateChange((ctx, state) => {
     store.dispatch(uploadMcdaControl.setState('regular'));
   }
 });
+
+/** Edit MCDA */
+export async function editMCDA(
+  layerState: LogicalLayerState,
+  layerActions: LogicalLayerActions,
+) {
+  const config = await editMCDAConfig(layerState);
+  if (config?.id) {
+    layerActions.destroy();
+    store.dispatch([
+      mcdaLayerAtom.createMCDALayer({ ...config, id: config.id }),
+      enabledLayersAtom.set(config.id),
+      ...getMutualExcludedActions(layerState),
+    ]);
+  }
+}
 
 export function initMCDA() {
   mcdaControl.init();
