9c5c089a3688da11434f2a98ab32516f51b8773c
Aliaksandr Baranau <abaranau.dev@gmail.com>
2024-03-28 13:54:58 +0100

    fix: fix read-only value error when using "no" normalization value

--- src/core/logical_layers/renderers/stylesConfigs/mcda/mcdaStyle.ts
+++ src/core/logical_layers/renderers/stylesConfigs/mcda/mcdaStyle.ts
@@ -111,14 +111,17 @@ function generateLayerPaint(props: PaintProps) {
 }
 
 export function createMCDAStyle(config: MCDAConfig) {
-  const [absoluteMin = 0, absoluteMax = 1] = config.layers.reduce((acc, l) => {
-    // Show full range of values between min max if normalization not enabled
-    const range: [number, number] = l.normalization === 'no' ? l.range : [0, 1];
-    if (acc.length === 0) return range;
-    acc[0] = Math.min(acc[0], range[0]);
-    acc[1] = Math.min(acc[1], range[1]);
-    return acc;
-  }, [] as [number, number] | []);
+  const [absoluteMin = 0, absoluteMax = 1] = config.layers.reduce(
+    (acc, l) => {
+      // Show full range of values between min max if normalization not enabled
+      const range: [number, number] = l.normalization === 'no' ? l.range : [0, 1];
+      if (acc.length === 0) return [...range];
+      acc[0] = Math.min(acc[0], range[0]);
+      acc[1] = Math.min(acc[1], range[1]);
+      return acc;
+    },
+    [] as [number, number] | [],
+  );
 
   const mcdaResult = linearNormalization(config.layers);
 
