875a2363b81a9c81a7096beab638d2f05ea4b5f3
Алексей Мурашко <amur@aleksejs-air.home>
2024-04-03 18:39:25 +0200

    Expand login tests to cover all key projects and fix config

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_URL,
+    baseURL: JSON.parse(process.env.PROJECTS!).atlas.url,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     ignoreHTTPSErrors: true,
