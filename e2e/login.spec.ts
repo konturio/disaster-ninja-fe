f7e93126e9064cfbc224f6f258a093b04bddb6ce
Aliaksei Murashka <112632451+amurKontur@users.noreply.github.com>
2024-04-10 13:03:49 +0200

    Adapt tests to support CI and broad them to other projects (#685)
    * Expand login tests to cover all key projects and fix config
    
    * fix: rename tests examples (#682)
    
    * fix: 18065: correct bivariate legend style calculation rules (#680)
    
    * fix: 18065: correct bivariate legend style calculation rules
    
    * fix: 18065: add tests and types
    
    * fix(18075): don't show out-of range MCDA results as transparent for "sentiments" color config (#684)
    
    * Expand login tests to cover all key projects and fix config
    
    * added simplified loop and removed base url
    
    * fixed projects, moved projects configs to separate json, fixed logics of atlas check
    
    * fix readme
    
    * moved projects-config to e2e folder
    
    * Added CI pipeline
    
    * fix pipeline
    
    * fix pipeline name
    
    * fixed pipeline name
    
    * fix rabbit review
    
    * fix error of pipeline steps
    
    * added shell call
    
    * Added timeout and node.js config installation
    
    * Moved input to separate variable as for rabbit suggestion
    
    * fix syntax
    
    * Refactor e2e test configuration and environment setup (#692)
    
    This commit introduces several changes to the end-to-end (e2e) testing setup and configuration. The primary changes include:
    
    - Standardization of environment variable names across different files, replacing `TEST_EMAIL` and `TEST_PASSWORD` with `EMAIL` and `PASSWORD`.
    - Removal of hardcoded environment URLs in the GitHub Actions workflow file, replacing them with a dynamic setup that adjusts based on the input environment.
    - Deletion of an example test file that was no longer needed.
    - Update of the login test specification to accommodate changes in the environment setup and to remove hardcoded values.
    - Refactoring of the projects configuration JSON to include environment-specific URLs and additional properties such as `hasCookieBanner`.
    - Adjustment of the Playwright configuration to prioritize the production environment file.
    
    These changes aim to simplify the configuration process, reduce duplication, and make the e2e testing framework more flexible and maintainable.
    
    ---------
    
    Co-authored-by: Алексей Мурашко <amur@aleksejs-air.home>
    Co-authored-by: dqunbp <dqunbp@gmail.com>
    Co-authored-by: IlyaLosikKontur <162133804+IlyaLosikKontur@users.noreply.github.com>
    Co-authored-by: Alexander Baranov <abaranau.dev@gmail.com>

---
+++ e2e/login.spec.ts
@@ -0,0 +1,54 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { test, expect } from '@playwright/test';
+
+type Project = {
+  env: 'prod' | 'dev' | 'test';
+  name: string;
+  url: string;
+  title: string;
+  hasCookieBanner: boolean;
+};
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const data = fs.readFileSync(path.join(__dirname, 'projects-config.json')).toString();
+
+const appName = process.env.APP_NAME ?? 'all';
+const environment = process.env.ENVIRONMENT ?? 'prod';
+const projects: Project[] = JSON.parse(data)
+  .filter((project: Project) => project.env === environment)
+  .filter((project: Project) => appName === 'all' || project.name === appName);
+
+// Create a loop to loop over all the projects and create a test for everyone
+for (const project of projects) {
+  test(`As User, I can login to ${project.title}`, async ({ page }) => {
+    await page.goto(project.url);
+
+    // Expect a title "to contain" a Kontur Atlas.
+    await expect(page).toHaveTitle(`${project.title}`);
+
+    await page.getByText('Login').click();
+
+    await expect(page.getByRole('button', { name: 'Log in' })).toBeVisible();
+
+    // Getting email field and filling in
+    const emailInput = page.getByRole('textbox').first();
+    await emailInput.fill(process.env.EMAIL!);
+
+    // Getting password field and filling in
+    const passwordInput = page.locator('input[type="password"]');
+    await passwordInput.fill(process.env.PASSWORD!);
+
+    // Currently OAM project doesn't have cookies popups
+    if (project.hasCookieBanner) await page.getByText('Accept optional cookies').click();
+
+    // Getting Log in button and clicking
+    await page.getByRole('button', { name: 'Log in' }).click();
+
+    // Check that logout button is present
+    await expect(page.getByRole('button', { name: 'Logout' })).toBeVisible();
+  });
+}
--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,9 @@ import * as dotenv from 'dotenv';
  * https://github.com/motdotla/dotenv
  */
 
-dotenv.config({ path: ['.env.playwright.local', '.env.playwright'] });
+dotenv.config({
+  path: ['.env.playwright.production', '.env.playwright.local', '.env.playwright'],
+});
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -24,9 +26,6 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_URL,
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     ignoreHTTPSErrors: true,
     trace: 'on-first-retry',
