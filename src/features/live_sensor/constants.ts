7048ead3426f391aa92825c41145ea0f80e952ca
Alexander Baranov <abaranau.dev@gmail.com>
2024-04-10 16:50:57 +0200

    fix(18180): add Record Sensors button to Toolbar (#689)
    * fix(18180): add Start Sensor Recording button to Toolbar
    
    * "Start Sensor Recording" -> "Record sensors"

--- src/features/live_sensor/constants.ts
+++ src/features/live_sensor/constants.ts
@@ -1,3 +1,2 @@
-export const SENSOR_CONTROL = 'sensorControl';
-export const SENSOR_CONTROL_NAME = 'Live sensor control';
+export const SENSOR_CONTROL_ID = 'sensorControl';
 export const SENSOR_PRECISION = 6;
