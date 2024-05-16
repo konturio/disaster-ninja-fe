350897d5a5b47809c55f181bf1d0f344583efdeb
Aliaksandr Baranau <abaranau.dev@gmail.com>
2024-05-16 02:37:55 +0200

    refactor: Extract Selected Area and Reference Area layers creation into common util function

--- src/features/focused_geometry_layer/index.ts
+++ src/features/focused_geometry_layer/index.ts
@@ -1,10 +1,6 @@
-import { layersRegistryAtom } from '~core/logical_layers/atoms/layersRegistry';
-import { layersLegendsAtom } from '~core/logical_layers/atoms/layersLegends';
-import { layersSettingsAtom } from '~core/logical_layers/atoms/layersSettings';
-import { createAsyncWrapper } from '~utils/atoms/createAsyncWrapper';
-import { store } from '~core/store/store';
 import { forceRun } from '~utils/atoms/forceRun';
 import { FOCUSED_GEOMETRY_LOGICAL_LAYER_ID } from '~core/focused_geometry/constants';
+import { registerNewGeometryLayer } from '~core/logical_layers/utils/registerNewGeometryLayer';
 import {
   FOCUSED_GEOMETRY_LOGICAL_LAYER_TRANSLATION_KEY,
   FOCUSED_GEOMETRY_COLOR,
@@ -21,40 +17,12 @@ export function initFocusedGeometryLayer() {
   // Connect layer source with focused geometry
   forceRun(createFocusedGeometrySourceAtom(FOCUSED_GEOMETRY_LOGICAL_LAYER_ID));
 
-  store.dispatch([
-    // Set layer settings once
-    layersSettingsAtom.set(
-      FOCUSED_GEOMETRY_LOGICAL_LAYER_ID,
-      createAsyncWrapper({
-        name: FOCUSED_GEOMETRY_LOGICAL_LAYER_TRANSLATION_KEY,
-        id: FOCUSED_GEOMETRY_LOGICAL_LAYER_ID,
-        boundaryRequiredForRetrieval: false,
-        ownedByUser: false,
-      }),
-    ),
-    // Sel layer legend once
-    layersLegendsAtom.set(
-      FOCUSED_GEOMETRY_LOGICAL_LAYER_ID,
-      createAsyncWrapper({
-        type: 'simple',
-        name: FOCUSED_GEOMETRY_LOGICAL_LAYER_TRANSLATION_KEY,
-        steps: [
-          {
-            stepShape: 'circle',
-            stepName: FOCUSED_GEOMETRY_LOGICAL_LAYER_TRANSLATION_KEY,
-            style: {
-              color: FOCUSED_GEOMETRY_COLOR,
-            },
-          },
-        ],
-      }),
-    ),
-    // Add layer to registry, so it available from panel and use common lifecycle
-    layersRegistryAtom.register({
-      renderer: new FocusedGeometryRenderer({
-        id: FOCUSED_GEOMETRY_LOGICAL_LAYER_ID,
-      }),
+  registerNewGeometryLayer(
+    FOCUSED_GEOMETRY_LOGICAL_LAYER_ID,
+    FOCUSED_GEOMETRY_LOGICAL_LAYER_TRANSLATION_KEY,
+    new FocusedGeometryRenderer({
       id: FOCUSED_GEOMETRY_LOGICAL_LAYER_ID,
     }),
-  ]);
+    FOCUSED_GEOMETRY_COLOR,
+  );
 }
--- src/features/reference_area/initReferenceAreaLayer.ts
+++ src/features/reference_area/initReferenceAreaLayer.ts
@@ -1,12 +1,9 @@
 import { store } from '~core/store/store';
-import { layersSettingsAtom } from '~core/logical_layers/atoms/layersSettings';
-import { layersLegendsAtom } from '~core/logical_layers/atoms/layersLegends';
-import { layersRegistryAtom } from '~core/logical_layers/atoms/layersRegistry';
 import { createAsyncWrapper } from '~utils/atoms/createAsyncWrapper';
-import { enabledLayersAtom } from '~core/logical_layers/atoms/enabledLayers';
 import { referenceAreaAtom } from '~core/shared_state/referenceArea';
 import { createGeoJSONLayerSource } from '~core/logical_layers/utils/createGeoJSONLayerSource';
 import { layersSourcesAtom } from '~core/logical_layers/atoms/layersSources';
+import { registerNewGeometryLayer } from '~core/logical_layers/utils/registerNewGeometryLayer';
 import {
   REFERENCE_AREA_COLOR,
   REFERENCE_AREA_LOGICAL_LAYER_ID,
@@ -20,11 +17,20 @@ let isInitialized = false;
 export function initReferenceAreaLayer() {
   if (isInitialized) return;
   isInitialized = true;
+
   // when referenceAreaAtom updates, we create a new reference area layer source
   store.v3ctx.subscribe(referenceAreaAtom, (geometry) => {
     setReferenceAreaLayerSource(geometry);
   });
-  createReferenceAreaLayer();
+
+  registerNewGeometryLayer(
+    REFERENCE_AREA_LOGICAL_LAYER_ID,
+    REFERENCE_AREA_LOGICAL_LAYER_TRANSLATION_KEY,
+    new ReferenceAreaRenderer({
+      id: REFERENCE_AREA_LOGICAL_LAYER_ID,
+    }),
+    REFERENCE_AREA_COLOR,
+  );
 }
 
 function setReferenceAreaLayerSource(geometry: GeometryWithHash | null) {
@@ -59,44 +65,4 @@ function setReferenceAreaLayerSource(geometry: GeometryWithHash | null) {
     );
   }
   return geometry;
-}
-
-function createReferenceAreaLayer() {
-  store.dispatch([
-    // Set layer settings once
-    layersSettingsAtom.set(
-      REFERENCE_AREA_LOGICAL_LAYER_ID,
-      createAsyncWrapper({
-        name: REFERENCE_AREA_LOGICAL_LAYER_TRANSLATION_KEY,
-        id: REFERENCE_AREA_LOGICAL_LAYER_ID,
-        boundaryRequiredForRetrieval: false,
-        ownedByUser: false,
-      }),
-    ),
-    // Sel layer legend once
-    layersLegendsAtom.set(
-      REFERENCE_AREA_LOGICAL_LAYER_ID,
-      createAsyncWrapper({
-        type: 'simple',
-        name: REFERENCE_AREA_LOGICAL_LAYER_TRANSLATION_KEY,
-        steps: [
-          {
-            stepShape: 'circle',
-            stepName: REFERENCE_AREA_LOGICAL_LAYER_TRANSLATION_KEY,
-            style: {
-              color: REFERENCE_AREA_COLOR,
-            },
-          },
-        ],
-      }),
-    ),
-    // Add layer to registry, so it available from panel and use common lifecycle
-    layersRegistryAtom.register({
-      renderer: new ReferenceAreaRenderer({
-        id: REFERENCE_AREA_LOGICAL_LAYER_ID,
-      }),
-      id: REFERENCE_AREA_LOGICAL_LAYER_ID,
-    }),
-    enabledLayersAtom.set(REFERENCE_AREA_LOGICAL_LAYER_ID),
-  ]);
 }
