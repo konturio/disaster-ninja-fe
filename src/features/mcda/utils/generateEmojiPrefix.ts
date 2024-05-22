678ac1ac52d5ae2b894ec7f937beda563a2c3a15
Natallia Harshunova <nattallius@gmail.com>
2024-05-22 12:58:50 +0200

    feat(18186): add some checks

--- src/features/mcda/utils/generateEmojiPrefix.ts
+++ src/features/mcda/utils/generateEmojiPrefix.ts
@@ -1,8 +1,11 @@
 import emojiRegex from 'emoji-regex';
 
 export const generateEmojiPrefix = (emoji = '') => {
-  const regex = emojiRegex();
-  const emojiCount = [...emoji.matchAll(regex)].length;
-  const result = emojiCount === 1 ? `\u3000\u00A0\u00A0${emoji}` : `${emoji}`;
-  return result;
+  try {
+    const regex = emojiRegex();
+    const emojiCount = [...emoji.matchAll(regex)].length;
+    return emojiCount === 1 ? `\u3000\u00A0\u00A0${emoji}` : `${emoji}`;
+  } catch {
+    return '';
+  }
 };
