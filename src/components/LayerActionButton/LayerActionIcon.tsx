fb1f531610fe2c206bb2442aeeb677718c9f6eb1
Natallia Harshunova <nattallius@gmail.com>
2024-05-31 20:23:58 +0200

    feat(12618): add key accessibility

--- src/components/LayerActionButton/LayerActionIcon.tsx
+++ src/components/LayerActionButton/LayerActionIcon.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import { useState } from 'react';
 import { Tooltip, TooltipTrigger, TooltipContent } from '~core/tooltips';
 import style from './LayerActionButton.module.css';
 import type { PropsWithChildren } from 'react';
@@ -15,10 +16,23 @@ export function LayerActionIcon({
   children,
   className,
 }: PropsWithChildren<LayerActionButtonProps>) {
+  const [isOpen, setIsOpen] = useState<boolean>();
+
+  function onKeyUp(event: React.KeyboardEvent<HTMLDivElement>) {
+    if (event.key === 'Enter') {
+      onClick();
+    }
+  }
+
   return (
-    <Tooltip placement="top">
+    <Tooltip placement="top" open={isOpen}>
       <TooltipTrigger asChild>
         <div
+          onFocus={() => setIsOpen(true)}
+          onBlur={() => setIsOpen(false)}
+          onMouseEnter={() => setIsOpen(true)}
+          onMouseLeave={() => setIsOpen(false)}
+          onKeyUp={onKeyUp}
           className={clsx(style.actionButton, className)}
           onClick={onClick}
           tabIndex={0}
