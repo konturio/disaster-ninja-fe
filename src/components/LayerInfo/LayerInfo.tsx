8e7545eb800c2994faf0c64e06b14d03a4428124
Aliaksandr Baranau <abaranau.dev@gmail.com>
2024-06-03 13:27:47 +0200

    chore(18623): remove unnecessary useMemo from LayersInfo, add comment

--- src/components/LayerInfo/LayerInfo.tsx
+++ src/components/LayerInfo/LayerInfo.tsx
@@ -15,21 +15,24 @@ type LayerInfoProps = {
 
 export const LayerInfo = React.memo(
   ({ layersInfo, tooltipId, className }: LayerInfoProps) => {
-    const infoString = useMemo(() => {
-      const lines = layersInfo?.map((info) => {
-        const name = info?.name && `#### ${info?.name}`;
-        const copyrights = info?.copyrights
-          ?.filter((v) => v)
-          .map((copyright) => `- ${copyright}\n`);
-        return name || info.description || copyrights?.length
-          ? [name, info.description, copyrights]
-          : null;
-      });
-      return lines
-        .flat(2)
-        .filter((line) => !!line)
-        .join('\n');
-    }, [layersInfo]);
+    /* for each item of layersInfo, add lines in order:
+      - name
+      - description
+      - copyrights (as unordered list)
+    */
+    const lines = layersInfo?.map((info) => {
+      const name = info?.name && `#### ${info?.name}`;
+      const copyrights = info?.copyrights
+        ?.filter((v) => v)
+        .map((copyright) => `- ${copyright}\n`);
+      return name || info.description || copyrights?.length
+        ? [name, info.description, copyrights]
+        : null;
+    });
+    const infoString = lines
+      .flat(2)
+      .filter((line) => line)
+      .join('\n');
 
     if (infoString) {
       return (
