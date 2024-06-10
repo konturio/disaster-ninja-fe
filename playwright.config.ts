07389ef9ffc82ba4d1176a7e57fb2e3ddea116ca
Aliaksei Murashka <amurashka@kontur.io>
2024-06-11 00:22:39 +0200

    test: slack integration and workflow update

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
 import * as dotenv from 'dotenv';
+import { LogLevel } from '@slack/web-api';
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -28,7 +29,35 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 4 : 6,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    [
+      './node_modules/playwright-slack-report/dist/src/SlackReporter.js',
+      {
+        channels: ['health_check'], // provide one or more Slack channels
+        sendResults: 'always', // "always" , "on-failure", "off"
+        maxNumberOfFailuresToShow: 100,
+        meta: [
+          {
+            key: `Environment 👷🏻‍♂️`,
+            value: `${process.env.ENVIRONMENT?.toUpperCase()}`,
+          },
+          {
+            key: `Application 📲`,
+            value: `${process.env.APP_NAME?.toUpperCase()}`,
+          },
+          {
+            key: 'Tested user with no rights',
+            value: process.env.EMAIL,
+          },
+        ],
+        slackOAuthToken: process.env.SLACK_BOT_USER_OAUTH_TOKEN,
+        slackLogLevel: LogLevel.DEBUG,
+        disableUnfurl: true,
+        showInThread: true,
+      },
+    ],
+    ['html'], // other reporters
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
