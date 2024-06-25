5329783d07491c7a3d5fa92f44d08a4dba9393c9
Natallia Harshunova <nattallius@gmail.com>
2024-06-25 10:56:28 +0200

    feat(18228): refactor request error handling

--- src/core/api/subscription.ts
+++ src/core/api/subscription.ts
@@ -3,13 +3,9 @@ import { configRepo } from '~core/config';
 import type { CurrentSubscription } from '~features/subscriptions/types';
 
 export async function getCurrentUserSubscription() {
-  try {
-    return await apiClient.get<CurrentSubscription | null>(
-      '/current_user/billing_subscription',
-      { appId: configRepo.get().id },
-      true,
-    );
-  } catch (error) {
-    return null;
-  }
+  return await apiClient.get<CurrentSubscription | null>(
+    '/current_user/billing_subscription',
+    { appId: configRepo.get().id },
+    true,
+  );
 }
