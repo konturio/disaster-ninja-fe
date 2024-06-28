e10ce3742bdfb695be3776e3603b091cc5abd904
Aliaksandr Baranau <abaranau.dev@gmail.com>
2024-06-28 02:10:17 +0200

    feat(18430): added AxisTransformation type

--- src/utils/bivariate/types/stat.types.ts
+++ src/utils/bivariate/types/stat.types.ts
@@ -1,5 +1,7 @@
 /** Full docs: https://gist.github.com/Akiyamka/8ad19a8de3c955ac1f27f67281c12fdf#correlationrate */
 
+import type { TransformationFunction } from '~core/logical_layers/renderers/stylesConfigs/mcda/types';
+
 export type CornerRange = 'good' | 'bad' | 'important' | 'unimportant' | 'neutral';
 
 export type OverlayColor = {
@@ -16,6 +18,14 @@ export type Step = {
 type Unit = { id: string | null; longName: string | null; shortName: string | null };
 export type Quotient = [string, string]; // this field will be removed in next tickets, only Quotients will stay
 
+export type AxisTransformation = {
+  transformation: TransformationFunction;
+  min: number;
+  mean: number;
+  skew: number;
+  stddev: number;
+};
+
 export type Axis = {
   id: string;
   label: string;
@@ -24,6 +34,7 @@ export type Axis = {
   quotients?: Indicator[];
   quality?: number;
   parent?: Quotient;
+  transformation?: AxisTransformation;
 };
 
 export type CorrelationRate = {
