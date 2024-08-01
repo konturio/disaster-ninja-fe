ebc15738452dc65e19e3ff760d7cee3359239055
Aliaksandr Baranau <abaranau.dev@gmail.com>
2024-08-01 16:06:21 +0200

    Replace default values in to-number with Infinity and -Infinity for clamp and min/max operations

--- src/core/logical_layers/renderers/stylesConfigs/mcda/calculations/operations.ts
+++ src/core/logical_layers/renderers/stylesConfigs/mcda/calculations/operations.ts
@@ -31,7 +31,7 @@ export class MapMath implements IsomorphMath<MapExpression> {
   clamp = (x: MapExpression, min: MapExpression, max: MapExpression): MapExpression => [
     'let',
     'clampedX',
-    ['to-number', x, 1],
+    ['to-number', x, Infinity],
     [
       'case',
       ['<', ['var', 'clampedX'], min],
@@ -44,17 +44,17 @@ export class MapMath implements IsomorphMath<MapExpression> {
   min = (v1: MapExpression, v2: MapExpression): MapExpression => [
     'let',
     'v1',
-    ['to-number', v1, 0],
+    ['to-number', v1, Infinity],
     'v2',
-    ['to-number', v2, 0],
+    ['to-number', v2, Infinity],
     ['case', ['<', ['var', 'v2'], ['var', 'v1']], ['var', 'v2'], ['var', 'v1']],
   ];
   max = (v1: MapExpression, v2: MapExpression): MapExpression => [
     'let',
     'v1',
-    ['to-number', v1, 0],
+    ['to-number', v1, -Infinity],
     'v2',
-    ['to-number', v2, 0],
+    ['to-number', v2, -Infinity],
     ['case', ['>', ['var', 'v2'], ['var', 'v1']], ['var', 'v2'], ['var', 'v1']],
   ];
 }
