74242cd5af46137cd1c60a678288b969b5b51fef
dqunbp <dqunbp@gmail.com>
2024-08-09 14:19:50 +0300

    chore(osm_edit_link): remove comments

--- src/features/osm_edit_link/openOsmLink.ts
+++ src/features/osm_edit_link/openOsmLink.ts
@@ -1,6 +1,5 @@
 import { URL_ZOOM_OFFSET } from '~core/constants';
 
-// Функция для открытия ссылки в редакторах OpenStreetMap, iD и RapiD
 export function openOsmLink(
   { lat, lng, zoom },
   baseLink = 'https://www.openstreetmap.org/edit#map=',
@@ -23,7 +22,6 @@ function calculateMaxJosmArea(
   return { left, right, top, bottom };
 }
 
-// Функция для открытия ссылки в JOSM
 export function openJosmLink(
   { lat, lng },
   baseLink = 'http://127.0.0.1:8111/load_and_zoom?',
