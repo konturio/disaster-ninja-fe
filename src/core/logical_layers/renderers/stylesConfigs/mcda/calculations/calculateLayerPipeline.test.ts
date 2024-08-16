81601d647ed6ffe39965687d6d859e10e84202a5
Aliaksandr Baranau <abaranau.dev@gmail.com>
2024-08-16 13:51:58 +0200

    test(MCDA): 19182 more complex unit tests for layerCalculationPipeline (#820)
    * chore: remove min and add lowerBound ahd upperBound properties to AxisTransformation
    
    * feat(MCDA): 18652 add text controls for sigma range buttons
    
    * feat: add simplified transformation statistic fetching
    
    * feat: show temporary debug info by clicking on Transform label
    
    * chore: use double click for showing debug info
    
    * fix: use non-transformed statistics values to set ranges
    
    * fix(MCDA): 19181 Fix Clamp Outliers logic
    
    * feat: better chart scaling
    
    * feat: use non-transformed mean + sigma to calculate predefined ranges
    
    * fix(MCDA): proper clamping operation for MCDA style expression
    
    * feat: clamp transformed values by lower/upper bounds of used transformation
    
    * feat: clamp transformed values by lower/upper bounds of used transformation
    
    * chore: extract generateSigmaRange function
    
    * feat: add transformed values clamping + improved scaling in TransformationsChart
    
    * feat: add datasetStats field to MCDA axis. Use datasetStats to set MCDA range to 3-sigma upon creation
    
    * feat(MCDA): 19176 Add clamping of transformed values by upperBound and lowerBound of used transformation
    
    * chore: replace isNumber() from @turf/helpers with our in-house one
    
    * chore: remove arbitrary default values from to-number to avoid masking errors
    
    * chore: remove arbitrary default values from to-number to avoid masking errors
    
    * Revert "chore: remove arbitrary default values from to-number to avoid masking errors"
    
    This reverts commit 43fe468ceeb71390f106732de40fdb7e29b5381e.
    
    * Replace default values in to-number with Infinity and -Infinity for clamp and min/max operations
    
    * chore: replace Infinity with Number.POSITIVE_INFINITY
    
    * fix: fix changing axis transformation
    
    * fix: do not clamp transformed values if outliers === 'unmodified'
    
    * fix: don't apply secondary clamping to transofrmation: 'no'
    
    * chore: rename state
    
    * chore: simplify layer update function
    
    * chore: extract MCDATransformationDebugInfo into separate component
    
    * chore: extract MCDARangeControls into separate component
    
    * chore: move range validation into MCDARangeControls component
    
    * chore: remove unused code
    
    * fix: remove incorrect case of setting sigma range
    
    * chore: add new way of getting datasetRange
    
    * test: update snapshot
    
    * chore: simplify setToFullDatasetRange()
    
    * chore: remove TransformationsChart.propType
    
    * chore: simplify setToSigmaRange()
    
    * chore: remove unused import
    
    * chore: small refactors to address PR comments
    
    * chore(TransformationsChart): add explanatory comments to scale functions
    
    * test(MCDA): add unit tests (with CSV-based data) for calculateLayerPipeline. Add xlsx file with formulas for updating CSV file
    
    * fix: fix inline style
    
    * test: move csvtojson to dependencies to see if it affects depcruiser error in CI
    
    * test: update lockfile
    
    * chore: move cvstojson back to devDependencies
    
    * Revert "chore: move cvstojson back to devDependencies"
    
    This reverts commit e5eb590b622b3b30da38c7627d5ce7997ca3f8c5.
    
    * fix: remove csvtojson, use papaparse instead
    
    * chore: add readme file for calculateLayerPipeline test
    
    * Update src/core/logical_layers/renderers/stylesConfigs/mcda/calculations/testData/readme.md
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * chore: update readme
    
    * chore: fix grammar mistakes in readme
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * chore: update readme file with more information
    
    * chore: move readme file to parent folder
    
    * chore: fix typo
    
    * chore: fix typo (again)
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ src/core/logical_layers/renderers/stylesConfigs/mcda/calculations/calculateLayerPipeline.test.ts
@@ -0,0 +1,162 @@
+import fs from 'fs';
+import { test, describe, expect } from 'vitest';
+import papa from 'papaparse';
+import * as dot from 'dot-object';
+import { calculateLayerPipeline, inViewCalculations } from '.';
+import type { MCDALayer, TransformationFunction } from '../types';
+
+const PRECISION = 0.0000000001;
+
+describe('mcda calculations', async () => {
+  const filePath = __dirname + '/testData/calculateLayerPipeline.testdata.csv';
+  const jsonResult: MCDATestEntry[] = await parseCsvTestFile(filePath);
+  if (!jsonResult?.length) {
+    throw new Error('Could not find parsed test data');
+  }
+  jsonResult.forEach((testEntry) => {
+    test(`${testEntry.testId}: ${testEntry.description}`, () => {
+      expect(testEntry.description).toBeTruthy();
+      const calculateNumber = calculateLayerPipeline(inViewCalculations, () => ({
+        num: testEntry.value.numerator,
+        den: testEntry.value.denominator,
+      }));
+      const transformationFunctions: TransformationFunction[] = [
+        'no',
+        'log',
+        'log_epsilon',
+        'square_root',
+        'cube_root',
+      ];
+      for (const transformationFunction of transformationFunctions) {
+        const testAxis: MCDALayer = {
+          ...DEFAULT_AXIS,
+          ...testEntry.axis,
+          transformation: {
+            ...DEFAULT_AXIS.transformation!,
+            transformation: transformationFunction,
+            lowerBound: testEntry.transformation[transformationFunction].lowerBound,
+            upperBound: testEntry.transformation[transformationFunction].upperBound,
+          },
+        };
+        expect(
+          Math.abs(
+            calculateNumber(testAxis) -
+              testEntry.transformation[transformationFunction].expectedScore,
+          ),
+        ).toBeLessThanOrEqual(PRECISION);
+      }
+    });
+  });
+});
+
+function parseCsvTestFile(filePath: string): Promise<MCDATestEntry[]> {
+  const file = fs.readFileSync(filePath).toString();
+  return new Promise((res) => {
+    papa.parse<FlatMCDATestEntryDTO>(file, {
+      dynamicTyping: true,
+      header: true,
+      delimiter: ',',
+      skipEmptyLines: true,
+      complete: (parsed) => {
+        if (parsed.errors.length > 0) {
+          throw new Error(
+            `Error parsing csv file:', ${parsed.errors.map((err) => `\n(row #${err.row}) ${err.type}: ${err.message}`, 0)}`,
+          );
+        }
+        const expandedResult = parsed.data.map<MCDATestEntry>(
+          (row) => dot.object(row) as MCDATestEntry,
+        );
+        res(expandedResult);
+      },
+      error: (e) => {
+        throw e;
+      },
+    });
+  });
+}
+
+const DEFAULT_AXIS: MCDALayer = {
+  id: 'test',
+  name: 'TEST',
+  axis: ['indicator_numerator', 'indicator_denominator'],
+  indicators: [
+    {
+      name: 'indicator_numerator',
+      label: 'Numerator label',
+      unit: {
+        id: 'celc_deg',
+        shortName: '°C',
+        longName: 'degrees Celsius',
+      },
+    },
+    {
+      name: 'indicator_denominator',
+      label: 'Denominator label',
+      unit: {
+        id: 'null',
+        shortName: null,
+        longName: null,
+      },
+    },
+  ],
+  range: [0, 100],
+  sentiment: ['bad', 'good'],
+  coefficient: 1,
+  outliers: 'clamp',
+  transformationFunction: 'no',
+  transformation: {
+    skew: 0,
+    stddev: 0,
+    mean: 0,
+    lowerBound: 0,
+    upperBound: 0,
+    transformation: 'no',
+  },
+  normalization: 'max-min',
+  unit: '',
+  datasetStats: { minValue: -30, maxValue: 100, mean: 0, stddev: 10 },
+};
+
+type TransformationTestData = {
+  lowerBound: number;
+  upperBound: number;
+  expectedScore: number;
+};
+interface MCDATestEntry {
+  testId: number;
+  description: string;
+  axis: Partial<MCDALayer>;
+  value: {
+    numerator: number;
+    denominator: number;
+  };
+  transformation: Record<TransformationFunction, TransformationTestData>;
+}
+
+type FlatMCDATestEntryDTO = {
+  testId: number;
+  description: string;
+  'axis.coefficient': number;
+  'axis.sentiment[0]': string;
+  'axis.sentiment[1]': string;
+  'axis.normalization': string;
+  'axis.outliers': string;
+  'axis.datasetStats.minValue': number;
+  'axis.range[0]': number;
+  'axis.range[1]': number;
+  'value.numerator': number;
+  'value.denominator': number;
+  'transformation.no.expectedScore': number;
+  'transformation.log.lowerBound': number;
+  'transformation.log.upperBound': number;
+  'transformation.log.expectedScore': number;
+  'transformation.log_epsilon.lowerBound': number;
+  'transformation.log_epsilon.upperBound': number;
+  'transformation.log_epsilon.expectedScore': number;
+  'transformation.square_root.lowerBound': number;
+  'transformation.square_root.upperBound': number;
+  'transformation.square_root.expectedScore': number;
+  'transformation.cube_root.lowerBound': number;
+  'transformation.cube_root.upperBound': number;
+  'transformation.cube_root.expectedScore': number;
+};
