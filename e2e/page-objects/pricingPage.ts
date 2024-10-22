30376b3449ebbe0bc35285c6a1dd2cd24ce156b9
Aliaksei Murashka <amurashka@kontur.io>
2024-10-22 13:30:22 +0300

    test: speed up promises fullfillment for pricing page

--- e2e/page-objects/pricingPage.ts
+++ e2e/page-objects/pricingPage.ts
@@ -10,19 +10,17 @@ export class PricingPage extends HelperBase {
     await this.page.getByText('Enterprise').waitFor({ state: 'visible' });
     const plansAndPricingTexts = await this.page.getByText('Plans & Pricing').all();
     await Promise.all([
-      expect(await this.page.title()).toContain('Plans & Pricing'),
+      expect(this.page).toHaveTitle(/Plans & Pricing/),
       expect(this.page.getByText('Professional', { exact: true })).toBeVisible(),
       expect(this.page.getByText('Educational', { exact: true })).toBeVisible(),
       expect(this.page.getByText('Custom', { exact: true })).toBeVisible(),
+      expect(this.page.getByText('Book a demo', { exact: true })).toHaveAttribute(
+        'href',
+        'https://calendly.com/kbakhanko/atlas-demo',
+      ),
       expect(
-        await this.page.getByText('Book a demo', { exact: true }).getAttribute('href'),
-      ).toEqual('https://calendly.com/kbakhanko/atlas-demo'),
-      expect(
-        await this.page
-          .getByText('Request trial', { exact: true })
-          .first()
-          .getAttribute('href'),
-      ).toEqual('https://www.kontur.io/demo-call/'),
+        this.page.getByText('Request trial', { exact: true }).first(),
+      ).toHaveAttribute('href', 'https://www.kontur.io/demo-call/'),
       expect(this.page.getByText('Contact sales', { exact: true })).toBeVisible(),
       plansAndPricingTexts.forEach((text) => expect(text).toBeVisible()),
       expect(this.page.getByText('Save 5%', { exact: true })).toBeVisible(),
