35a830c1622fcd62cd2a8a18cd42c1d862117cf8
Aliaksei Murashka <amurashka@kontur.io>
2024-11-18 17:34:58 +0300

    test: added fixes of floating open osm tests

--- e2e/openOsmUpdateProfileWithPro.spec.ts
+++ e2e/openOsmUpdateProfileWithPro.spec.ts
@@ -77,15 +77,18 @@ test.describe(`As PRO User, I can use different OSM editors to open the map and 
           pageManager.atNavigationMenu.clickButtonToOpenPage('Map'),
           pageManager.atMap.waitForUrlToMatchPattern(/\?map=/i, page, 'domcontentloaded'),
         ]);
-        await pageManager.atMap.waitForTextBeingVisible('Tools');
+        await pageManager.atMap.waitForTextBeingVisible('Tools', page);
+
+        // TO DO: async behaviour of app being loaded is not stable and waiting for events like router-layout-ready is not possible for already loaded app, so waiting for timeout is needed + to emulate real user behaviour
+        await page.waitForTimeout(process.env.CI ? 7000 : 5000);
 
         const coordinates = await pageManager.atMap.getViewportFromUrl();
 
-        const editMapBtn = pageManager.atToolBar.getButtonByText('Edit map in OSM');
+        const editMapBtn = pageManager.atToolBar.getButtonByText('Edit map in OSM', page);
         await editMapBtn.hover();
 
         const [newPage] = await Promise.all([
-          context.waitForEvent('page', { timeout: 35000 }),
+          context.waitForEvent('page', { timeout: 25000 }),
           editMapBtn.click({
             delay: 330,
           }),
--- e2e/openOsmWithUser.spec.ts
+++ e2e/openOsmWithUser.spec.ts
@@ -18,15 +18,18 @@ for (const project of projects) {
       pageManager.atNavigationMenu.clickButtonToOpenPage('Map'),
       pageManager.atMap.waitForUrlToMatchPattern(/\?map=/i, page, 'domcontentloaded'),
     ]);
-    await pageManager.atMap.waitForTextBeingVisible('Tools');
+    await pageManager.atMap.waitForTextBeingVisible('Tools', page);
+
+    // TO DO: async behaviour of app being loaded is not stable and waiting for events like router-layout-ready is not possible for already loaded app, so waiting for timeout is needed + to emulate real user behaviour
+    await page.waitForTimeout(process.env.CI ? 7000 : 5000);
 
     const coordinates = await pageManager.atMap.getViewportFromUrl();
 
-    const editMapBtn = pageManager.atToolBar.getButtonByText('Edit map in OSM');
+    const editMapBtn = pageManager.atToolBar.getButtonByText('Edit map in OSM', page);
     await editMapBtn.hover();
 
     const [newPage] = await Promise.all([
-      context.waitForEvent('page'),
+      context.waitForEvent('page', { timeout: 25000 }),
       editMapBtn.click({
         delay: 330,
       }),
