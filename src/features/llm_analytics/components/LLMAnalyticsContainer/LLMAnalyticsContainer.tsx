2541ab3d1d49b33d0be11e81b669d36c717f735e
Aliaksandr Baranau <abaranau.dev@gmail.com>
2024-11-19 09:58:10 +0100

    feat(LLM analytics): 19963 updated LLM analytics help section

--- src/features/llm_analytics/components/LLMAnalyticsContainer/LLMAnalyticsContainer.tsx
+++ src/features/llm_analytics/components/LLMAnalyticsContainer/LLMAnalyticsContainer.tsx
@@ -4,10 +4,10 @@ import { ErrorMessage } from '~components/ErrorMessage/ErrorMessage';
 import { createStateMap } from '~utils/atoms';
 import { focusedGeometryAtom } from '~core/focused_geometry/model';
 import { PagesDocument } from '~core/pages';
-import { i18n } from '~core/localization';
 import { llmAnalyticsAtom } from '~features/llm_analytics/atoms/llmAnalyticsResource';
 import { LLMAnalyticsEmptyState } from '../LLMAnalyticsEmptyState/LLMAnalyticsEmptyState';
 import { MarkdownWrapper } from '../MarkdownWrapper/MarkdownWrapper';
+import { LLMAnalyticsPlaceholder } from '../LLMAnalyticsPlaceholder/LLMAnalyticsPlaceholder';
 
 export const LLMAnalyticsContainer = () => {
   const [{ error, loading, data }] = useAtom(llmAnalyticsAtom);
@@ -33,12 +33,9 @@ export const LLMAnalyticsContainer = () => {
       }
       return (
         <>
+          <LLMAnalyticsPlaceholder />
           <PagesDocument
             doc={[
-              {
-                type: 'md',
-                data: i18n.t('llm_analytics.header_info'),
-              },
               {
                 type: 'md',
                 data: data.data,
---
+++ src/features/llm_analytics/components/LLMAnalyticsPlaceholder/LLMAnalyticsPlaceholder.tsx
@@ -0,0 +1,69 @@
+import {
+  EditGeometry16,
+  Play24,
+  Reference16,
+  SelectArea16,
+  Upload16,
+  User24,
+} from '@konturio/default-icons';
+import { Text } from '@konturio/ui-kit';
+import { Trans } from 'react-i18next';
+import { goTo } from '~core/router/goTo';
+import { i18n } from '~core/localization';
+import styles from './LLMAnalyticsPlaceholder.module.css';
+
+export function LLMAnalyticsPlaceholder() {
+  return (
+    <div className={styles.placeholderBody}>
+      <div className={styles.mainText}>
+        <Trans
+          i18nKey="llm_analytics.guide.select_area"
+          components={{
+            icon1: <SelectArea16 />,
+            icon2: <EditGeometry16 />,
+            icon3: <Upload16 />,
+          }}
+        />
+      </div>
+      <div>
+        <Text type="long-m">{i18n.t('llm_analytics.guide.you_can_also')}</Text>
+      </div>
+      <div>
+        <Trans
+          i18nKey="llm_analytics.guide.fill_bio"
+          components={{
+            icon: <User24 />,
+            lnk: (
+              <span
+                className={styles.clickableText}
+                onClick={() => goTo('/profile')}
+              ></span>
+            ),
+          }}
+        />
+      </div>
+      <div>
+        <Trans
+          i18nKey="llm_analytics.guide.select_and_save_as_reference_area"
+          components={{
+            icon: <Reference16 />,
+          }}
+        />
+      </div>
+      <div>
+        <Trans
+          i18nKey="llm_analytics.guide.learn_more"
+          components={{
+            icon: <Play24 />,
+            lnk: (
+              <span
+                className={styles.clickableText}
+                onClick={() => goTo('/profile')}
+              ></span>
+            ),
+          }}
+        />
+      </div>
+    </div>
+  );
+}
