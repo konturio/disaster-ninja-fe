6a0d69788323492f64fa22d876202fca59ca2320
Aliaksandr Baranau <abaranau.dev@gmail.com>
2024-11-20 01:36:00 +0100

    chore: viewport -> camera

--- src/core/shared_state/currentMapPosition.ts
+++ src/core/shared_state/currentMapPosition.ts
@@ -57,10 +57,10 @@ export const currentMapPositionAtom = createAtom(
       let position = { bbox: bbox.flat() } as MapPosition;
       const map = currentMapAtom.getState();
       if (map) {
-        const viewport = getCameraForBbox(bbox, map);
-        if (viewport.center && 'lng' in viewport.center) {
-          const { zoom } = viewport;
-          const { lat, lng } = viewport.center;
+        const camera = getCameraForBbox(bbox, map);
+        if (camera.center && 'lng' in camera.center) {
+          const { zoom } = camera;
+          const { lat, lng } = camera.center;
           position = { ...position, lat, lng, zoom: zoom ?? map.getZoom() };
           jumpTo(map, position);
         }
