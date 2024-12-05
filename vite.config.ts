f0da992e328d0579d478249fb6896287e105cfc1
VK <vkozel@kontur.io>
2024-12-05 13:47:35 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -97,18 +97,18 @@ export default ({ mode }) => {
         // experimentalLogSideEffects: true,
         output: {
           manualChunks(id: string, { getModuleInfo, getModuleIds }) {
-            // react
-            if (id.includes('node_modules/react/')) return 'react';
-            if (id.includes('node_modules/react-dom/')) return 'react-dom';
+            // // react
+            // if (id.includes('node_modules/react/')) return 'react';
+            // if (id.includes('node_modules/react-dom/')) return 'react-dom';
 
-            // nebula
-            if (id.includes('node_modules/@loaders.gl')) return 'loaders-gl';
-            if (id.includes('node_modules/@luma.gl')) return 'luma-gl';
-            if (id.includes('node_modules/@deck.gl')) return 'deck-gl';
+            // // nebula
+            // if (id.includes('node_modules/@loaders.gl')) return 'loaders-gl';
+            // if (id.includes('node_modules/@luma.gl')) return 'luma-gl';
+            // if (id.includes('node_modules/@deck.gl')) return 'deck-gl';
 
-            // etc
-            if (id.includes('node_modules/recharts')) return 'recharts';
-            if (id.includes('node_modules/maplibre-gl')) return 'maplibre-gl';
+            // // etc
+            // if (id.includes('node_modules/recharts')) return 'recharts';
+            // if (id.includes('node_modules/maplibre-gl')) return 'maplibre-gl';
           },
           // experimentalMinChunkSize: 16000,
         },
