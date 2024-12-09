cd7aa53a822541446522b6f4f05bf83c659bf42c
Aliaksei Murashka <112632451+amurKontur@users.noreply.github.com>
2024-12-09 23:30:53 +0100

    [FE] Fix failing tests (#920)
    * test: updated registration and privacy tests
    
    * test: used methods chaining for registration test

--- e2e/registration.spec.ts
+++ e2e/registration.spec.ts
@@ -1,124 +1,136 @@
 import { expect } from '@playwright/test';
 import { faker } from '@faker-js/faker';
 import { test } from './fixtures/test-options.ts';
-import { getProjects } from './page-objects/helperBase.ts';
+import { getProjects, getTestData } from './page-objects/helperBase.ts';
 import type { Project } from './page-objects/helperBase.ts';
 import type { APIRequestContext } from '@playwright/test';
 
+const countriesToTest = ['us', 'ca', 'pl'];
+
 const projects = getProjects().filter((project: Project) => project.env !== 'prod');
+
+const testedPhoneByCountry: Array<[string, string]> = getTestData(
+  'phone-by-country',
+).filter((countryPhone: [string, string]) => countriesToTest.includes(countryPhone[0]));
 
 // Registration tests should run one by one not to kill application
 test.describe.configure({ mode: 'serial' });
 
-// Create a loop to loop over all the projects and create a test for everyone
-for (const project of projects) {
-  test(`As Guest, I can register at ${project.title}, verify email, login and check that this profile is mine`, async ({
-    page,
-    pageManager,
-    context,
-    playwright,
-  }) => {
-    const firstName = faker.person.firstName();
-    const lastName = faker.person.lastName();
-    const fullName = `${firstName} ${lastName}`;
-    // E.164 format for phone numbers
-    const phone = `+${faker.number.int({ min: 1, max: 999 })}${faker.string.numeric(10)}`;
-    // LinkedIn URL format
-    const linkedin = `https://www.linkedin.com/in/${faker.internet.userName()}`;
-    const username = `${firstName}${lastName}${faker.number.int(1000)}`.toLowerCase();
-    const email = `${username}@testdeleteme.com`;
+for (const [countryCode, fullPhone] of testedPhoneByCountry) {
+  const [phoneCode, ...numbers] = fullPhone.split(' ');
+  const phone = numbers.join('');
 
-    const password = faker.internet.password({ length: 12 });
+  // Create a loop to loop over all the projects and create a test for everyone
+  for (const project of projects) {
+    test(`As Guest, I can register at ${project.title} with ${countryCode.toUpperCase()} phone, verify email, login and check that this profile is mine`, async ({
+      page,
+      pageManager,
+      context,
+      playwright,
+    }) => {
+      const firstName = faker.person.firstName();
+      const lastName = faker.person.lastName();
+      const fullName = `${firstName} ${lastName}`;
 
-    await pageManager.atBrowser.openProject(project);
-    await pageManager.atNavigationMenu.clickButtonToOpenPage('Login');
+      // LinkedIn URL format
+      const linkedin = `https://www.linkedin.com/in/${faker.internet.userName()}`;
+      const username = `${firstName}${lastName}${faker.number.int(1000)}`.toLowerCase();
+      const email = `${username}@testdeleteme.com`;
 
-    const keycloakPage =
-      await pageManager.atLoginPage.clickSignUpAndNavigateToKeycloak(context);
+      const password = faker.internet.password({ length: 12 });
 
-    // Create new context for API separate from browser context
-    const apiContext: APIRequestContext = await playwright.request.newContext();
+      await pageManager.atBrowser.openProject(project);
+      await pageManager.atNavigationMenu.clickButtonToOpenPage('Login');
 
-    // Register and get admin token in parallel
-    const [_, adminToken] = await Promise.all([
-      pageManager.atKeycloakPage.registerAndSeeVerificationEmailInfo(
-        project,
-        keycloakPage,
-        {
-          fullName,
-          email,
-          password,
-          phone,
-          linkedin,
-        },
-      ),
-      pageManager.atKeycloakPage.getAdminToken({
-        project,
-        apiContext,
-        adminName: process.env.ADMIN_KEYCLOAK!,
-        adminPassword: process.env.ADMIN_KEYCLOAK_PASSWORD!,
-      }),
-    ]);
+      const keycloakPage =
+        await pageManager.atLoginPage.clickSignUpAndNavigateToKeycloak(context);
 
-    const userId = await pageManager.atKeycloakPage.verifyEmail({
-      project,
-      apiContext,
-      email,
-      adminToken,
-      adminName: process.env.ADMIN_KEYCLOAK!,
-      adminPassword: process.env.ADMIN_KEYCLOAK_PASSWORD!,
-      username,
-    });
+      // Create new context for API separate from browser context
+      const apiContext: APIRequestContext = await playwright.request.newContext();
+      // Register and get admin token in parallel
+      const [_, adminToken] = await Promise.all([
+        pageManager.atKeycloakPage.registerAndSeeVerificationEmailInfo(
+          project,
+          keycloakPage,
+          {
+            fullName,
+            email,
+            password,
+            phoneCode,
+            countryCode,
+            phone,
+            linkedin,
+          },
+        ),
+        pageManager.atKeycloakPage.getAdminToken({
+          project,
+          apiContext,
+          adminName: process.env.ADMIN_KEYCLOAK!,
+          adminPassword: process.env.ADMIN_KEYCLOAK_PASSWORD!,
+        }),
+      ]);
 
-    await pageManager.atBrowser.openProject(project, { skipCookieBanner: true });
-    await pageManager.atNavigationMenu.clickButtonToOpenPage('Login');
-    await pageManager.atLoginPage.typeLoginPasswordAndLogin(email, password, {
-      project,
-      shouldSuccess: true,
-    });
-
-    // Atlas redirects to pricing page after login
-    if (project.name === 'atlas') {
-      await pageManager.atPricingPage.checkPageAndTextsAvailability();
-      await pageManager.atPricingPage.clickBtnAndAssertUrl({
-        context,
-        buttonName: 'Request trial',
-        expectedUrlPart: 'demo-call',
-      });
-      await pageManager.atPricingPage.clickBtnAndAssertUrl({
-        context,
-        buttonName: 'Book a demo',
-        expectedUrlPart: 'atlas-demo',
-      });
-      await pageManager.atNavigationMenu.clickButtonToOpenPage('Profile');
-    }
-
-    await pageManager.atProfilePage.checkLogoutBtnProfileTitleAndEmail(email);
-
-    const fullNameAfterRegistration = await pageManager.atProfilePage.getFullNameValue();
-    expect(fullNameAfterRegistration).toEqual(fullName);
-
-    // Without logout app will crash after current user deletion
-    await pageManager.atProfilePage.clickLogout();
-
-    // Get new token and wait for 1 sec in parallel
-    const [newAdminToken] = await Promise.all([
-      pageManager.atKeycloakPage.getAdminToken({
+      const userId = await pageManager.atKeycloakPage.verifyEmail({
         project,
         apiContext,
+        email,
+        adminToken,
         adminName: process.env.ADMIN_KEYCLOAK!,
         adminPassword: process.env.ADMIN_KEYCLOAK_PASSWORD!,
-      }),
-      page.waitForTimeout(1000),
-    ]);
+        username,
+      });
 
-    // It won't delete test user in case of failed test,
-    // But it is the safest way to clean up
-    await pageManager.atKeycloakPage.deleteUserById({
-      adminToken: newAdminToken,
-      project,
-      apiContext,
-      userId,
+      await pageManager.atBrowser.openProject(project, { skipCookieBanner: true });
+      await pageManager.atNavigationMenu.clickButtonToOpenPage('Login');
+      await pageManager.atLoginPage.typeLoginPasswordAndLogin(email, password, {
+        project,
+        shouldSuccess: true,
+      });
+
+      // Atlas redirects to pricing page after login
+      if (project.name === 'atlas') {
+        await pageManager.atPricingPage.checkPageAndTextsAvailability();
+        await pageManager.atPricingPage.clickBtnAndAssertUrl({
+          context,
+          buttonName: 'Request trial',
+          expectedUrlPart: 'demo-call',
+        });
+        await pageManager.atPricingPage.clickBtnAndAssertUrl({
+          context,
+          buttonName: 'Book a demo',
+          expectedUrlPart: 'atlas-demo',
+        });
+        await pageManager.atNavigationMenu.clickButtonToOpenPage('Profile');
+      }
+
+      await pageManager.atProfilePage.checkLogoutBtnProfileTitleAndEmail(email);
+
+      const fullNameAfterRegistration =
+        await pageManager.atProfilePage.getFullNameValue();
+      expect(fullNameAfterRegistration).toEqual(fullName);
+
+      // Without logout app will crash after current user deletion
+      await pageManager.atProfilePage.clickLogout();
+
+      // Get new token and wait for 1 sec in parallel
+      const [newAdminToken] = await Promise.all([
+        pageManager.atKeycloakPage.getAdminToken({
+          project,
+          apiContext,
+          adminName: process.env.ADMIN_KEYCLOAK!,
+          adminPassword: process.env.ADMIN_KEYCLOAK_PASSWORD!,
+        }),
+        page.waitForTimeout(1000),
+      ]);
+
+      // It won't delete test user in case of failed test,
+      // But it is the safest way to clean up
+      await pageManager.atKeycloakPage.deleteUserById({
+        adminToken: newAdminToken,
+        project,
+        apiContext,
+        userId,
+      });
     });
-  });
+  }
 }
