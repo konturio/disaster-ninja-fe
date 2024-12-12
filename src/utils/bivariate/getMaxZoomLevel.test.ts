1e4cb3a89b75a9ca746c62f369dc87e91f8885ff
Aliaksandr Baranau <abaranau.dev@gmail.com>
2024-12-12 18:38:27 +0100

    test: update tests

--- src/utils/bivariate/getMaxZoomLevel.test.ts
+++ src/utils/bivariate/getMaxZoomLevel.test.ts
@@ -1,47 +1,62 @@
 import { describe, expect, it } from 'vitest';
-import { getMaxIndicatorsZoomLevel } from './getMaxZoomLevel';
+import { getMaxNumeratorZoomLevel } from './getMaxZoomLevel';
 import type { Indicator } from '~utils/bivariate';
 
 describe('getMaxIndicatorsZoomLevel(', () => {
-  it('must return the biggest zoom level if all indicators have maxZoom property', () => {
-    expect(getMaxIndicatorsZoomLevel(TEST_INDICATORS, 14)).toEqual(20);
+  it('must return the biggest zoom level of all numerator that have maxZoom property', () => {
+    expect(
+      getMaxNumeratorZoomLevel(
+        [axisWithZoomLevels(11, 14), axisWithZoomLevels(9, 14)],
+        15,
+      ),
+    ).toEqual(11);
   });
 
-  it('must return fallback value if no indicators have maxZoom property', () => {
-    const indicatorsWithoutZoomLevel = structuredClone(TEST_INDICATORS);
-    indicatorsWithoutZoomLevel[0].maxZoom = undefined;
-    indicatorsWithoutZoomLevel[1].maxZoom = undefined;
-    expect(getMaxIndicatorsZoomLevel(indicatorsWithoutZoomLevel, 14)).toEqual(14);
+  it('must return fallback value if no numerators have maxZoom property', () => {
+    expect(
+      getMaxNumeratorZoomLevel(
+        [axisWithZoomLevels(undefined, 7), axisWithZoomLevels(undefined, 9)],
+        15,
+      ),
+    ).toEqual(15);
   });
 
-  it('must return the biggest zoom value if at least one indicator has maxZoom property', () => {
-    const indicatorsWithSingleZoomLevel = structuredClone(TEST_INDICATORS);
-    indicatorsWithSingleZoomLevel[0].maxZoom = undefined;
-    expect(getMaxIndicatorsZoomLevel(indicatorsWithSingleZoomLevel, 14)).toEqual(9);
+  it('must return the biggest maxZoom value if at least one numerator has maxZoom property', () => {
+    expect(
+      getMaxNumeratorZoomLevel(
+        [axisWithZoomLevels(undefined, 14), axisWithZoomLevels(9, 14)],
+        20,
+      ),
+    ).toEqual(9);
   });
 });
 
-const TEST_INDICATORS: Indicator[] = [
-  {
-    name: 'indicator_numerator',
-    label: 'Numerator label',
-    unit: {
-      id: 'celc_deg',
-      shortName: '°C',
-      longName: 'degrees Celsius',
+function axisWithZoomLevels(
+  numeratorMaxZoom: number | undefined,
+  denominatorMaxZoom?: number | undefined,
+): Indicator[] {
+  return [
+    {
+      name: 'indicator_numerator',
+      label: 'Numerator label',
+      unit: {
+        id: 'celc_deg',
+        shortName: '°C',
+        longName: 'degrees Celsius',
+      },
+      direction: [['unimportant'], ['important']],
+      maxZoom: numeratorMaxZoom,
     },
-    direction: [['unimportant'], ['important']],
-    maxZoom: 20,
-  },
-  {
-    name: 'indicator_denominator',
-    label: 'Denominator label',
-    unit: {
-      id: 'null',
-      shortName: null,
-      longName: null,
+    {
+      name: 'indicator_denominator',
+      label: 'Denominator label',
+      unit: {
+        id: 'null',
+        shortName: null,
+        longName: null,
+      },
+      direction: [['unimportant'], ['important']],
+      maxZoom: denominatorMaxZoom,
     },
-    direction: [['unimportant'], ['important']],
-    maxZoom: 9,
-  },
-];
+  ];
+}
