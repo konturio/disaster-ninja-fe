9ea657f20ee1fb2c8e8d4914d956ad5dbd34d297
Natallia-Harshunova <169041964+Natallia-Harshunova@users.noreply.github.com>
2024-12-19 11:41:52 +0300

    fix(20444): fix crosshair on top of boundaries marker (#936)
    Co-authored-by: Natallia Harshunova <nattallius@gmail.com>

--- src/utils/map/markers.ts
+++ src/utils/map/markers.ts
@@ -3,15 +3,16 @@ import { ApplicationMapMarker } from '~components/ConnectedMap/ConnectedMap';
 import type { Marker } from '~core/types';
 
 // wrap react element into HTMLElement
-function renderInline(reactElement: React.ReactElement) {
+function renderInline(marker: Marker) {
   const container = document.createElement('div');
+  container.className = marker.wrapperClass || '';
   const root = createRoot(container);
-  root.render(reactElement);
+  root.render(marker.el);
   return container;
 }
 
 export function convertToAppMarker(id: string, marker: Marker): ApplicationMapMarker {
-  const appMarker = new ApplicationMapMarker(id, renderInline(marker.el));
+  const appMarker = new ApplicationMapMarker(id, renderInline(marker));
   appMarker.setLngLat(marker.coordinates);
   return appMarker;
 }
