09010ff42841a588ccb1a35feedca5780c1d5612
Aliaksandr Baranau <abaranau.dev@gmail.com>
2024-12-23 12:25:57 +0100

    chore: refactor eventMcdaCriteriaExtractor into several smaller functions

--- src/features/events_list/helpers/eventsMcdaSort.ts
+++ src/features/events_list/helpers/eventsMcdaSort.ts
@@ -8,7 +8,30 @@ import { isNumber } from '~utils/common';
 import { EVENT_TYPES_SORTING_SCORES, SEVERITY_SORTING_SCORES } from '../constants';
 import type { Event } from '~core/types';
 
-const extractor: MCDASortCriteriaExtractor<Event> = (event: Event) => {
+function getAffectedPopulationDensity(event: Event) {
+  return isNumber(event.affectedPopulation) &&
+    isNumber(event.settledArea) &&
+    event.settledArea > 0
+    ? event.affectedPopulation / event.settledArea
+    : 0;
+}
+
+function getSettledArea(event: Event) {
+  return Math.log(
+    1 + (isNumber(event.settledArea) && event.settledArea >= 0 ? event.settledArea : 0),
+  );
+}
+
+function getAffectedPopulation(event: Event) {
+  return Math.log(
+    1 +
+      (isNumber(event.affectedPopulation) && event.affectedPopulation >= 0
+        ? event.affectedPopulation
+        : 0),
+  );
+}
+
+const eventMcdaCriteriaExtractor: MCDASortCriteriaExtractor<Event> = (event: Event) => {
   const criteriaMap = new Map<string, number>();
   // eventType
   criteriaMap.set(
@@ -21,30 +44,14 @@ const extractor: MCDASortCriteriaExtractor<Event> = (event: Event) => {
     SEVERITY_SORTING_SCORES[event.severity] ?? SEVERITY_SORTING_SCORES.UNKNOWN,
   );
   // affectedPopulation
-  criteriaMap.set(
-    'affectedPopulation',
-    Math.log(
-      1 +
-        (isNumber(event.affectedPopulation) && event.affectedPopulation >= 0
-          ? event.affectedPopulation
-          : 0),
-    ),
-  );
+  criteriaMap.set('affectedPopulation', getAffectedPopulation(event));
   // settledArea
-  criteriaMap.set(
-    'settledArea',
-    Math.log(
-      1 + (isNumber(event.settledArea) && event.settledArea >= 0 ? event.settledArea : 0),
-    ),
-  );
+  criteriaMap.set('settledArea', getSettledArea(event));
   // affectedPopulationDensity
-  const affectedPopulationDensity =
-    isNumber(event.affectedPopulation) &&
-    isNumber(event.settledArea) &&
-    event.settledArea > 0
-      ? event.affectedPopulation / event.settledArea
-      : 0;
-  criteriaMap.set('affectedPopulationDensity', Math.log(1 + affectedPopulationDensity));
+  criteriaMap.set(
+    'affectedPopulationDensity',
+    Math.log(1 + getAffectedPopulationDensity(event)),
+  );
   // updatedAt
   if (event.updatedAt) {
     criteriaMap.set('updatedAt', new Date(event.updatedAt).getTime());
@@ -66,5 +73,5 @@ export function sortEventsByMcda(
     console.warn('sortEventsByMcda: incorrect config. Returning the original array');
     return events;
   }
-  return sortByMCDAScore(events, config, extractor, sortOrder);
+  return sortByMCDAScore(events, config, eventMcdaCriteriaExtractor, sortOrder);
 }
