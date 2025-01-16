6d971b3a747c08f6426eedcc405011167b8cac15
Aliaksandr Baranau <abaranau.dev@gmail.com>
2025-01-16 12:34:36 +0100

    feat: show Bivariate legend for Multivariate layer

--- src/components/MultivariateLegend/MultivariateLegend.tsx
+++ src/components/MultivariateLegend/MultivariateLegend.tsx
@@ -1,11 +1,26 @@
-import { MCDALegend } from '@konturio/ui-kit';
+import { Legend as BiLegend, MCDALegend } from '@konturio/ui-kit';
 import { generateMCDALegendColors } from '~utils/mcda/mcdaLegendsUtils';
+import { BIVARIATE_LEGEND_SIZE } from '~components/BivariateLegend/const';
+import { DEFAULT_MULTIBIVARIATE_STEPS } from '~utils/multivariate/constants';
+import { invertClusters } from '~utils/bivariate';
 import s from './MultivariateLegend.module.css';
-import type { MCDAConfig } from '~core/logical_layers/renderers/stylesConfigs/mcda/types';
+import type { ColorTheme } from '~core/types';
 import type { MultivariateLayerConfig } from '~core/logical_layers/renderers/MultivariateRenderer/types';
+import type { MCDAConfig } from '~core/logical_layers/renderers/stylesConfigs/mcda/types';
+import type { Cell } from '@konturio/ui-kit/tslib/Legend/types';
 
 export type MultivariateLegendProps = {
   config: MultivariateLayerConfig;
+};
+
+type MultiBivariateLegendAxisProp = {
+  label: string;
+  steps: {
+    label?: string;
+    value: number;
+  }[];
+  quality?: number;
+  quotient: [string, string];
 };
 
 function createMCDALegend(mcdaConfig: MCDAConfig): JSX.Element {
@@ -16,10 +31,38 @@ function createMCDALegend(mcdaConfig: MCDAConfig): JSX.Element {
   return <MCDALegend title={mcdaConfig.name} steps={5} colors={legendColors} />;
 }
 
+function createBivariateLegend(base: MCDAConfig, annex: MCDAConfig, colors: ColorTheme) {
+  const xAxis: MultiBivariateLegendAxisProp = {
+    label: base.name,
+    steps: DEFAULT_MULTIBIVARIATE_STEPS,
+    quotient: ['', ''],
+  };
+  const yAxis: MultiBivariateLegendAxisProp = {
+    label: annex.name,
+    steps: DEFAULT_MULTIBIVARIATE_STEPS,
+    quotient: ['', ''],
+  };
+  const cells = invertClusters(
+    colors.map(({ id, color, isFallbackColor }) => ({
+      label: id,
+      color,
+      isFallbackColor,
+    })),
+    'label',
+  ) as Cell[];
+  return (
+    <BiLegend cells={cells} size={BIVARIATE_LEGEND_SIZE} axis={{ x: xAxis, y: yAxis }} />
+  );
+}
+
 export function MultivariateLegend({ config }: MultivariateLegendProps) {
-  if (!config?.annex) {
-    return createMCDALegend(config.base.config);
+  if (config.annex && config.colors?.type === 'bivariate') {
+    return createBivariateLegend(
+      config.base.config,
+      config.annex.config,
+      config.colors.colors,
+    );
   } else {
-    return <div className={s.legendName}>[ MultivariateLegend ]</div>;
+    return createMCDALegend(config.base.config);
   }
 }
