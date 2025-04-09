20f43db0f3f75df8c428793887127aaed70efcea
Aliaksandr Baranau <abaranau.dev@gmail.com>
2025-04-09 17:55:40 +0200

    feat: change default bivariate color scheme for Multivariate layers

--- src/utils/multivariate/constants.ts
+++ src/utils/multivariate/constants.ts
@@ -18,38 +18,38 @@ export const DEFAULT_MULTIBIVARIATE_STEPS: Step[] = [
 export const DEFAULT_MULTIBIVARIATE_COLORS = [
   {
     id: 'A1',
-    color: 'rgba(204,204,204,0.5)',
+    color: 'rgba(232, 232, 157, 0.5)',
   },
   {
     id: 'A2',
-    color: 'rgba(206,154,151,0.5)',
+    color: 'rgba(229, 154, 55, 0.5)',
   },
   {
     id: 'A3',
-    color: 'rgba(228,26,28,0.5)',
+    color: 'rgba(228, 26, 28, 0.5)',
   },
   {
     id: 'B1',
-    color: 'rgba(136,135,222,0.5)',
+    color: 'rgba(215, 242, 224, 0.5)',
   },
   {
     id: 'B2',
-    color: 'rgba(166,166,166,0.5)',
+    color: 'rgba(228, 185, 129, 0.5)',
   },
   {
     id: 'B3',
-    color: 'rgba(180,80,75,0.5)',
+    color: 'rgba(228, 127, 129, 0.5)',
   },
   {
     id: 'C1',
-    color: 'rgba(28,26,228,0.5)',
+    color: 'rgba(177, 228, 194, 0.5)',
   },
   {
     id: 'C2',
-    color: 'rgba(78,77,178,0.5)',
+    color: 'rgba(172, 216, 170, 0.5)',
   },
   {
     id: 'C3',
-    color: 'rgba(128,128,128,0.5)',
+    color: 'rgba(174, 205, 184, 0.5)',
   },
 ];
