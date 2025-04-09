ade85fe19c0c8d9b52e73e528afaa3ca034f0408
Aliaksandr Baranau <abaranau.dev@gmail.com>
2025-04-09 19:33:03 +0200

    chore: update labels on bivariate legend

--- src/components/MultivariateLegend/MultivariateLegend.tsx
+++ src/components/MultivariateLegend/MultivariateLegend.tsx
@@ -4,6 +4,7 @@ import { BIVARIATE_LEGEND_SIZE } from '~components/BivariateLegend/const';
 import { DEFAULT_MULTIBIVARIATE_STEPS } from '~utils/multivariate/constants';
 import { invertClusters, type Step } from '~utils/bivariate';
 import { CornerTooltipWrapper } from '~components/BivariateLegend/CornerTooltipWrapper';
+import { i18n } from '~core/localization';
 import { DEFAULT_BASE_DIRECTION, DEFAULT_SCORE_DIRECTION } from './constants';
 import type { Direction } from '~utils/bivariate';
 import type { LayerMeta } from '~core/logical_layers/types/meta';
@@ -58,12 +59,12 @@ function createBivariateLegend(
   colors: ColorTheme,
 ) {
   const xAxis: MultiBivariateLegendAxisProp = {
-    label: base.name,
+    label: i18n.t('multivariate.compare'),
     steps: baseSteps,
     quotient: ['', ''],
   };
   const yAxis: MultiBivariateLegendAxisProp = {
-    label: score.name,
+    label: i18n.t('multivariate.score'),
     steps: scoreSteps,
     quotient: ['', ''],
   };
