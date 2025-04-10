5c5b809299b5961f7467e98465f2b2bd199e462d
Aliaksei Murashka <amurashka@kontur.io>
2025-04-11 02:55:44 +0300

    test: update fixme links

--- e2e/reloadWithPro.spec.ts
+++ e2e/reloadWithPro.spec.ts
@@ -15,7 +15,7 @@ for (const project of projects) {
     test(`Url of map is still the same`, async ({ pageManager }) => {
       test.fixme(
         project.name === 'oam',
-        'Fix https://kontur.fibery.io/Tasks/Task/routing-oam-url-param-map-2.122--0.000-0.000-is-opened-first-instead-of-map-2.122-0.000-0.000-19889 to unblock oam test',
+        'Fix https://kontur.fibery.io/Tasks/Task/reopen-routing-oam-url-param-map-2.122--0.000-0.000-is-opened-first-instead-of-map-2.122-0.000-0.000-21381 to unblock oam test',
       );
       await pageManager.atNavigationMenu.clickButtonToOpenPage('Map');
       if (project.name !== 'disaster-ninja')
@@ -27,7 +27,7 @@ for (const project of projects) {
     test('My profile has the same data', async ({ pageManager }) => {
       test.fixme(
         project.name === 'oam',
-        'Fix https://kontur.fibery.io/Tasks/Task/routing-oam-url-param-map-2.122--0.000-0.000-is-opened-first-instead-of-map-2.122-0.000-0.000-19889 to unblock oam test',
+        'Fix https://kontur.fibery.io/Tasks/Task/reopen-routing-oam-url-param-map-2.122--0.000-0.000-is-opened-first-instead-of-map-2.122-0.000-0.000-21381 to unblock oam test',
       );
       await pageManager.atNavigationMenu.clickButtonToOpenPage('Profile');
       const settingsValues = await pageManager.atProfilePage.getAndAssertProfileData(
