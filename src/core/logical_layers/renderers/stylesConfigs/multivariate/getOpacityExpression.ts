37cdbcefeb3678febc119db8b61053d9e45d0ebf
Aliaksandr Baranau <abaranau.dev@gmail.com>
2025-04-25 15:21:04 +0200

    feat: change default opacity percentage

--- src/core/logical_layers/renderers/stylesConfigs/multivariate/getOpacityExpression.ts
+++ src/core/logical_layers/renderers/stylesConfigs/multivariate/getOpacityExpression.ts
@@ -4,5 +4,5 @@ import type { MapExpression } from '../mcda/calculations/operations';
 
 export function getOpacityExpression(opacity: MCDALayerStyle | number): MapExpression {
   const opacityScore = multivariateAxisToScore(opacity) as unknown as MapExpression;
-  return ['case', ['>=', opacityScore, 0.66], 1, ['>=', opacityScore, 0.33], 0.65, 0.3];
+  return ['case', ['>=', opacityScore, 0.66], 1, ['>=', opacityScore, 0.33], 0.55, 0.1];
 }
