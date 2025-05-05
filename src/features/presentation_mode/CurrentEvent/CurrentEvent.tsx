7de89ccdfd62c4e353d53767238572bcf5be3a9b
Natallia Harshunova <nattallius@gmail.com>
2025-05-05 10:16:18 +0200

    feat(20921): small fixes

--- src/features/presentation_mode/CurrentEvent/CurrentEvent.tsx
+++ src/features/presentation_mode/CurrentEvent/CurrentEvent.tsx
@@ -1,23 +1,15 @@
 import { useAtom } from '@reatom/npm-react';
 import { currentEventResourceAtom } from '~core/shared_state/currentEventResource';
-import {
-  useUniLayoutContextValue,
-  UniLayoutContext,
-} from '~components/Uni/Layout/UniLayoutContext';
 import { eventCardLayoutTemplate } from '~features/events_list/components/EventsPanel/eventLayouts';
 import { UniLayout } from '~components/Uni/Layout/UniLayout';
 
 export const CurrentEvent = () => {
   const [{ data: currentEvent }] = useAtom(currentEventResourceAtom.v3atom);
 
-  const layoutContextValue = useUniLayoutContextValue({
-    layout: eventCardLayoutTemplate,
-  });
-
   if (!currentEvent) return null;
 
   return (
-    <div className="knt-panel knt-current-event" style={{ zoom: 1.4 }}>
+    <div className="knt-panel knt-current-event">
       <UniLayout data={currentEvent} layout={eventCardLayoutTemplate}></UniLayout>
     </div>
   );
