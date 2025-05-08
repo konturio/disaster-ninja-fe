22955b7c4b17502e340dd005edceecf1d330e782
Natallia Harshunova <nattallius@gmail.com>
2025-05-08 23:55:25 +0200

    feat(20921): add app features

--- src/views/Map/Layouts/Presentation/Presentation.tsx
+++ src/views/Map/Layouts/Presentation/Presentation.tsx
@@ -3,25 +3,31 @@ import { MapTitle } from '~features/presentation_mode/MapTitle/MapTitle';
 import { LayersCopyrights } from '~components/LayersCopyrights/LayersCopyrights';
 import { LegendsPanel } from '~features/presentation_mode/LegendsPanel/LegendsPanel';
 import { CurrentEvent } from '~components/CurrentEvent/CurrentEvent';
+import { configRepo } from '~core/config';
+import { AppFeature } from '~core/app/types';
 import s from './Presentation.module.css';
 import type { ReactNode } from 'react';
+
+const featureFlags = configRepo.get().features;
 
 export function PresentationLayout({ scaleAndLogo }: { scaleAndLogo?: ReactNode }) {
   return (
     <div className={s.container}>
       <div className={s.headerRow}>
         <div className={s.breadcrumbs}>
-          <BreadcrumbsPanel />
+          {featureFlags[AppFeature.ADMIN_BOUNDARY_BREADCRUMBS] && <BreadcrumbsPanel />}
         </div>
         <div className={s.title}>
-          <MapTitle />
+          {featureFlags[AppFeature.MAP_TITLE] && <MapTitle />}
         </div>
       </div>
       <div className={s.sidePanel}>
-        <CurrentEvent />
-        <LegendsPanel />
+        {featureFlags[AppFeature.CURRENT_EVENT] && <CurrentEvent />}
+        {featureFlags[AppFeature.LEGEND_PANEL] && <LegendsPanel />}
       </div>
-      <div className={s.copyrights}>{<LayersCopyrights />}</div>
+      <div className={s.copyrights}>
+        {featureFlags[AppFeature.LAYERS_COPYRIGHTS] && <LayersCopyrights />}
+      </div>
       <div className={s.scaleAndLogo}>{scaleAndLogo}</div>
     </div>
   );
