24ad545068d3b98946d8d82dfb4ef0095cd54c36
Natallia Harshunova <nattallius@gmail.com>
2025-05-09 10:31:54 +0200

    feat(21323): pr fixes

--- src/components/MapTitle/MapTitle.tsx
+++ src/components/MapTitle/MapTitle.tsx
@@ -9,12 +9,8 @@ type MapTitleProps = {
 
 export const MapTitle = ({ title, description }: MapTitleProps) => {
   return (
-    <div className={clsx(s.container, 'knt-panel')}>
-      <AppLogo
-        isOpen={true}
-        logoContainerClassName={s.logoContainer}
-        labelClassName={s.appLabel}
-      />
+    <div className={clsx(s.container, 'knt-panel knt-map-title')}>
+      <AppLogo isOpen={true} labelClassName={s.appLabel} />
       {title && <div className={s.title}>{title}</div>}
       {description && <div className={s.description}>{description}</div>}
     </div>
--- src/features/side_bar/components/AppLogo/AppLogo.tsx
+++ src/features/side_bar/components/AppLogo/AppLogo.tsx
@@ -6,20 +6,14 @@ import s from './AppLogo.module.css';
 type AppLogoProps = {
   className?: string;
   labelClassName?: string;
-  logoContainerClassName?: string;
   isOpen: boolean;
 };
 
-export function AppLogo({
-  isOpen,
-  labelClassName,
-  logoContainerClassName,
-  className,
-}: AppLogoProps) {
+export function AppLogo({ isOpen, labelClassName, className }: AppLogoProps) {
   const iconPath = configRepo.get().sidebarIconUrl;
 
   const appIcon = iconPath ? (
-    <div className={clsx(s.logoContainer, logoContainerClassName)}>
+    <div className={clsx(s.logoContainer, 'knt-app-icon')}>
       <img src={iconPath} alt={i18n.t('sidebar.icon_alt')} />
     </div>
   ) : null;
