3a810222160b897ec917e067f8ad703ae313e59a
Aliaksandr Baranau <abaranau.dev@gmail.com>
2025-05-19 19:32:07 +0200

    feat: make BBbox filter independent of the panel

--- src/features/events_list/components/EventsPanelSettings/EventsPanelSettings.tsx
+++ src/features/events_list/components/EventsPanelSettings/EventsPanelSettings.tsx
@@ -1,5 +1,7 @@
+import { useAtom } from '@reatom/react-v2';
 import { configRepo } from '~core/config';
 import { AppFeature } from '~core/app/types';
+import { eventListFilters } from '~features/events_list/atoms/eventListFilters';
 import { EventListSettingsRow } from '../EventListSettingsRow/EventListSettingsRow';
 import { FeedSelectorFlagged } from '../FeedSelector';
 import { BBoxFilterToggle } from '../BBoxFilterToggle/BBoxFilterToggle';
@@ -7,10 +9,22 @@ import { BBoxFilterToggle } from '../BBoxFilterToggle/BBoxFilterToggle';
 const featureFlags = configRepo.get().features;
 
 export function EventsPanelSettings() {
+  const [bbox, { setBBoxFilterFromCurrentMapView, resetBboxFilter }] = useAtom(
+    eventListFilters,
+    (filters) => filters.bbox,
+    [],
+  );
+
   return (
     <EventListSettingsRow>
       <FeedSelectorFlagged />
-      {featureFlags[AppFeature.EVENTS_LIST__BBOX_FILTER] && <BBoxFilterToggle />}
+      {featureFlags[AppFeature.EVENTS_LIST__BBOX_FILTER] && (
+        <BBoxFilterToggle
+          currentFilter={bbox}
+          onSetFilter={setBBoxFilterFromCurrentMapView}
+          onCleanFilter={resetBboxFilter}
+        />
+      )}
       {/* TODO: for now we don't want these sort buttons, there was no design for them */}
       {/* <EventListSortButton
               onSort={onSort}
