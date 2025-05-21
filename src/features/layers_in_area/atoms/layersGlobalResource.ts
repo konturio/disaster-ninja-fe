37d6f6f6121b8cb8f0cdd2d9e2dfb45078b89734
Darafei Praliaskouski <me@komzpa.net>
2025-05-21 20:57:18 +0400

    fix screenshot readiness event

--- src/features/layers_in_area/atoms/layersGlobalResource.ts
+++ src/features/layers_in_area/atoms/layersGlobalResource.ts
@@ -1,12 +1,15 @@
 import { createAsyncAtom } from '~utils/atoms/createAsyncAtom';
 import { filterUnsupportedLayerTypes } from '~core/logical_layers/layerTypes';
 import { getGlobalLayers } from '~core/api/layers';
+import { dispatchMetricsEventOnce } from '~core/metrics/dispatch';
 
 export const layersGlobalResource = createAsyncAtom(
   null,
   async (_, abortController) => {
     const layers = await getGlobalLayers(abortController);
-    return filterUnsupportedLayerTypes(layers || []);
+    const filteredLayers = filterUnsupportedLayerTypes(layers || []);
+    dispatchMetricsEventOnce('layersGlobalResource', filteredLayers.length > 0);
+    return filteredLayers;
   },
   'layersGlobalResource',
 );
