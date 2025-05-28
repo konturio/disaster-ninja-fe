d4876ea5c1728a0d7ff051cad9cad67a49149602
Aliaksandr Baranau <abaranau.dev@gmail.com>
2025-05-28 14:41:02 +0200

    feat: improve number filter regexps

--- src/utils/form/inputFilters.ts
+++ src/utils/form/inputFilters.ts
@@ -1,2 +1,2 @@
-export const INPUT_FILTER_NUMBER = /[^.\-\d]/;
-export const INPUT_FILTER_POSITIVE_NUMBER = /[^.\d]/;
+export const INPUT_FILTER_NUMBER = /[^.\-\d]/g;
+export const INPUT_FILTER_POSITIVE_NUMBER = /[^.\d]/g;
