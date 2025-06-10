3ad49df46c2949f1a584eb05473e86d111503d0c
Darafei Praliaskouski <me@komzpa.net>
2025-06-11 00:43:13 +0400

    feat(locate_me): 18696 show spinner while locating

---
+++ src/features/locate_me/LocateMeButton.tsx
@@ -0,0 +1,51 @@
+import { Locate24 } from '@konturio/default-icons';
+import { Spinner } from '@konturio/ui-kit';
+import { useState, useCallback } from 'react';
+import { i18n } from '~core/localization';
+import { currentNotificationAtom } from '~core/shared_state';
+import { setCurrentMapPosition } from '~core/shared_state/currentMapPosition';
+import { store } from '~core/store/store';
+import { LOCATE_ME_CONTROL_NAME, LOCATE_ME_ZOOM } from './constants';
+import type { WidgetProps } from '~core/toolbar/types';
+
+export function LocateMeButton({
+  controlComponent: ControlComponent,
+  onClick,
+  state,
+  id,
+}: WidgetProps) {
+  const [isLocating, setIsLocating] = useState(false);
+
+  const handleClick = useCallback(() => {
+    setIsLocating(true);
+    navigator.geolocation.getCurrentPosition(
+      (location) => {
+        const { latitude: lat, longitude: lng } = location.coords;
+        setCurrentMapPosition(store.v3ctx, { lat, lng, zoom: LOCATE_ME_ZOOM });
+        setIsLocating(false);
+      },
+      (error) => {
+        currentNotificationAtom.showNotification.dispatch(
+          'warning',
+          { title: error.message || i18n.t('locate_me.get_location_error') },
+          3,
+        );
+        setIsLocating(false);
+      },
+    );
+    onClick();
+  }, [onClick]);
+
+  return (
+    <ControlComponent
+      id={id}
+      size="tiny"
+      icon={isLocating ? <Spinner size={16} /> : <Locate24 width={16} height={16} />}
+      onClick={handleClick}
+      active={state === 'active'}
+      disabled={state === 'disabled'}
+    >
+      {LOCATE_ME_CONTROL_NAME}
+    </ControlComponent>
+  );
+}
