adf88ef9833ecb166202b536c5db67684a296739
alesiahil <112590025+alesiahil@users.noreply.github.com>
2025-06-29 19:40:12 +0300

    chore(features): 11105 remove redundant feature flags

--- src/views/Map/Map.tsx
+++ src/views/Map/Map.tsx
@@ -90,11 +90,7 @@ export function MapPage() {
     if (featureFlags[AppFeature.OSM_EDIT_LINK]) {
       import('~features/osm_edit_link/').then(({ initOsmEditLink }) => initOsmEditLink());
     }
-    // TODO add feature flag to replace 'draw_tools' to 'focused_geometry_editor'
-    if (
-      featureFlags[AppFeature.DRAW_TOOLS] ||
-      featureFlags[AppFeature.FOCUSED_GEOMETRY_EDITOR]
-    ) {
+    if (featureFlags[AppFeature.FOCUSED_GEOMETRY_EDITOR]) {
       import('~widgets/FocusedGeometryEditor').then(({ initFocusedGeometry }) =>
         initFocusedGeometry(),
       );
