35ca089651dbc6d9120923fd5b4ca2438603bd12
Aliaksandr Baranau <abaranau.dev@gmail.com>
2025-07-03 16:42:22 +0200

    add style prop to Image component

--- src/components/Uni/Components/Image.tsx
+++ src/components/Uni/Components/Image.tsx
@@ -1,18 +1,20 @@
 import clsx from 'clsx';
 import { applyValueToTemplate } from '../helpers/applyValueToTemplate';
 import s from './Image.module.css';
+import type { CSSProperties } from 'react';
 
 export interface ImageProps {
   value?: string;
   className?: string;
   urlTemplate?: string;
+  style?: CSSProperties;
 }
 
-export function Image({ value, className, urlTemplate }: ImageProps) {
+export function Image({ value, className, urlTemplate, style }: ImageProps) {
   const url = urlTemplate ? applyValueToTemplate(urlTemplate, value) : value;
 
   return (
-    <div className={clsx(s.imageContainer, className)}>
+    <div className={clsx(s.imageContainer, className)} style={style}>
       <img src={url} className={s.image}></img>
     </div>
   );
