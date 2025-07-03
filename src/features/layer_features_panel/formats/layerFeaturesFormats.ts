a931e9e80a37d8ae6fcd608ddb483ed5e1550079
Aliaksandr Baranau <abaranau.dev@gmail.com>
2025-07-03 15:03:34 +0200

    remove unused date_time format

--- src/features/layer_features_panel/formats/layerFeaturesFormats.ts
+++ src/features/layer_features_panel/formats/layerFeaturesFormats.ts
@@ -1,18 +1,7 @@
 import { i18n } from '~core/localization';
 import { capitalize } from '~utils/common';
 
-const language = i18n.instance.language || 'default';
-const dateTimeFormatter = new Intl.DateTimeFormat(language, {
-  year: 'numeric',
-  month: 'short',
-  day: 'numeric',
-  timeZoneName: 'short',
-}).format;
-
 export const layerFeaturesFormats = {
-  date_time(value?: string) {
-    return value ? dateTimeFormatter(new Date(value)) : '';
-  },
   project_id(value?: string | number) {
     return value ? `#${value}` : '';
   },
