fbbe8d47204a0d5a881b79846c5866d9fac628d6
Aliaksandr Baranau <abaranau.dev@gmail.com>
2025-07-24 14:12:30 +0200

    feat: rework MultivariateLegend to include all present dimensions

--- src/components/MultivariateLegend/MultivariateLegend.tsx
+++ src/components/MultivariateLegend/MultivariateLegend.tsx
@@ -205,8 +205,6 @@ function createFillLegend(config: MultivariateLayerConfig) {
   } else if (config.base?.config.layers?.length) {
     return createMCDALegend(config.base.config, i18n.t('multivariate.compare'));
   }
-  // TODO: implement more complex legend based on included dimensions
-  return <>Multivariate legend fallback</>;
 }
 
 export function MultivariateLegend({ config }: MultivariateLegendProps) {
