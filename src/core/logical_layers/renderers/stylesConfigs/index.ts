60bb579d44a398ed7f296762c3006d653d45425b
Aliaksandr Baranau <abaranau.dev@gmail.com>
2025-07-24 15:12:57 +0200

    chore: update comments

--- src/core/logical_layers/renderers/stylesConfigs/index.ts
+++ src/core/logical_layers/renderers/stylesConfigs/index.ts
@@ -21,35 +21,38 @@ export const styleConfigs: Record<
   multivariate: (config: MultivariateLayerStyle['config']) => {
     let multivariateStyle: FillLayerSpecification;
     if (config.score && config.base) {
-      // if we have both score and base - the layer becomes bivariate
+      // if we have both score and base dimension - use bivariate colors
       const colorsAndStyle = generateBivariateColorsAndStyleForMultivariateLayer(
         config,
         SOURCE_LAYER_BIVARIATE,
       );
       multivariateStyle = colorsAndStyle[1];
     } else if (config.score?.config?.layers.length) {
-      // create MCDA based on Score dimension
+      // create MCDA colors based on Score dimension
       multivariateStyle = createMCDAStyle(config.score.config);
     } else if (config.base?.config?.layers.length) {
+      // create MCDA colors based on base (Compare) dimension
       multivariateStyle = createMCDAStyle(config.base.config);
     } else {
-      // create filter based on all MCDA layers from opacity and extrusion
+      // No color dimensions - create monochrome fill
       const layersForFilter: MCDAConfig['layers'] = [];
+      // create filter based on all MCDA layers from opacity and extrusion
       if (typeof config.opacity === 'object' && config.opacity?.config.layers.length) {
         layersForFilter.push(...config.opacity.config.layers);
       }
       if (config.extrusion?.height.config.layers.length) {
         layersForFilter.push(...config.extrusion.height.config.layers);
       }
-      // create grey monochrome fill
+      // monochrome fill specification
       multivariateStyle = createMonochromeFillSpec(
         mcdaFilterSetup(layersForFilter),
-        // grey fill if there's opacity or extrusion. Transparent fill otherwise
+        // grey fill if there's opacity or extrusion dimensions. Transparent fill otherwise
         layersForFilter.length > 0 ? DEFAULT_GREY_FILL_COLOR : TRANSPARENT_COLOR,
         SOURCE_LAYER_MCDA,
       );
     }
     if (config.opacity !== undefined) {
+      // apply opacity to fill layers
       const opacity = !isNumber(config.opacity)
         ? createOpacityStepsExpression(config.opacity)
         : config.opacity;
