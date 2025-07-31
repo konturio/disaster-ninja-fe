f787e179bab2299bf9ab141a7aaf3bf2aa3e35b6
Aliaksandr Baranau <abaranau.dev@gmail.com>
2025-07-31 15:49:30 +0200

    Revert "feat(ci): 21190 cache docker layers" (#1197)
    * Revert "feat(ci): 21190 cache docker layers (#1161)"
    
    This reverts commit 746077fecae72ef96341b0c2940f9fbea2dbe2fb.
    
    * Revert "fix(workflows): no-ticket set up buildx (#1183)"
    
    This reverts commit 206eec841ad0a0f06b110d52d952983b89c0b9d8.

--- server/main.go
+++ server/main.go
@@ -16,15 +16,9 @@ import (
 )
 
 const (
-        Host = ""
+	Host = ""
+	Port = "80"
 )
-
-func getPort() string {
-        if p := os.Getenv("PORT"); p != "" {
-                return p
-        }
-        return "8080"
-}
 
 type TemplateVariables struct {
 	CurrentQueries string
@@ -173,9 +167,8 @@ func main() {
 	http.Handle("/active/static/", http.StripPrefix("/active/static", http.HandlerFunc(handleStaticFiles(fs))))
 	http.Handle("/metrics", promhttp.Handler())
 	http.Handle("/active/log", http.StripPrefix("/active", http.HandlerFunc(writeLogs)))
-        port := getPort()
-        log.Println("Server listening:", "http://"+Host+":"+port)
-        err := http.ListenAndServe(Host+":"+port, nil)
+	log.Println("Server listening:", "http://"+Host+":"+Port)
+	err := http.ListenAndServe(Host+":"+Port, nil)
 	if err != nil {
 		log.Fatal("Error Starting the HTTP Server :", err)
 		return
