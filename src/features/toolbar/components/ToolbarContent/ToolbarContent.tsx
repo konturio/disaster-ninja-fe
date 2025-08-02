3aff2b59d7385d2c87d4c337ef9f7479ac697a7c
alesiahil <112590025+alesiahil@users.noreply.github.com>
2025-08-03 00:46:11 +0300

    feat(toolbar): remove group names and divider

--- src/features/toolbar/components/ToolbarContent/ToolbarContent.tsx
+++ src/features/toolbar/components/ToolbarContent/ToolbarContent.tsx
@@ -1,5 +1,3 @@
-import { Fragment } from 'react';
-import { ChevronDown16 } from '@konturio/default-icons';
 import { useToolbarContent } from '~features/toolbar/hooks/use-toolbar-content';
 import { ToolbarControl } from '../ToolbarControl/ToolbarControl';
 import { ToolbarButton } from '../ToolbarButton/ToolbarButton';
@@ -10,33 +8,21 @@ export const ToolbarContent = () => {
 
   return (
     <div className={s.toolbarContent}>
-      {toolbarContent.map(({ sectionName, buttons }, index) => (
-        <Fragment key={sectionName}>
-          {index > 0 && <div className={s.sectionDivider} />}
-          {
-            <div className={s.section}>
-              <div className={s.sectionContent}>
-                {buttons.map(({ id, settings, stateAtom }) => {
-                  return (
-                    <ToolbarControl
-                      id={id}
-                      data-testid={id}
-                      key={id}
-                      settings={settings}
-                      stateAtom={stateAtom}
-                      controlComponent={ToolbarButton}
-                    />
-                  );
-                })}
-              </div>
-              <label className={s.sectionLabel}>
-                <input type="checkbox" className={s.sectionToggleCheckbox} />
-                {sectionName}
-                <ChevronDown16 className={s.sectionArrow} />
-              </label>
-            </div>
-          }
-        </Fragment>
+      {toolbarContent.map(({ sectionName, buttons }) => (
+        <div key={sectionName} className={s.section}>
+          <div className={s.sectionContent}>
+            {buttons.map(({ id, settings, stateAtom }) => (
+              <ToolbarControl
+                id={id}
+                data-testid={id}
+                key={id}
+                settings={settings}
+                stateAtom={stateAtom}
+                controlComponent={ToolbarButton}
+              />
+            ))}
+          </div>
+        </div>
       ))}
     </div>
   );
