891053e6deee698d6eb776a29f8e507b2f05fa6d
alesiahil <112590025+alesiahil@users.noreply.github.com>
2025-08-11 18:22:27 +0300

    fix(toolbar): remove group names and divider (#1217)
    Co-authored-by: VK <112831093+vkozio@users.noreply.github.com>

--- src/features/toolbar/components/ToolbarContent/ToolbarContent.tsx
+++ src/features/toolbar/components/ToolbarContent/ToolbarContent.tsx
@@ -1,5 +1,4 @@
-import { Fragment, useRef } from 'react';
-import { ChevronDown16 } from '@konturio/default-icons';
+import { useRef } from 'react';
 import { useToolbarContent } from '~features/toolbar/hooks/use-toolbar-content';
 import { useWheelHorizontalScroll } from '~utils/hooks/useWheelHorizontalScroll';
 import { ToolbarControl } from '../ToolbarControl/ToolbarControl';
@@ -7,39 +6,21 @@ import { ToolbarButton } from '../ToolbarButton/ToolbarButton';
 import s from './ToolbarContent.module.css';
 
 export const ToolbarContent = () => {
-  const toolbarContent = useToolbarContent();
+  const toolbarContent = useToolbarContent().flatMap(({ buttons }) => buttons);
   const scrollRef = useRef<HTMLDivElement>(null);
   useWheelHorizontalScroll(scrollRef);
 
   return (
-    <div className={s.toolbarContent} ref={scrollRef}>
-      {toolbarContent.map(({ sectionName, buttons }, index) => (
-        <Fragment key={sectionName}>
-          {index > 0 && <div className={s.sectionDivider} />}
-          {
-            <div className={s.section}>
-              <div className={s.sectionContent}>
-                {buttons.map(({ id, settings, stateAtom }) => {
-                  return (
-                    <ToolbarControl
-                      id={id}
-                      data-testid={id}
-                      key={id}
-                      settings={settings}
-                      stateAtom={stateAtom}
-                      controlComponent={ToolbarButton}
-                    />
-                  );
-                })}
-              </div>
-              <label className={s.sectionLabel}>
-                <input type="checkbox" className={s.sectionToggleCheckbox} />
-                {sectionName}
-                <ChevronDown16 className={s.sectionArrow} />
-              </label>
-            </div>
-          }
-        </Fragment>
+    <div className={s.toolbarContent}>
+      {toolbarContent.map(({ id, settings, stateAtom }) => (
+        <ToolbarControl
+          id={id}
+          data-testid={id}
+          key={id}
+          settings={settings}
+          stateAtom={stateAtom}
+          controlComponent={ToolbarButton}
+        />
       ))}
     </div>
   );
